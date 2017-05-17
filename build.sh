d06ffa9bfed8fb3fc5fcf2047ef9be9c38d15bcd
texasaggie97 <mark.silva@ni.com>
2017-05-17 14:11:10 -0500

    Move generated nidmm files into nidmm folder

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 
-rm bin/*
+rm -Rf bin/nidmm
+mkdir bin/nidmm
 
-python3 codegen/generateTemplate.py --template codegen/templates/library.py.mako --metadata metadata/nidmm/nidmm_metadata.py --dest-file bin/nidmm.py -v -v
+python3 codegen/generateTemplate.py --template codegen/templates/library.py.mako --metadata metadata/nidmm/nidmm_metadata.py --dest-file bin/nidmm/library.py -v -v
