3a5559900b175f60db0278a0b8d4c8ea82e1072b
texasaggie97 <mark.silva@ni.com>
2017-05-17 14:37:14 -0500

    Add attribute generation to build process

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ rm -Rf bin/nidmm
 mkdir bin/nidmm
 
 python3 codegen/generateTemplate.py --template codegen/templates/library.py.mako --metadata metadata/nidmm/nidmm_metadata.py --dest-file bin/nidmm/library.py -v -v
+python3 codegen/generateTemplate.py --template codegen/templates/attributes.py.mako --metadata metadata/nidmm/nidmm_metadata.py --dest-file bin/nidmm/attributes.py -v -v
+
+
