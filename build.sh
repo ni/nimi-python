780df0ef88a02b8ea4f77b3c26eb561e52ef7455
Marcos Kirsch <github@kirsch.com.mx>
2017-05-23 15:43:59 -0500

    Moved all source code into src/ directory. Created NI-DMM directory for API specific files.

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
-
-rm -Rf bin/nidmm
+rm -Rf bin
+mkdir bin
 mkdir bin/nidmm
 
-python3 codegen/generateTemplate.py --template codegen/templates/library.py.mako --metadata metadata/nidmm/nidmm_metadata.py --dest-file bin/nidmm/library.py -v -v
-python3 codegen/generateTemplate.py --template codegen/templates/attributes.py.mako --metadata metadata/nidmm/nidmm_metadata.py --dest-file bin/nidmm/attributes.py -v -v
+python3 src/codegen/generateTemplate.py \
+    --template src/codegen/templates/library.py.mako \
+    --metadata src/NI-DMM/metadata/nidmm_metadata.py \
+    --dest-file bin/nidmm/library.py -v -v
 
+python3 src/codegen/generateTemplate.py \
+    --template src/codegen/templates/attributes.py.mako \
+    --metadata src/NI-DMM/metadata/nidmm_metadata.py \
+    --dest-file bin/nidmm/attributes.py -v -v
 
