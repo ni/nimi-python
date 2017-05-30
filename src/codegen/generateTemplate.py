7336fde6d0346f8c9a97a094077a3174b1e548cc
Marcos Kirsch <github@kirsch.com.mx>
2017-05-30 17:41:28 -0500

    Add missing VISA/IVI to ctypes mappings.

--- src/codegen/generateTemplate.py
+++ src/codegen/generateTemplate.py
@@ -15,8 +15,15 @@ types = {
     'ViBoolean': 'c_ushort',
     'ViSession': 'c_ulong',
     'ViChar': 'c_char_p',
-    'ViInt32': 'c_ulong',
+    'ViUInt32': 'c_ulong',
+    'ViInt32': 'c_long',
+    'ViInt16': 'c_short',
+    'ViUInt16': 'c_ushort',
+    'ViReal32': 'c_float',
     'ViReal64': 'c_double',
+    'ViString': 'c_char_p',
+    'ViConstString': 'c_char_p',
+    'ViAttr': 'c_long',
 }
 
 def configureLogging(lvl = logging.WARNING, logfile = None):
