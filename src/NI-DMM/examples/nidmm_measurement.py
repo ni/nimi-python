513970f540a328b106463dba112ed73af70b98d6
Marcos Kirsch <github@kirsch.com.mx>
2017-06-05 15:36:01 -0500

    Display defaults in help, improve error handling, return error code.

--- src/NI-DMM/examples/nidmm_measurement.py
+++ src/NI-DMM/examples/nidmm_measurement.py
@@ -8,13 +8,17 @@ sys.path.append(os.path.join(sys.path[0],'../../../bin/'))
 import argparse
 import nidmm
 
-parser = argparse.ArgumentParser(description='Performs a single measurement using the NI-DMM API.')
+parser = argparse.ArgumentParser(description='Performs a single measurement using the NI-DMM API.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-n', '--name', default='PXI1Slot2', help='Resource name of a National Instruments Digital Multimeter.')
 parser.add_argument('-f', '--function', default='DC_VOLTS', choices=nidmm.Function.__members__.keys(), type=str.upper, help='Measurement function.')
 parser.add_argument('-r', '--range', default=10, type=float, help='Measurement range.')
 parser.add_argument('-d', '--digits', default=6.5, type=float, help='Digits of resolution for the measurement.')
 args = parser.parse_args()
 
-with nidmm.Session(args.name) as session:
-    session.configureMeasurementDigits(nidmm.Function[args.function], args.range, args.digits)
-    print(session.read())
+try:
+    with nidmm.Session(args.name) as session:
+        session.configureMeasurementDigits(nidmm.Function[args.function], args.range, args.digits)
+        print(session.read())
+except nidmm.Error as e:
+    print(e)
+    sys.exit(e.code)
