c19667df3c94d95ed88db9ee752a8b3c8aae6adb
texasaggie97 <mark.silva@ni.com>
2017-06-06 16:21:54 -0500

    Move metadata into separate files

--- build.sh
+++ build.sh
@@ -4,18 +4,22 @@ mkdir bin/nidmm
 
 python3 src/codegen/generateTemplate.py \
     --template src/codegen/templates/library.py.mako \
+    --driver NI-DMM \
     --dest-file bin/nidmm/library.py -v -v
 
 python3 src/codegen/generateTemplate.py \
     --template src/codegen/templates/attributes.py.mako \
+    --driver NI-DMM \
     --dest-file bin/nidmm/attributes.py -v -v
 
 python3 src/codegen/generateTemplate.py \
     --template src/codegen/templates/enums.py.mako \
+    --driver NI-DMM \
     --dest-file bin/nidmm/enums.py -v -v
 
 python3 src/codegen/generateTemplate.py \
     --template src/codegen/templates/session.py.mako \
+    --driver NI-DMM \
     --dest-file bin/nidmm/session.py -v -v
 
 cp src/NI-DMM/nidmm/__init__.py     bin/nidmm/__init__.py
