1123aed5e497a8ae92dfc6545794ebdf6636fd30
texasaggie97 <mark.silva@ni.com>
2017-06-07 14:11:14 -0500

    Rename NI-DMM -> nidmm

--- src/nidmm/metadata/config.py
+++ src/nidmm/metadata/config.py
@@ -2,6 +2,7 @@ config = {
     'module_name': 'nidmm',
     'module_version': '0.1',
     'c_function_prefix': 'niDMM_',
+    'files_to_generate': ['attributes.py', 'enums.py', 'library.py', 'session.py'],
     'session_description': 'An NI-DMM session to a National Instruments Digital Multimeter',
     'library_windows':
     {
