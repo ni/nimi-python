1380bc21f1e2c9f004413549cce66945735dd5e0
Marcos Kirsch <github@kirsch.com.mx>
2017-06-12 14:45:49 -0500

    placeholder for helper.py unit tests.

---
+++ src/tests/test_helper.py
@@ -0,0 +1,12 @@
+import codegen.helper
+
+def test_foo():
+    assert False
+
+'''
+Functions to test:
+    def  shoutcase_to_camelcase(shout_string):
+    def camelcase_to_snakecase(camelcase_string):
+    def function_to_method_name(f):
+    def extract_input_parameters(parameters):
+'''
