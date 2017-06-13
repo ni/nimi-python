c4f4c114dbc646b99f459e52038d974123b26012
texasaggie97 <mark.silva@ni.com>
2017-06-13 17:26:10 -0500

    Make build into a zipped python package
    * Move from src/codegen to build
    * Move build.py into build
    * Modularize into multiple python files to be more logical
    * Add build/__init__.py to make it a package
    * Add build/__main__.py for the entry point to the package
    * Add ability to generate individual make templates from the command
    lone

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
+
 # Python 3 is required for build.py
-python3 build.py clean make -v --metadata src/nidmm/metadata
+
+rm -Rf bin
+mkdir bin
+
+# Create fake build.py that is an executable zip file and use that for subsequent steps
+py -m zipapp -o bin/build.pyz build
+
+py bin/build.pyz clean make local_install -v --metadata src/nidmm/metadata
 
 
---
+++ build/generate_template.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+from mako.template import Template
+from mako.exceptions import RichTraceback
+import logging
+import argparse
+import os
+import pkg_resources
+import pprint
+import sys
+
+pp = pprint.PrettyPrinter(indent=3)
+
+def generate_template(template_name, template_params, dest_file, in_zip_file=False):
+    try:
+        # if we are in a zip file, we need to get the mako template as a resource string
+        #  from the contents of the zip file
+        if in_zip_file:
+            tmpl = pkg_resources.resource_string(__name__, template_name)
+            template = Template(tmpl)
+        else:
+            template = Template(filename=template_name)
+        rendered_template = template.render(template_parameters=template_params)
+
+    except:
+        # Because mako expands into python, we catch all errors, not just MakoException.
+        # Ideally, we'd use text_error_template, but it sucks.  html_error_template,
+        # however, is useful.  Unfortunately emitting html isn't acceptable.  So we
+        # re-implement using mako.exceptions.RichTraceback here.
+        tback = RichTraceback(traceback=None)
+        line = tback.lineno
+        lines = tback.source.split('\n')
+
+        # The underlying error.
+        logging.error("\n%s: %s\n" % ( str(tback.error.__class__.__name__), str(tback.error) ))
+        logging.error("Offending Template: %s\n" % template_name)
+
+        # Show a source listing of the template, with offending line marked.
+        for index in range(max(0, line - 4), min(len(lines), line + 5)):
+            if index + 1 == line:
+                logging.error(">> %#08d: %s" % (index + 1, lines[index]))
+            else:
+                logging.error("   %08d: %s" % (index + 1, lines[index]))
+
+        logging.error("\nTraceback (most recent call last):")
+        for (filename, lineno, function, line) in tback.reverse_traceback:
+            logging.error("   File %s, line %d, in %s\n     %s" % (filename, lineno, function, line))
+
+        logging.error("\n")
+        sys.exit(1)
+
+    logging.debug(rendered_template)
+    fileHandlePublic = open(dest_file, 'wb')
+    fileHandlePublic.write(bytes(rendered_template, "UTF-8"))
+    fileHandlePublic.close()
+
+
+
