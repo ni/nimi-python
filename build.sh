74cb28bdd7a210ceeb3452c07d133c519fb20c8c
texasaggie97 <mark.silva@ni.com>
2017-06-13 17:38:19 -0500

    Add TODOs and comments

--- build.sh
+++ build.sh
@@ -5,8 +5,12 @@ rm -Rf bin
 mkdir bin
 
 # Create fake build.py that is an executable zip file and use that for subsequent steps
+# Needs Python 3.5+ for zipapp
+# TODO(texasaggie97) change to python when able
 py -m zipapp -o bin/build.pyz build
 
+# Needs Python 3.5+ for pkg_resources
+# TODO(texasaggie97) change to python when able
 py bin/build.pyz clean make local_install -v --metadata src/nidmm/metadata
 
 
