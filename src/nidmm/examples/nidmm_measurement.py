251af9ac20c890f070980724b18e4e56f0079606
Frank <fladolcetta@gmail.com>
2017-06-13 10:13:40 -0500

    Updating to use InitWithOptions by default in template Updating dmm example to add arguments to use full features of InitWithOptions

--- src/nidmm/examples/nidmm_measurement.py
+++ src/nidmm/examples/nidmm_measurement.py
@@ -9,14 +9,17 @@ import argparse
 import nidmm
 
 parser = argparse.ArgumentParser(description='Performs a single measurement using the NI-DMM API.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-n', '--name', default='PXI1Slot2', help='Resource name of a National Instruments Digital Multimeter.')
-parser.add_argument('-f', '--function', default='DC_VOLTS', choices=nidmm.Function.__members__.keys(), type=str.upper, help='Measurement function.')
-parser.add_argument('-r', '--range', default=10, type=float, help='Measurement range.')
-parser.add_argument('-d', '--digits', default=6.5, type=float, help='Digits of resolution for the measurement.')
+parser.add_argument('-n',  '--name', default='PXI1Slot2', help='Resource name of a National Instruments Digital Multimeter.')
+parser.add_argument('-i',  '--idquery', default=0, help='Verifies that the device you initialize is one that the driver supports.')
+parser.add_argument('-re', '--reset', default=False, help='Specifies whether to reset the instrument during the initialization procedure.')
+parser.add_argument('-o',  '--optionstring', default='', help='Sets the initial value of certain properties for the session.')
+parser.add_argument('-f',  '--function', default='DC_VOLTS', choices=nidmm.Function.__members__.keys(), type=str.upper, help='Measurement function.')
+parser.add_argument('-ra', '--range', default=10, type=float, help='Measurement range.')
+parser.add_argument('-d',  '--digits', default=6.5, type=float, help='Digits of resolution for the measurement.')
 args = parser.parse_args()
 
 try:
-    with nidmm.Session(args.name) as session:
+    with nidmm.Session(args.name, args.idquery, args.reset, args.optionstring) as session:
         session.configure_measurement_digits(nidmm.Function[args.function], args.range, args.digits)
         print(session.read())
 except nidmm.Error as e:
