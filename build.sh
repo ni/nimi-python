6900e4970f9204b706f978b69c9274790e88277b
texasaggie97 <mark.silva@ni.com>
2017-06-14 14:16:01 -0500

    Change to python

--- build.sh
+++ build.sh
@@ -6,11 +6,9 @@ mkdir bin
 
 # Create fake build.py that is an executable zip file and use that for subsequent steps
 # Needs Python 3.5+ for zipapp
-# TODO(texasaggie97) change to python when able
-py -m zipapp -o bin/build.pyz build
+python -m zipapp -o bin/build.pyz build
 
 # Needs Python 3.5+ for pkg_resources
-# TODO(texasaggie97) change to python when able
-py bin/build.pyz clean make local_install -v --metadata src/nidmm/metadata
+python bin/build.pyz clean make local_install -v --metadata src/nidmm/metadata
 
 
