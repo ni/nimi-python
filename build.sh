63afd0524d04a7d11c010f58413b6f5c9ac93967
Mark Silva <github@markesilva.com>
2017-06-15 10:14:44 -0500

    Build/add buildbat (#33)
    * Add build.bat for Windows
    * Use correct action in build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
-
 # Python 3 is required for build.py
 rm -Rf bin
 mkdir bin
 
-# Create fake build.py that is an executable zip file and use that for subsequent steps
+# Create Create Python zipped application for ease of use
 # Needs Python 3.5+ for zipapp
 python -m zipapp -o bin/build.pyz build
 
 # Needs Python 3.5+ for pkg_resources
-python bin/build.pyz clean make local_install -v --metadata src/nidmm/metadata
+python bin/build.pyz clean make make_installer -v --metadata src/nidmm/metadata
 
 
 
