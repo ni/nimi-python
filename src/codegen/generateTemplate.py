86429addc8b752ae10796f85379d0f0ce7f600d4
Marcos Kirsch <github@kirsch.com.mx>
2017-06-15 17:23:24 -0500

    Add type mapping for VISA type to python type.

--- src/codegen/generateTemplate.py
+++ src/codegen/generateTemplate.py
@@ -11,20 +11,20 @@ import sys
 pp = pprint.PrettyPrinter(indent=3)
 
 type_map = {
-    'ViStatus': 'c_long',
-    'ViRsrc': 'c_char_p',
-    'ViBoolean': 'c_ushort',
-    'ViSession': 'c_ulong',
-    'ViChar': 'c_char_p',
-    'ViUInt32': 'c_ulong',
-    'ViInt32': 'c_long',
-    'ViInt16': 'c_short',
-    'ViUInt16': 'c_ushort',
-    'ViReal32': 'c_float',
-    'ViReal64': 'c_double',
-    'ViString': 'c_char_p',
-    'ViConstString': 'c_char_p',
-    'ViAttr': 'c_long',
+    'ViStatus':      {'python_type':'int', 'ctypes_type':'c_long'},
+    'ViRsrc':        {'python_type':'str', 'ctypes_type':'c_char_p'},
+    'ViBoolean':     {'python_type':'bool', 'ctypes_type':'c_ushort'},
+    'ViSession':     {'python_type':'int', 'ctypes_type':'c_ulong'},
+    'ViChar':        {'python_type':'int', 'ctypes_type':'c_char_p'},
+    'ViUInt32':      {'python_type':'int', 'ctypes_type':'c_ulong'},
+    'ViInt32':       {'python_type':'int', 'ctypes_type':'c_long'},
+    'ViInt16':       {'python_type':'int', 'ctypes_type':'c_short'},
+    'ViUInt16':      {'python_type':'int', 'ctypes_type':'c_ushort'},
+    'ViReal32':      {'python_type':'float', 'ctypes_type':'c_float'},
+    'ViReal64':      {'python_type':'float', 'ctypes_type':'c_double'},
+    'ViString':      {'python_type':'str', 'ctypes_type':'c_char_p'},
+    'ViConstString': {'python_type':'str', 'ctypes_type':'c_char_p'},
+    'ViAttr':        {'python_type':'int', 'ctypes_type':'c_long'},
 }
 
 def configure_logging(lvl = logging.WARNING, logfile = None):
