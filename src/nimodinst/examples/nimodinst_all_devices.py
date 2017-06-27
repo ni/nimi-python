5ddec16cdad4f39a799fac0b3816b65955fdf04f
texasaggie97 <mark.silva@ni.com>
2017-06-27 13:00:59 -0500

    Use len() instead of internal variable

--- src/nimodinst/examples/nimodinst_all_devices.py
+++ src/nimodinst/examples/nimodinst_all_devices.py
@@ -6,7 +6,7 @@ import sys
 try:
     with nimodinst.Session('') as session:
         if len(session) > 0:
-            print("%d items" % session.item_count)
+            print("%d items" % len(session))
             print("{: >20} {: >15} {: >10}".format('Name', 'Model', 'S/N'))
         for d in session:
             print("{: >20} {: >15} {: >10}".format(d.device_name, d.device_model, d.serial_number))
