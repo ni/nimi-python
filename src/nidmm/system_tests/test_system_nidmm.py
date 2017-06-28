cf6047be2134031a41fb7668b1a894c22cda8a27
Marcos Kirsch <github@kirsch.com.mx>
2017-06-28 14:11:18 -0500

    Change name of device in system test to match example.

--- src/nidmm/system_tests/test_system_nidmm.py
+++ src/nidmm/system_tests/test_system_nidmm.py
@@ -19,13 +19,13 @@ def test_invalid_device_name():
 
 
 def test_take_simple_measurement_works():
-    with nidmm.Session("Dev1") as session:
+    with nidmm.Session("PXI1Slot2") as session:
         session.configure_measurement_digits(nidmm.Function.DC_CURRENT, 1, 5.5)
         assert session.read(1000) < 0.01 # Assume nothing is connected to device, reads back around 0.
 
 
 def test_wrong_parameter_type():
-    with nidmm.Session("Dev1") as session:
+    with nidmm.Session("PXI1Slot2") as session:
         try:
             # We are passing a number where an enum is expected.
             session.configure_measurement_digits(1, 10, 5.5)
@@ -36,7 +36,7 @@ def test_wrong_parameter_type():
 
 
 def test_warning():
-    with nidmm.Session("Dev1") as session:
+    with nidmm.Session("PXI1Slot2") as session:
         session.configure_measurement_digits(nidmm.Function.RES_2_WIRE, 1e6, 3.5)
         try:
             print(session.read(1000)) # Assume nothing is connected to device, overrange!
@@ -47,31 +47,31 @@ def test_warning():
 
 
 def test_ViBoolean_attribute():
-    with nidmm.Session("Dev1") as session:
+    with nidmm.Session("PXI1Slot2") as session:
         assert session.simulate is False
         # TODO(marcoskirsch): set a boolean
 
 
 def test_ViString_attribute():
-    with nidmm.Session("Dev1") as session:
+    with nidmm.Session("PXI1Slot2") as session:
         assert "1A67CAC" == session.serial_number
         # TODO(marcoskirsch): set a string
 
 
 def test_ViInt32_attribute():
-    with nidmm.Session("Dev1") as session:
+    with nidmm.Session("PXI1Slot2") as session:
         session.sample_count = 5
         assert 5 == session.sample_count
 
 
 def test_ViReal64_attribute():
-    with nidmm.Session("Dev1") as session:
+    with nidmm.Session("PXI1Slot2") as session:
         session.range = 50 # Coerces up!
         assert 100 == session.range
 
 
 def test_Enum_attribute():
-    with nidmm.Session("Dev1") as session:
+    with nidmm.Session("PXI1Slot2") as session:
         session.function = nidmm.Function.AC_CURRENT
         assert session.function == nidmm.Function.AC_CURRENT
         assert type(session.function) is nidmm.Function
@@ -84,7 +84,7 @@ def test_Enum_attribute():
 
 
 def test_ViSession_attribute():
-    with nidmm.Session("Dev1") as session:
+    with nidmm.Session("PXI1Slot2") as session:
         try:
             session.io_session = 5
             assert false
@@ -97,5 +97,4 @@ def test_ViSession_attribute():
         except TypeError as e:
             print(e)
             pass
-
 
