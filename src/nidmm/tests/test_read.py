dc7303af2b93cb2779c1accbc5bea13cfe3c71ba
texasaggie97 <mark.silva@ni.com>
2017-06-29 11:11:51 -0500

    Test cleanup
    * Set side effect return value right before using
    * Use "constant" for session number

--- src/nidmm/tests/test_read.py
+++ src/nidmm/tests/test_read.py
@@ -9,7 +9,7 @@ from unittest.mock import create_autospec
 from unittest.mock import DEFAULT
 
 nidmm_side_effects = nidmm.tests.mock_library.side_effects_helper()
-nidmm_side_effects['InitWithOptions']['newVi'] = 42
+SESSION_NUM_FOR_TEST = 42
 
 @patch('nidmm.session.errors', spec_set=['_handle_error'])
 @patch('nidmm.ctypes_library.nidmm_ctypes_library', autospec=True)
@@ -25,14 +25,15 @@ def test_simple_read(patched_library, patched_ctypes_library, patched_errors):
     patched_ctypes_library.niDMM_close.side_effect = nidmm_side_effects.niDMM_close_side_effect
     patched_ctypes_library.niDMM_ConfigureMeasurementDigits.side_effect = nidmm_side_effects.niDMM_ConfigureMeasurementDigits_side_effect
 
+    nidmm_side_effects['InitWithOptions']['newVi'] = SESSION_NUM_FOR_TEST
+
     with nidmm.Session("Dev1") as session:
-        assert(session.vi == 42)
-        vi = session.vi # We have to save the session number so we cal validate the close call
+        assert(session.vi == SESSION_NUM_FOR_TEST)
         patched_ctypes_library.niDMM_InitWithOptions.assert_called_once_with(b'Dev1', 0, False, b'', ANY)
         patched_errors._handle_error.assert_called_once_with(session, patched_ctypes_library.niDMM_InitWithOptions.return_value)
 
         session.configure_measurement_digits(nidmm.Function.DC_CURRENT, 1, 5.5)
         patched_ctypes_library.niDMM_ConfigureMeasurementDigits.assert_called_once_with(session.vi, nidmm.Function.DC_CURRENT.value, 1, 5.5)
         patched_errors._handle_error.assert_called_with(session, patched_ctypes_library.niDMM_ConfigureMeasurementDigits.return_value)
-    patched_ctypes_library.niDMM_close.assert_called_once_with(vi)
+    patched_ctypes_library.niDMM_close.assert_called_once_with(SESSION_NUM_FOR_TEST)
 
