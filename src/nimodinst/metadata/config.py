aa47fb6790459e755b89378d55e3a87c8b9ad274
texasaggie97 <mark.silva@ni.com>
2017-06-29 11:51:30 -0500

    Fix session description

--- src/nimodinst/metadata/config.py
+++ src/nimodinst/metadata/config.py
@@ -4,7 +4,7 @@ config = {
     'module_version': '0.1',
     'c_function_prefix': 'niModInst_',
     'driver_name': 'NI-ModInst',
-    'session_description': 'An NI-ModInst session to a National Instruments Digital Multimeter',
+    'session_description': 'A NI-ModInst session to get device information',
     'library_name':
     {
         'Windows': {'32bit': 'nimodinst.dll', '64bit': 'nimodinst_64.dll'},
