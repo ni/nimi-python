a39142a2e95f598debfa94306c095d51467f06cb
Mark Silva <mark.silva@ni.com>
2017-07-20 11:38:56 -0500

    Add enum_docs.py

--- src/nidmm/metadata/__init__.py
+++ src/nidmm/metadata/__init__.py
@@ -3,5 +3,6 @@ from metadata.functions import functions
 from metadata.attributes import attributes
 from metadata.attribute_docs import attribute_docs
 from metadata.enums import enums
+from metadata.enum_docs import enum_docs
 
 __version__ = config['module_version']
