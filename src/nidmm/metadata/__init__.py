e821840424ec16b4bf4d4c92493c7759c58d7b0d
Mark Silva <mark.silva@ni.com>
2017-08-03 11:42:07 -0500

    Merge hand maintained information into generated functions list
    * Allows use of regex to minimize entries
    * Separete lists for visibility, enums, and parameter types - not required, just for maintainability

--- src/nidmm/metadata/__init__.py
+++ src/nidmm/metadata/__init__.py
@@ -1,17 +1,39 @@
 from metadata.config import config
 from metadata.functions import functions
-from metadata.functions_addon import functions_addon
 from metadata.attributes import attributes
 from metadata.enums import enums
 
+import re
 def merge_dicts(into, outof):
-    for item in outof:
+    for item in sorted(outof):
         if type(outof[item]) is dict:
-            assert type(into[item]) is dict
-            merge_dicts(into[item], outof[item])
+            if item in into and type(into[item] is list):
+                merge_dicts(into[item], outof[item])
+            elif item in into:
+                merge_dicts(into[item], outof[item])
+            elif type(into) is list:
+                for item2 in outof[item]:
+                    into[item][item2] = outof[item][item2]
+            else:
+                # Handle regex in addon
+                for item2 in into:
+                    if re.search(item, item2):
+                        assert type(into[item2]) is dict
+                        merge_dicts(into[item2], outof[item])
         else:
-            into[item] = outof[item]
+            if item in into:
+                into[item] = outof[item]
+            else:
+                for item2 in sorted(into):
+                    if re.search(item, item2):
+                        into[item2] = outof[item]
 
-merge_dicts(functions, functions_addon)
+from metadata.functions_addon import functions_codegen_method
+from metadata.functions_addon import functions_enums
+from metadata.functions_addon import functions_params_types
+
+merge_dicts(functions, functions_codegen_method)
+merge_dicts(functions, functions_enums)
+merge_dicts(functions, functions_params_types)
 
 __version__ = config['module_version']
--- src/nidmm/metadata/functions_addon.py
+++ src/nidmm/metadata/functions_addon.py
@@ -1,15 +1,49 @@
-functions_addon = {
-    'InitWithOptions': {
-        'codegen_method': 'private',
-    },
-    'Initiate': {
-        'codegen_method': 'private',
-    },
-    'close': {
-        'codegen_method': 'private',
-    },
-    'Abort': {
-        'codegen_method': 'private',
-    },
+functions_codegen_method = {
+    'InitWithOptions':  { 'codegen_method': 'private',  },
+    'Initiate':         { 'codegen_method': 'private',  },
+    'close':            { 'codegen_method': 'private',  },
+    'Abort':            { 'codegen_method': 'private',  },
+    'CheckAttribute.+': { 'codegen_method': 'no',       },
+    'SetAttribute.+':   { 'codegen_method': 'private',  },
+    'GetAttribute.+':   { 'codegen_method': 'private',  },
+    'init':             { 'codegen_method': 'no',       },
+    'GetError':         { 'codegen_method': 'private',  },
+    'GetErrorMessage':  { 'codegen_method': 'private',  },
+    'ClearError':       { 'codegen_method': 'private',  },
+    'Control':          { 'codegen_method': 'no',       },
+    'LockSession':      { 'codegen_method': 'private',  },
+    'UnlockSession':    { 'codegen_method': 'private',  },
+    '.+ExtCal':         { 'codegen_method': 'no',       },
+    'CalAdjust.+':      { 'codegen_method': 'no',       },
+    '.+UserDefined.+':  { 'codegen_method': 'no',       },
+    'SetCalPassword':   { 'codegen_method': 'no',       },
+}
+
+functions_enums = {
+    'ConfigureTrigger':             { 'parameters': { 1: { 'enum': 'Terminal',                  }, }, },
+    'ConfigureMeasCompleteDest':    { 'parameters': { 1: { 'enum': 'Terminal',                  }, }, },
+    'ConfigureMultiPoint':          { 'parameters': { 3: { 'enum': 'Terminal',                  }, }, },
+    'ConfigureTriggerSlope':        { 'parameters': { 1: { 'enum': 'Slope',                     }, }, },
+    'GetApertureTimeInfo':          { 'parameters': { 2: { 'enum': 'ApertureTimeUnits',         }, }, },
+    'ConfigureAutoZeroMode':        { 'parameters': { 1: { 'enum': 'EnabledSetting',            }, }, },
+    'ConfigureMeasurementDigits':   { 'parameters': { 1: { 'enum': 'Function',                  }, }, },
+    'ConfigureMeasurementAbsolute': { 'parameters': { 1: { 'enum': 'Function',                  }, }, },
+    'ConfigureMeasCompleteSlope':   { 'parameters': { 1: { 'enum': 'Slope',                     }, }, },
+    'ConfigureSampleTriggerSlope':  { 'parameters': { 1: { 'enum': 'Slope',                     }, }, },
+    'ReadStatus':                   { 'parameters': { 2: { 'enum': 'AcquisitionStatus',         }, }, },
+    'ConfigureADCCalibration':      { 'parameters': { 1: { 'enum': 'EnabledSetting',            }, }, },
+    'ConfigureOffsetCompOhms':      { 'parameters': { 1: { 'enum': 'EnabledSetting',            }, }, },
+    'ConfigureCurrentSource':       { 'parameters': { 1: { 'enum': 'CurrentSource',             }, }, },
+    'ConfigureCableCompType':       { 'parameters': { 1: { 'enum': 'CableCompensationType',     }, }, },
+    'ConfigureWaveformAcquisition': { 'parameters': { 1: { 'enum': 'Function',                  }, }, },
+    'ConfigureWaveformCoupling':    { 'parameters': { 1: { 'enum': 'WaveformCouplingMode',      }, }, },
+    'ConfigureTransducerType':      { 'parameters': { 1: { 'enum': 'TemperatureTransducerType', }, }, },
+    'ConfigureThermistorType':      { 'parameters': { 1: { 'enum': 'TemperatureThermistorType', }, }, },
 
 }
+
+functions_params_types = {
+    'GetAttributeViString':         { 'parameters': { 4: { 'type': 'ViString',                  }, }, },
+    'SetAttributeViString':         { 'parameters': { 3: { 'type': 'ViString',                  }, }, },
+}
+
