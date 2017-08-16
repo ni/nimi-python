8fc7cae6f74f244f210223b404dd16ebcebd2883
Jaleel <mailforjaleel@gmail.com>
2017-08-16 10:28:00 -0500

    added more system tests

--- src/nidmm/system_tests/test_system_nidmm.py
+++ src/nidmm/system_tests/test_system_nidmm.py
@@ -171,3 +171,20 @@ def test_get_dev_temp(device_info):
         temperature = session.get_dev_temp('')
         print(temperature)
         assert 20 <= temperature <= 50
+
+        
+def test_method_with_noinput_nooutput(device_info):
+    with nidmm.Session(device_info['name']) as session:
+        assert session.reset_with_defaults() == None
+        
+        
+def test_method_with_ViBoolean_output_type_method(device_info):
+    with nidmm.Session(device_info['name']) as session:
+        assert session.get_self_cal_supported() in [True, False]
+      
+     
+def test_method_with_enum_output_type_method(device_info):
+    with nidmm.Session(device_info['name']) as session:
+        #will have to update after https://github.com/ni/nimi-python/issues/128 fixed
+        assert session.read_status()[1] == 4        
+        
