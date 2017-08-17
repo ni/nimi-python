188c09d6cc79187ec140bd8ce85293bce6e03e9e
Mark Silva <mark.silva@ni.com>
2017-08-17 11:52:06 -0500

    Instantiating a Visa type returns an instance of the underlying intrinsic type
    Exception is ViBoolean. Underlying intrinsic type is int (can't subclass bool) but instance is bool()

--- build/templates/python_types.py
+++ build/templates/python_types.py
@@ -1,53 +1,53 @@
 
 
 class ViStatus(int):
-    def python_type(self):
-        return 'integer'
+    def __new__(cls, val=0):
+        return int(val)
 
 
 class ViRsrc(str):
-    def python_type(self):
-        return 'string'
+    def __new__(cls, val=0):
+        return str(val)
 
 
 class ViSession(int):
-    def python_type(self):
-        return 'integer'
+    def __new__(cls, val=0):
+        return int(val)
 
 
 class ViChar(int):
-    def python_type(self):
-        return 'integer'
+    def __new__(cls, val=0):
+        return int(val)
 
 
 class ViUInt32(int):
-    def python_type(self):
-        return 'integer'
+    def __new__(cls, val=0):
+        return int(val)
 
 
 class ViInt32(int):
-    def python_type(self):
-        return 'integer'
+    def __new__(cls, val=0):
+        return int(val)
 
 
 class ViInt16(int):
-    def python_type(self):
-        return 'integer'
+    def __new__(cls, val=0):
+        return int(val)
 
 
 class ViUInt16(int):
-    def python_type(self):
-        return 'integer'
+    def __new__(cls, val=0):
+        return int(val)
 
 
 class ViString(str):
-    def python_type(self):
-        return 'string'
+    def __new__(cls, val=0):
+        return str(val)
 
 
 class ViAttr(int):
-    def python_type(self):
-        return 'integer'
+    def __new__(cls, val=0):
+        return int(val)
 
 
 class ViConstString(ViString):
@@ -55,8 +55,8 @@ class ViConstString(ViString):
     def value(self):  # Makes 'value' readonly
         return super(ViConstString, ViString)
 
-    def python_type(self):
-        return 'string'
+    def __new__(cls, val=0):
+        return str(val)
 
 
 # Python's bool cannot be subclassed:
@@ -66,22 +66,16 @@ class ViBoolean(int):
     def from_param(cls, param):
         return True if bool(param) else False
 
-    def python_type(self):
-        return 'bool'
-
     def __new__(cls, val=0):
-        if val:
-            return True
-        else:
-            return False
+        return bool(val)
 
 
 class ViReal32(float):
-    def python_type(self):
-        return 'float'
+    def __new__(cls, val=0):
+        return float(val)
 
 
 class ViReal64(float):
-    def python_type(self):
-        return 'float'
+    def __new__(cls, val=0):
+        return float(val)
 
