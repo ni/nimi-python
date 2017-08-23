8ad7d81bfdf112c36f1338f1d0f0854006c3ee0f
Jaleel <mailforjaleel@gmail.com>
2017-08-23 12:22:40 -0500

    added space after commas and modified test name to have only lowercase character

--- src/nidmm/system_tests/test_system_nidmm.py
+++ src/nidmm/system_tests/test_system_nidmm.py
@@ -198,13 +198,13 @@ def test_writeonly_attribute(device_info):
 			
 			
 def test_init_with_valid_optionstring(device_info):
-    with nidmm.Session(device_info['name'], False, True, 'Simulate=1') as session:
+    with nidmm.Session(device_info['name'], False, True, 'Simulate = 1') as session:
         assert session.simulate == True
 
 
 def test_init_with_invalid_optionstring(device_info):
     try:
-        with nidmm.Session(device_info['name'], False, True, 'Invalidstring=1') as session:
+        with nidmm.Session(device_info['name'], False, True, 'Invalidstring = 1') as session:
             assert False
     except nidmm.Error as e:
         assert e.code == -1074134965 # Error : The option string parameter contains an entry with an unknown option name.
