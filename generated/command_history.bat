13dc7e987d413c6e9293d936e1a361467d516f01
Mark Silva <mark.silva@ni.com>
2017-08-26 21:51:50 -0500

    Update generated files

--- generated/command_history.bat
+++ generated/command_history.bat
@@ -87,3 +87,6 @@ cp /mnt/d/GitHub/nimi-python/tox.ini /mnt/d/GitHub/nimi-python/bin/nidmm/tox.ini
 cd /mnt/d/GitHub/nimi-python/bin/nidmm && tox -e flake8
 cp /mnt/d/GitHub/nimi-python/tox.ini /mnt/d/GitHub/nimi-python/bin/nimodinst/tox.ini
 cd /mnt/d/GitHub/nimi-python/bin/nimodinst && tox -e flake8
+cd /mnt/d/GitHub/nimi-python/bin/nifake && set DRIVER=nifake && tox
+cd /mnt/d/GitHub/nimi-python/bin/nidmm && set DRIVER=nidmm && tox
+cd /mnt/d/GitHub/nimi-python/bin/nimodinst && set DRIVER=nimodinst && tox
