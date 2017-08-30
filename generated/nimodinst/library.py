14e5e4e05b0819a562a89fe40e0f5534168685cb
Marcos Kirsch <github@kirsch.com.mx>
2017-08-30 16:56:45 -0500

    Update generated files

--- generated/nimodinst/library.py
+++ generated/nimodinst/library.py
@@ -1,40 +1,69 @@
 # This file was generated
 
-import platform
+import ctypes
+import threading
 
-from nimodinst import ctypes_library
-from nimodinst import errors
+from nimodinst.ctypes_types import *  # noqa: F403,H303
+import nimodinst.python_types
 
 
-class LibrarySingleton(object):
+class Library(object):
+    """
+    Wrapper around driver library.
+    Class will setup the correct ctypes information for every function on first call.
+    """
 
-    _instance = None
+    def __init__(self, library_name, library_type):
+        self._func_lock = threading.Lock()
+        # We cache the cfunc object from the ctypes.CDLL object
+        self.niModInst_CloseInstalledDevicesSession_cfunc = None
+        self.niModInst_GetExtendedErrorInfo_cfunc = None
+        self.niModInst_GetInstalledDeviceAttributeViInt32_cfunc = None
+        self.niModInst_GetInstalledDeviceAttributeViString_cfunc = None
+        self.niModInst_OpenInstalledDevicesSession_cfunc = None
 
-    def _get_library_name():
-        try:
-            return {'Linux': {'64bit': {'name': 'libnimodinst.so', 'type': 'cdll'}},
-                    'Windows': {'32bit': {'name': 'nimodinst.dll', 'type': 'windll'},
-                                '64bit': {'name': 'nimodinst_64.dll', 'type': 'cdll'}}}[platform.system()][platform.architecture()[0]]['name']
-        except KeyError:
-            raise errors.UnsupportedConfigurationError
+        if library_type == 'windll':
+            self._library = ctypes.WinDLL(library_name)
+        else:
+            assert library_type == 'cdll'
+            self._library = ctypes.CDLL(library_name)
 
-    def _get_library_type():
-        try:
-            return {'Linux': {'64bit': {'name': 'libnimodinst.so', 'type': 'cdll'}},
-                    'Windows': {'32bit': {'name': 'nimodinst.dll', 'type': 'windll'},
-                                '64bit': {'name': 'nimodinst_64.dll', 'type': 'cdll'}}}[platform.system()][platform.architecture()[0]]['type']
-        except KeyError:
-            raise errors.UnsupportedConfigurationError
+    def niModInst_CloseInstalledDevicesSession(self, handle):  # noqa: N802
+        with self._func_lock:
+            if self.niModInst_CloseInstalledDevicesSession_cfunc is None:
+                self.niModInst_CloseInstalledDevicesSession_cfunc = self._library.niModInst_CloseInstalledDevicesSession
+                self.niModInst_CloseInstalledDevicesSession_cfunc.argtypes = [ViSession_ctype]  # noqa: F405
+                self.niModInst_CloseInstalledDevicesSession_cfunc.restype = nimodinst.python_types.ViStatus
+        return self.niModInst_CloseInstalledDevicesSession_cfunc(handle)
 
-    def __init__(self):
-        if LibrarySingleton._instance is None:
-            try:
-                LibrarySingleton._instance = ctypes_library.Library(LibrarySingleton._get_library_name(), LibrarySingleton._get_library_type())
-            except OSError:
-                raise errors.DriverNotInstalledError()
+    def niModInst_GetExtendedErrorInfo(self, error_info_buffer_size, error_info):  # noqa: N802
+        with self._func_lock:
+            if self.niModInst_GetExtendedErrorInfo_cfunc is None:
+                self.niModInst_GetExtendedErrorInfo_cfunc = self._library.niModInst_GetExtendedErrorInfo
+                self.niModInst_GetExtendedErrorInfo_cfunc.argtypes = [ViInt32_ctype, ViString_ctype]  # noqa: F405
+                self.niModInst_GetExtendedErrorInfo_cfunc.restype = nimodinst.python_types.ViStatus
+        return self.niModInst_GetExtendedErrorInfo_cfunc(error_info_buffer_size, error_info)
 
-        self._library = LibrarySingleton._instance
+    def niModInst_GetInstalledDeviceAttributeViInt32(self, handle, index, attribute_id, attribute_value):  # noqa: N802
+        with self._func_lock:
+            if self.niModInst_GetInstalledDeviceAttributeViInt32_cfunc is None:
+                self.niModInst_GetInstalledDeviceAttributeViInt32_cfunc = self._library.niModInst_GetInstalledDeviceAttributeViInt32
+                self.niModInst_GetInstalledDeviceAttributeViInt32_cfunc.argtypes = [ViSession_ctype, ViInt32_ctype, ViInt32_ctype, ctypes.POINTER(ViInt32_ctype)]  # noqa: F405
+                self.niModInst_GetInstalledDeviceAttributeViInt32_cfunc.restype = nimodinst.python_types.ViStatus
+        return self.niModInst_GetInstalledDeviceAttributeViInt32_cfunc(handle, index, attribute_id, attribute_value)
 
-    def get(self):
-        return self._library
+    def niModInst_GetInstalledDeviceAttributeViString(self, handle, index, attribute_id, attribute_value_buffer_size, attribute_value):  # noqa: N802
+        with self._func_lock:
+            if self.niModInst_GetInstalledDeviceAttributeViString_cfunc is None:
+                self.niModInst_GetInstalledDeviceAttributeViString_cfunc = self._library.niModInst_GetInstalledDeviceAttributeViString
+                self.niModInst_GetInstalledDeviceAttributeViString_cfunc.argtypes = [ViSession_ctype, ViInt32_ctype, ViInt32_ctype, ViInt32_ctype, ViString_ctype]  # noqa: F405
+                self.niModInst_GetInstalledDeviceAttributeViString_cfunc.restype = nimodinst.python_types.ViStatus
+        return self.niModInst_GetInstalledDeviceAttributeViString_cfunc(handle, index, attribute_id, attribute_value_buffer_size, attribute_value)
 
+    def niModInst_OpenInstalledDevicesSession(self, driver, handle, device_count):  # noqa: N802
+        with self._func_lock:
+            if self.niModInst_OpenInstalledDevicesSession_cfunc is None:
+                self.niModInst_OpenInstalledDevicesSession_cfunc = self._library.niModInst_OpenInstalledDevicesSession
+                self.niModInst_OpenInstalledDevicesSession_cfunc.argtypes = [ViConstString_ctype, ctypes.POINTER(ViSession_ctype), ctypes.POINTER(ViInt32_ctype)]  # noqa: F405
+                self.niModInst_OpenInstalledDevicesSession_cfunc.restype = nimodinst.python_types.ViStatus
+        return self.niModInst_OpenInstalledDevicesSession_cfunc(driver, handle, device_count)
---
+++ generated/nimodinst/library_singleton.py
@@ -0,0 +1,40 @@
+# This file was generated
+
+import platform
+
+from nimodinst import library
+from nimodinst import errors
+
+
+class LibrarySingleton(object):
+
+    _instance = None
+
+    def _get_library_name():
+        try:
+            return {'Linux': {'64bit': {'name': 'libnimodinst.so', 'type': 'cdll'}},
+                    'Windows': {'32bit': {'name': 'nimodinst.dll', 'type': 'windll'},
+                                '64bit': {'name': 'nimodinst_64.dll', 'type': 'cdll'}}}[platform.system()][platform.architecture()[0]]['name']
+        except KeyError:
+            raise errors.UnsupportedConfigurationError
+
+    def _get_library_type():
+        try:
+            return {'Linux': {'64bit': {'name': 'libnimodinst.so', 'type': 'cdll'}},
+                    'Windows': {'32bit': {'name': 'nimodinst.dll', 'type': 'windll'},
+                                '64bit': {'name': 'nimodinst_64.dll', 'type': 'cdll'}}}[platform.system()][platform.architecture()[0]]['type']
+        except KeyError:
+            raise errors.UnsupportedConfigurationError
+
+    def __init__(self):
+        if LibrarySingleton._instance is None:
+            try:
+                LibrarySingleton._instance = library.Library(LibrarySingleton._get_library_name(), LibrarySingleton._get_library_type())
+            except OSError:
+                raise errors.DriverNotInstalledError()
+
+        self._library = LibrarySingleton._instance
+
+    def get(self):
+        return self._library
+
