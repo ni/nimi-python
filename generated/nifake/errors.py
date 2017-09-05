7c32dc5b4d2ed092408f96eda5eb0b9a82d8b6f8
Mark Silva <mark.silva@ni.com>
2017-09-05 15:41:55 -0500

    Update generated files

--- generated/nifake/errors.py
+++ generated/nifake/errors.py
@@ -6,41 +6,41 @@ import platform
 import warnings
 
 
-def _is_success(error_code):
-    return (error_code == 0)
+def _is_success(code):
+    return (code == 0)
 
 
-def _is_error(error_code):
-    return (error_code < 0)
+def _is_error(code):
+    return (code < 0)
 
 
-def _is_warning(error_code):
-    return (error_code > 0)
+def _is_warning(code):
+    return (code > 0)
 
 
 class _ErrorBase(Exception):
 
-    def __init__(self, error_code, error_description):
+    def __init__(self, code, description):
 
-        self.code = error_code
-        self.description = error_description
+        self.code = code
+        self.description = description
         super(_ErrorBase, self).__init__(str(self.code) + ": " + self.description)
 
 
 class Error(_ErrorBase):
     '''An error originating from the NI-FAKE driver'''
 
-    def __init__(self, error_code, error_description):
-        assert (_is_error(error_code)), "Should not raise Error if error_code is not fatal."
-        super(Error, self).__init__(error_code, error_description)
+    def __init__(self, code, description):
+        assert (_is_error(code)), "Should not raise Error if code is not fatal."
+        super(Error, self).__init__(code, description)
 
 
 class NifakeWarning(Warning):
     '''A warning originating from the NI-FAKE driver'''
 
-    def __init__(self, warning_code, warning_description):
-        assert (_is_warning(warning_code)), "Should not raise Warning if error_code is not positive."
-        super(NifakeWarning, self).__init__('Warning {0} occurred.\n\n{1}'.format(warning_code, warning_description))
+    def __init__(self, code, description):
+        assert (_is_warning(code)), "Should not create Warning if code is not positive."
+        super(NifakeWarning, self).__init__('Warning {0} occurred.\n\n{1}'.format(code, description))
 
 
 class UnsupportedConfigurationError(Exception):
@@ -57,18 +57,18 @@ class DriverNotInstalledError(Exception):
         super(DriverNotInstalledError, self).__init__('The NI-FAKE runtime is not installed. Please visit http://www.ni.com/downloads/drivers/ to download and install it.')
 
 
-def handle_error(session, error_code, ignore_warnings):
-    if _is_success(error_code) or (_is_warning(error_code) and ignore_warnings):
+def handle_error(session, code, ignore_warnings):
+    if _is_success(code) or (_is_warning(code) and ignore_warnings):
         return
     try:
-        error_description = session.get_error_description(error_code)
+        description = session.get_error_description(code)
     except Exception as e:
         # TODO(marcoskirsch): Log this exception.
-        error_description = ""
-    if (_is_error(error_code)):
-        raise Error(error_code, error_description)
-    if (_is_warning(error_code)):
-        warnings.warn(NifakeWarning(error_code, error_description))
+        description = ""
+    if (_is_error(code)):
+        raise Error(code, description)
+    if (_is_warning(code)):
+        warnings.warn(NifakeWarning(code, description))
 
 
 warnings.filterwarnings("always", category=NifakeWarning)
