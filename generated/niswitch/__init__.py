92aa52e3ff155954850d52324709a63f36e03460
Frank <fladolcetta@gmail.com>
2017-09-05 17:52:05 -0500

    Updating generated

--- generated/niswitch/__init__.py
+++ generated/niswitch/__init__.py
@@ -3,5 +3,5 @@
 
 from niswitch.enums import *          # noqa: F403,F401,H303
 from niswitch.errors import Error     # noqa: F401
-from niswitch.errors import Warning   # noqa: F401
+from niswitch.errors import NiswitchWarning   # noqa: F401
 from niswitch.session import Session  # noqa: F401
