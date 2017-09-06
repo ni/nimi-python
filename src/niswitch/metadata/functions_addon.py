b4ba778593d859b541cf30d9be31ffb80fa35444
Frank <fladolcetta@gmail.com>
2017-09-06 09:59:33 -0500

    Adding issue 259 to functions_addon

--- src/niswitch/metadata/functions_addon.py
+++ src/niswitch/metadata/functions_addon.py
@@ -34,7 +34,7 @@ functions_enums = {
                                                       3: { 'enum': 'ScanAdvancedOutputConfigureScanTrigger',               }, }, },
 }
 
-# TODO(texasaggie97) can we get rid of this now that we are code generating the ivi-dance method of buffer retrieval?
+# TODO(texasaggie97) can we get rid of this now that we are code generating the ivi-dance method of buffer retrieval? Issue #259
 functions_params_types = {
     'GetAttributeViString':         { 'parameters': { 4: { 'type': 'ViString',                  }, }, },
     'GetNextInterchangeWarning':    { 'parameters': { 2: { 'type': 'ViString',                  }, }, },
