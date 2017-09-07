52b907d5df9c488ab54c26134dcd4147068e00d4
Mark Silva <mark.silva@ni.com>
2017-09-07 15:02:11 -0500

    Add common location for helper functions

---
+++ build/metadata_utilities.py
@@ -0,0 +1,28 @@
+# Useful functions for use in the metadata modules
+
+import re
+def merge_dicts(into, outof):
+    for item in sorted(outof):
+        if type(outof[item]) is dict:
+            if item in into and type(into[item] is list):
+                merge_dicts(into[item], outof[item])
+            elif item in into:
+                merge_dicts(into[item], outof[item])
+            elif type(into) is list:
+                for item2 in outof[item]:
+                    into[item][item2] = outof[item][item2]
+            else:
+                # Handle regex in addon
+                for item2 in into:
+                    if re.search(item, item2):
+                        assert type(into[item2]) is dict
+                        merge_dicts(into[item2], outof[item])
+        else:
+            if item in into:
+                into[item] = outof[item]
+            else:
+                for item2 in sorted(into):
+                    if re.search(item, item2):
+                        into[item2] = outof[item]
+
+
