be7f3be7d9b215dd2573872982773310f4a1f0f7
Mark Silva <github@markesilva.com>
2017-09-08 10:58:27 -0500

    Bug/bug238 modinst device (#274)
    * Add test for checking returned value
    
    * Remove attributes from Session
    
    * Index is required for attributes
    
    * Index is required for attributes
    
    * Index moved to session object
    
    Return Device object corresponding to given index
    
    * Add tests for new method of getting attributes
    
    * Update generated files
    
    * Update system tests for changes in where index goes
    
    * Make Device attributes read only
    
    * Unit test that device attributes are read only
    
    * Account for change in API
    
    * Update generated files
    
    * Remove system test that was added to unit test
    
    * Add tests for multiple devices
    
    * Add helper side effects functions for getting int and string attributes from multiple devices
    * Add tests that test int & string accross session[index] and using iteration
    
    * Update generated files

--- src/nimodinst/system_tests/test_system_nimodinst.py
+++ src/nimodinst/system_tests/test_system_nimodinst.py
@@ -28,7 +28,7 @@ def test_int_attribute_error_on_non_existant_device():
     with nimodinst.Session('') as session:
         device = len(session) + 1
         try:
-            session.slot_number[device]
+            session[device].slot_number
             assert False
         except nimodinst.Error as e:
             assert e.code == -250202  # NIMODINST_ERROR_INVALID_DEVICE_INDEX
@@ -39,78 +39,68 @@ def test_string_attribute_error_on_non_existant_device():
     with nimodinst.Session('') as session:
         device = len(session) + 1
         try:
-            session.slot_number[device]
+            session[device].slot_number
             assert False
         except nimodinst.Error as e:
             assert e.code == -250202  # NIMODINST_ERROR_INVALID_DEVICE_INDEX
             assert e.description.lower().find('the device index is out of the range of valid device indices for this session.') != -1
 
 
-def test_writeonly_attribute():
-    with nimodinst.Session('') as session:
-        assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
-        try:
-            session.device_name[0] = 'New Name'
-            assert False
-        except TypeError as e:
-            assert str(e).find('does not support item assignment') != -1
-
-
 def test_device_name_attribute():
     with nimodinst.Session('') as session:
         assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
-        assert isinstance(session.device_name[0], str)
-        assert len(session.device_name[0]) > 0  # device name must be at least 1 character
+        assert isinstance(session[0].device_name, str)
+        assert len(session[0].device_name) > 0  # device name must be at least 1 character
 
 
 def test_device_model_attribute():
     with nimodinst.Session('') as session:
         assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
-        assert len(session.device_model[0]) > 0
-        assert isinstance(session.device_model[0], str)
+        assert len(session[0].device_model) > 0
+        assert isinstance(session[0].device_model, str)
         pattern = r'(NI )?[A-Z]+e?-\d\d\d\d'
-        assert re.search(pattern, session.device_model[0]) is not None  # NI Model numbers are generally "NI PXIe-2532", but might also be "USB-2532"
+        assert re.search(pattern, session[0].device_model) is not None  # NI Model numbers are generally "NI PXIe-2532", but might also be "USB-2532"
 
 
 def test_serial_number_attribute():
     with nimodinst.Session('') as session:
         assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
         pattern = r'^[0-9A-F]+$'
-        assert isinstance(session.serial_number[0], str)
-        assert (len(session.serial_number[0]) == 0) | (re.search(pattern, session.serial_number[0]) is not None)  # NI Serial numbers hex unless it is simulated than it is 0
+        assert isinstance(session[0].serial_number, str)
+        assert (len(session[0].serial_number) == 0) | (re.search(pattern, session[0].serial_number) is not None)  # NI Serial numbers hex unless it is simulated than it is 0
 
 
 def test_bus_number_attribute():
     with nimodinst.Session('') as session:
         assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
-        assert isinstance(session.bus_number[0], int)
+        assert isinstance(session[0].bus_number, int)
 
 
 def test_chassis_number_attribute():
     with nimodinst.Session('') as session:
         assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
-        assert isinstance(session.chassis_number[0], int)
+        assert isinstance(session[0].chassis_number, int)
 
 
 def test_max_pciexpress_link_width_attribute():
     with nimodinst.Session('') as session:
         assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
-        assert isinstance(session.max_pciexpress_link_width[0], int)
+        assert isinstance(session[0].max_pciexpress_link_width, int)
 
 
 def test_pciexpress_link_width_attribute():
     with nimodinst.Session('') as session:
         assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
-        assert isinstance(session.pciexpress_link_width[0], int)
+        assert isinstance(session[0].pciexpress_link_width, int)
 
 
 def test_slot_number_attribute():
     with nimodinst.Session('') as session:
         assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
-        assert isinstance(session.slot_number[0], int)
+        assert isinstance(session[0].slot_number, int)
 
 
 def test_socket_number_attribute():
     with nimodinst.Session('') as session:
         assert len(session) > 0, 'Must have hardware for ModInst tests to be valid.'
-        assert isinstance(session.socket_number[0], int)
+        assert isinstance(session[0].socket_number, int)
