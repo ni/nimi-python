57d736615708b54821b91d769ed783cc355ee024
Mark Silva <mark.silva@ni.com>
2017-09-11 10:19:10 -0500

    Update generated files

--- generated/niswitch/setup.py
+++ generated/niswitch/setup.py
@@ -28,7 +28,7 @@ def read_contents(file_to_read):
 setup(
     name=pypi_name,
     zip_safe=True,
-    version='0.1.0.dev3',
+    version='0.2.0.dev1',
     description='NI-SWITCH Python API',
     long_description=read_contents('README.rst'),
     author='National Instruments',
