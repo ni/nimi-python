a148615185dfd05f9c327d63f1b4176997445dab
Frank <fladolcetta@gmail.com>
2017-09-15 13:07:38 -0500

    Fixing example to use relay action parameter like nidmm.

--- src/niswitch/examples/niswitch_relay_control.py
+++ src/niswitch/examples/niswitch_relay_control.py
@@ -7,18 +7,12 @@ import sys
 parser = argparse.ArgumentParser(description='Performs relay control with NI-SWITCH relays.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-n', '--name', default='PXI1Slot2', help='Resource name of a National Instruments Switch.')
 parser.add_argument('-rn', '--relay_name', default='k0', type=str, help='Relay Name.')
-parser.add_argument('-ra', '--relay_action', default='close', type=str, help='Relay Action. Accepts "open" and "close"')
+parser.add_argument('-ra', '--relay_action', default='OPEN_RELAY', choices=niswitch.RelayAction.__members__.keys(), type=str.upper, help='Relay Action.')
 args = parser.parse_args()
 
 try:
-    if args.relay_action == 'open':
-        action = niswitch.RelayAction.OPEN_RELAY
-    elif args.relay_action == 'close':
-        action = niswitch.RelayAction.CLOSE_RELAY
-    else:
-        raise Exception('Invalid relay action')
     with niswitch.Session(args.name) as session:
-        session.relay_control(args.relay_name, action)
+        session.relay_control(args.relay_name, niswitch.RelayAction[args.relay_action])
         print('Relay ', args.relay_name, ' has had the action ', args.relay_action, ' performed.')
 except niswitch.Error as e:
     sys.stderr.write(str(e))
