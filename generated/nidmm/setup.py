18985ff3f9f49ce2abb51fbe87803b6c51cd23c5
Mark Silva <mark.silva@ni.com>
2017-09-20 16:50:15 -0500

    Update generated files for release

--- generated/nidmm/setup.py
+++ generated/nidmm/setup.py
@@ -28,7 +28,7 @@ def read_contents(file_to_read):
 setup(
     name=pypi_name,
     zip_safe=True,
-    version='0.2.0.dev1',
+    version='0.2.0',
     description='NI-DMM Python API',
     long_description=read_contents('README.rst'),
     author='National Instruments',
