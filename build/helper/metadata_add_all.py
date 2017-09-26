f53c8441ecaeba3d573bcdd4ec047529e286fe15
Frank <fladolcetta@gmail.com>
2017-09-26 16:01:04 -0500

    Removing print_verbatim and using enum parameter

--- build/helper/metadata_add_all.py
+++ build/helper/metadata_add_all.py
@@ -124,7 +124,7 @@ def _add_library_call_name(parameter, session_name):
 def _add_default_value_name(parameter):
     '''Declaration with default value, if set'''
     if 'default_value' in parameter:
-        if 'print_verbatim' in parameter and parameter['print_verbatim'] is True:
+        if 'enum' in parameter and parameter['enum'] is not None:
             name = parameter['python_name'] + "=" + parameter['default_value']
         else:
             name = parameter['python_name'] + "=" + repr(parameter['default_value'])
