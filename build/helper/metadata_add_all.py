00db2a808049d006c90b4a0e0d83779fff9d612d
Frank <fladolcetta@gmail.com>
2017-09-26 14:43:01 -0500

    Flake8 fix

--- build/helper/metadata_add_all.py
+++ build/helper/metadata_add_all.py
@@ -124,7 +124,7 @@ def _add_library_call_name(parameter, session_name):
 def _add_default_value_name(parameter):
     '''Declaration with default value, if set'''
     if 'default_value' in parameter:
-        if 'print_verbatim' in parameter and parameter['print_verbatim'] == True:
+        if 'print_verbatim' in parameter and parameter['print_verbatim'] is True:
             name = parameter['python_name'] + "=" + parameter['default_value']
         else:
             name = parameter['python_name'] + "=" + repr(parameter['default_value'])
