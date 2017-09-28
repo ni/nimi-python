74440bd30b052f9bd51bacf219d6673ec07e6092
Frank <fladolcetta@gmail.com>
2017-09-28 11:42:57 -0500

    Adding system_test runner to make running system tests in test suites easier

---
+++ tools/run_python_system_tests.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import pip
+import time
+import json
+import zipfile
+import argparse
+import tempfile
+import urllib.request
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--driver', default='niswitch', type=str)
+args = parser.parse_args()
+
+
+# Install Tox
+print('****Installing tox to python.****')
+pip.main(['install', 'tox'])
+
+
+# Create Directory
+print('****Creating temporary directory.****')
+temp_dir = tempfile.gettempdir()
+working_directory = os.path.join(temp_dir, str(time.time()))
+if not os.path.exists(working_directory):
+    os.makedirs(working_directory)
+print(working_directory)
+
+
+# Read url and retrieve zip file link
+print('****Parsing github releases to obtain the latest zip file url.****')
+release_url = 'https://api.github.com/repos/ni/nimi-python/releases'
+with urllib.request.urlopen(release_url) as response:
+    html = response.read()
+url_data = json.loads(html.decode('ascii'))
+zip_url = url_data[0]['zipball_url']
+print(zip_url)
+
+# Download zip
+print('****Downloading latest zip file from github.****')
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
+my_zip_file = os.path.join(working_directory, 'zip.zip')
+urlretrieve(zip_url, my_zip_file)
+print('Download complete.')
+
+
+# Unzip files and get tox working directory
+print('****Unzipping Zip file.****')
+zip_folder = os.path.join(working_directory, 'zip_folder')
+zip_ref = zipfile.ZipFile(my_zip_file, 'r')
+zip_ref.extractall(zip_folder)
+zip_ref.close()
+print(zip_folder)
+
+# Run tox system tests
+print('****Running system tests in tox.****')
+tox_dir = os.path.join(zip_folder, os.listdir(zip_folder)[0])
+os.chdir(tox_dir)
+result = os.system('python -m tox -e ' + args.driver + '_system_tests')
+sys.exit(result)
