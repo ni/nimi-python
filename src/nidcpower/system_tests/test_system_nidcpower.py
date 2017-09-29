08d1bd0a3d4007457af055f187d3f00f24c3e5e5
Frank <fladolcetta@gmail.com>
2017-09-29 16:52:12 -0500

    Fixing flake8 failures on system tests

--- src/nidcpower/system_tests/test_system_nidcpower.py
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -7,5 +7,6 @@ def session():
     with nidcpower.Session('FakeDevice', False, True, 'Simulate=1, DriverSetup=Model:4143; BoardType:PXIe') as simulated_session:
         yield simulated_session
 
+
 def test_need_to_write_all():
     pass
