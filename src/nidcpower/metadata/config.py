04f34a24adc3c008a4a652ffd17dac7df1651abd
Marcos Kirsch <github@kirsch.com.mx>
2017-09-28 21:27:44 -0500

    Modify for NI-DCPower (came from NI-DMM)

--- src/nidcpower/metadata/config.py
+++ src/nidcpower/metadata/config.py
@@ -1,19 +1,19 @@
 config = {
     'metadata_version': '1.0',
-    'module_name': 'nidmm',
+    'module_name': 'nidcpower',
     'module_version': '0.3.0.dev0',
-    'c_function_prefix': 'niDMM_',
-    'driver_name': 'NI-DMM',
-    'session_class_description': 'An NI-DMM session to a National Instruments Digital Multimeter',
+    'c_function_prefix': 'niDCPower_',
+    'driver_name': 'NI-DCPower',
+    'session_class_description': 'An NI-DCPower session to a National Instruments Programmable Power Supply or Source Measure Unit.',
     'session_handle_parameter_name': 'vi',
     'library_info':
     {
         'Windows': {
-            '32bit': {'name': 'nidmm_32.dll', 'type': 'windll'},
-            '64bit': {'name': 'nidmm_64.dll', 'type': 'cdll'},
+            '32bit': {'name': 'nidcpower_32.dll', 'type': 'windll'},
+            '64bit': {'name': 'nidcpower_64.dll', 'type': 'cdll'},
         },
         'Linux': {
-            '64bit': {'name': 'libnidmm.so', 'type': 'cdll'},
+            '64bit': {'name': 'libnidcpower.so', 'type': 'cdll'},
         },
     },
     'context_manager_name': {
@@ -21,6 +21,6 @@ config = {
         'initiate': 'initiate',
         'abort': 'abort',
     },
-    'init_function': 'InitWithOptions',
+    'init_function': 'InitWithChannels',
 }
 
