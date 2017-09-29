f6a986ac1108064b9bde6f33d2c487565ebfc827
Marcos Kirsch <github@kirsch.com.mx>
2017-09-29 14:14:33 -0500

    Placeholder system tests

---
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -0,0 +1,11 @@
+import nidcpower
+import pytest
+
+
+@pytest.fixture(scope='function')
+def session():
+    with nidcpower.Session('FakeDevice', False, True, 'Simulate=1, DriverSetup=Model:4143; BoardType:PXIe') as simulated_session:
+        yield simulated_session
+
+def test_need_to_write_all():
+    pass
