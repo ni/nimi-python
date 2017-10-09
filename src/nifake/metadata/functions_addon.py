2870719d245017ef7bdb87f8a9e6174a83245005
Fladolcetta <frank.ladolcetta@ni.com>
2017-10-09 15:54:43 -0500

    Adding functions with fixed string to functions_addon

--- src/nifake/metadata/functions_addon.py
+++ src/nifake/metadata/functions_addon.py
@@ -42,6 +42,8 @@ functions_buffer_info = {
     'ReadMultiPoint':                        { 'parameters': { 3: { 'size': {'mechanism':'passed-in', 'value':'arraySize'}, }, }, },
     'GetAttributeViString':                  { 'parameters': { 4: { 'size': {'mechanism':'ivi-dance', 'value':'bufferSize'}, }, }, },
     'GetAStringWithSpecifiedMaximumSize':    { 'parameters': { 1: { 'size': {'mechanism':'passed-in', 'value':'bufferSize'}, }, }, },
+    'ReturnANumberAndAString':               { 'parameters': { 2: { 'size': {'mechanism':'fixed', 'value':256}, }, }, },
+    'GetAStringOfFixedMaximumSize':          { 'parameters': { 1: { 'size': {'mechanism':'fixed', 'value':256}, }, }, },
     'InitWithOptions':                       { 'parameters': { 0: { 'is_buffer': True, },
                                                                3: { 'is_buffer': True, }, }, },
     '.etAttribute.+':                        { 'parameters': { 1: { 'is_buffer': True, }, }, },
