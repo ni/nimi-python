49a5ff2606936436a974120f7730126458694297
injaleea <mailforjaleel@gmail.com>
2017-10-11 10:01:26 -0500

    removed tests needed enums from PR#454

--- src/nidcpower/system_tests/test_system_nidcpower.py
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -83,20 +83,7 @@ def test_measure_multiple(session):
     assert isinstance(current_measurements[1], float)
 
 
-TODO(Jaleel) : Enable once measurementTypes enums available
-def test_measure(session):
-    with nidcpower.Session('FakeDevice', '0', False, 'Simulate=1, DriverSetup=Model:4143; BoardType:PXIe') as session:
-        session.source_mode = nidcpower.SourceMode.SINGLE_POINT
-        session.output_function = nidcpower.OutputFunction.DC_VOLTAGE
-        session.voltage_level_range = 6
-        session.voltage_level = 2
-        with session.initiate():
-            reading = session.measure(nidcpower.MeasurementTypes.MEASURE_VOLTAGE)
-            assert session.query_in_compliance() is False
-        assert reading == 2
 '''
-
-
 def test_query_max_current_limit(session):
     with nidcpower.Session('FakeDevice', '0', False, 'Simulate=1, DriverSetup=Model:4143; BoardType:PXIe') as session:
         max_current_limit = session.query_max_current_limit(6)
@@ -110,7 +97,7 @@ def test_query_max_voltage_level(session):
         max_voltage_level = session.query_max_voltage_level(0.03)
         expected_max_voltage_level = 24  # for a simulated 4143 max voltage level should be 24V for 30mA current limit
         max_voltage_level_in_range = abs(max_voltage_level - expected_max_voltage_level) <= max(1e-09 * max(abs(max_voltage_level), abs(expected_max_voltage_level)), 0.0)  # https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python
-        assert max_voltage_level_in_range is True
+        assert max_voltage_level_in_range is True 
 
 
 def test_query_min_current_limit(session):
@@ -119,13 +106,3 @@ def test_query_min_current_limit(session):
         expected_min_current_limit = 0.0000001  # for a simulated 4143 min_current_limit should be 1uA for 6V voltage level
         min_current_limit_in_range = abs(min_current_limit - expected_min_current_limit) <= max(1e-09 * max(abs(min_current_limit), abs(expected_min_current_limit)), 0.0)  # https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python
         assert min_current_limit_in_range is True
-
-
-'''
-TODO(Jaleel): Enable once OutputStates enum available
-def test_query_output_state(session):
-    with nidcpower.Session('FakeDevice', '0', False, 'Simulate=1, DriverSetup=Model:4143; BoardType:PXIe') as session:
-        with session.initiate():
-            assert session.query_output_state(nidcpower.OutputStates.OUTPUT_CONSTANT_VOLTAGE) is True   # since default function is DCVolt when initiated output state for DC Volt\DC current should be True and False respectively
-            assert session.query_output_state(nidcpower.OutputStates.OUTPUT_CONSTANT_CURRENT) is False
-'''
