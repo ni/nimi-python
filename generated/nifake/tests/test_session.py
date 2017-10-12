347abf5a1593bf5ec7d351aded0ff429532dbeb4
Mark Silva <github@markesilva.com>
2017-10-12 18:21:51 -0500

    Move error handling methods to _SessionBase (#465)
    * Move error handling methods to _SessionBase
    
    * Update generated files
    
    * Add unit test for errors from rep cap object
    
    * Update generated files

--- generated/nifake/tests/test_session.py
+++ generated/nifake/tests/test_session.py
@@ -240,6 +240,22 @@ class TestSession(object):
                 assert e.code == test_error_code
                 assert e.description == test_error_desc
 
+    def test_error_with_rep_cap(self):
+        test_error_code = -42
+        test_error_desc = "The answer to the ultimate question"
+        self.patched_library.niFake_SetAttributeViReal64.side_effect = self.side_effects_helper.niFake_SetAttributeViReal64
+        self.side_effects_helper['SetAttributeViReal64']['return'] = test_error_code
+        self.patched_library.niFake_GetError.side_effect = self.side_effects_helper.niFake_GetError
+        self.side_effects_helper['GetError']['errorCode'] = test_error_code
+        self.side_effects_helper['GetError']['description'] = test_error_desc
+        with nifake.Session('dev1') as session:
+            try:
+                session['100'].read_write_double = 5.0
+                assert False
+            except nifake.Error as e:
+                assert e.code == test_error_code
+                assert e.description == test_error_desc
+
     def test_invalid_method_call_not_enough_parameters_error(self):
         self.patched_library.niFake_GetAStringWithSpecifiedMaximumSize.side_effect = self.side_effects_helper.niFake_GetAStringWithSpecifiedMaximumSize
         with nifake.Session('dev1') as session:
