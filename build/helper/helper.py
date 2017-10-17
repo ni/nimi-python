b616bada9fefbd5d83f31fb5c3eef8de94e4210a
Mark Silva <mark.silva@ni.com>
2017-10-17 17:51:22 -0500

    assert on unknown visa_type

--- build/helper/helper.py
+++ build/helper/helper.py
@@ -59,7 +59,7 @@ def get_python_type_for_visa_type(visa_type):
     if visa_type in type_map:
         return type_map[visa_type]
     else:
-        return 'int'
+        assert False, 'Unknown visa_type: {0}'.format(visa_type)
 
 
 
