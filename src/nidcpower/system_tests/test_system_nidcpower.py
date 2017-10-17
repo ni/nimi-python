52e61083b70dc25c9558cb9f620422e74371697e
Fladolcetta <frank.ladolcetta@ni.com>
2017-10-17 12:52:44 -0500

    Removing name of session as this is not needed. Removing system tests with session parameter that create their own session in thes test. Updating expected values.

--- src/nidcpower/system_tests/test_system_nidcpower.py
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.fixture(scope='function')
 def session():
-    with nidcpower.Session('FakeDevice', '', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as simulated_session:
+    with nidcpower.Session('', '', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as simulated_session:
         yield simulated_session
 
 
@@ -33,15 +33,15 @@ def test_error_message(session):
 
 def test_get_error(session):
     try:
-        session.instrument_model = 'Potato'
+        session.instrument_model = ''
         assert False
     except nidcpower.Error as e:
         assert e.code == -1074135027  # Error : Attribute is read-only.
         assert e.description.find('Attribute is read-only.') != -1
 
 
-def test_measure(session):
-    with nidcpower.Session('FakeDevice', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
+def test_measure():
+    with nidcpower.Session('', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
         session.source_mode = nidcpower.SourceMode.SINGLE_POINT
         session.output_function = nidcpower.OutputFunction.DC_VOLTAGE
         session.voltage_level_range = 6
@@ -52,16 +52,16 @@ def test_measure(session):
         assert reading == 2
 
 
-def test_query_output_state(session):
-    with nidcpower.Session('FakeDevice', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
+def test_query_output_state():
+    with nidcpower.Session('', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
         with session.initiate():
             assert session.query_output_state(nidcpower.OutputStates.OUTPUT_CONSTANT_VOLTAGE) is True   # since default function is DCVolt when initiated output state for DC Volt\DC current should be True and False respectively
             assert session.query_output_state(nidcpower.OutputStates.OUTPUT_CONSTANT_CURRENT) is False
 
 
-def test_config_aperture_time(session):
-    with nidcpower.Session('FakeDevice', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
-        expected_default_aperture_time = 0.016666666666666666
+def test_config_aperture_time():
+    with nidcpower.Session('', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
+        expected_default_aperture_time = 0.01666
         default_aperture_time = session.aperture_time
         assert session.aperture_time_units == nidcpower.ApertureTimeUnits.SECONDS
         default_aperture_time_in_range = abs(default_aperture_time - expected_default_aperture_time) <= max(1e-09 * max(abs(default_aperture_time), abs(expected_default_aperture_time)), 0.0)  # https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python
@@ -108,24 +108,24 @@ def test_measure_multiple(session):
 '''
 
 
-def test_query_max_current_limit(session):
-    with nidcpower.Session('FakeDevice', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
+def test_query_max_current_limit():
+    with nidcpower.Session('', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
         max_current_limit = session.query_max_current_limit(6)
-        expected_max_current_limit = 0.150000  # for a simulated 4162 max current limit should be 0.150000 for 6V Voltage level
+        expected_max_current_limit = 0.06  # for a simulated 4162 max current limit should be 0.06 for 6V Voltage level
         max_current_limit_in_range = abs(max_current_limit - expected_max_current_limit) <= max(1e-09 * max(abs(max_current_limit), abs(expected_max_current_limit)), 0.0)  # https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python
         assert max_current_limit_in_range is True
 
 
-def test_query_max_voltage_level(session):
-    with nidcpower.Session('FakeDevice', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
+def test_query_max_voltage_level():
+    with nidcpower.Session('', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
         max_voltage_level = session.query_max_voltage_level(0.03)
         expected_max_voltage_level = 24  # for a simulated 4162 max voltage level should be 24V for 30mA current limit
         max_voltage_level_in_range = abs(max_voltage_level - expected_max_voltage_level) <= max(1e-09 * max(abs(max_voltage_level), abs(expected_max_voltage_level)), 0.0)  # https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python
         assert max_voltage_level_in_range is True
 
 
-def test_query_min_current_limit(session):
-    with nidcpower.Session('FakeDevice', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
+def test_query_min_current_limit():
+    with nidcpower.Session('', '0', False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as session:
         min_current_limit = session.query_min_current_limit(0.03)
         expected_min_current_limit = 0.0000001  # for a simulated 4162 min_current_limit should be 1uA for 6V voltage level
         min_current_limit_in_range = abs(min_current_limit - expected_min_current_limit) <= max(1e-09 * max(abs(min_current_limit), abs(expected_min_current_limit)), 0.0)  # https://stackoverflow.com/questions/5595425/what-is-the-best-way-to-compare-floats-for-almost-equality-in-python
