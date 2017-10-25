80ff1383f0d59ed593ed5c4fa3583bb682f6a2f6
Mark Silva <mark.silva@ni.com>
2017-10-25 17:03:19 -0500

    Update obsoleted attributes

--- src/nidmm/metadata/attributes_addon.py
+++ src/nidmm/metadata/attributes_addon.py
@@ -2,6 +2,36 @@
 # Any changes to the API should be made here. attributes.py is code generated
 
 attributes_obsolete = {
+    1050203: { "codegen_method": "no" },  # NUM_CHANNELS
+    1050302: { "codegen_method": "no" },  # SPECIFIC_PREFIX
+    1050551: { "codegen_method": "no" },  # DRIVER_REVISION
+    1050503: { "codegen_method": "no" },  # SPECIFIC_DRIVER_MAJOR_VERSION
+    1050504: { "codegen_method": "no" },  # SPECIFIC_DRIVER_MINOR_VERSION
+    1050003: { "codegen_method": "no" },  # QUERY_INSTR_STATUS
+    1050503: { "codegen_method": "no" },  # DRIVER_MAJOR_VERSION
+    1050504: { "codegen_method": "no" },  # DRIVER_MINOR_VERSION
+    1050101: { "codegen_method": "no" },  # PRIMARY_ERROR
+    1050102: { "codegen_method": "no" },  # SECONDARY_ERROR
+    1050103: { "codegen_method": "no" },  # ERROR_ELABORATION
+    1050501: { "codegen_method": "no" },  # ENGINE_MAJOR_VERSION
+    1050502: { "codegen_method": "no" },  # ENGINE_MINOR_VERSION
+    1050553: { "codegen_method": "no" },  # ENGINE_REVISION
+    1050321: { "codegen_method": "no" },  # VISA_RM_SESSION
+    1050051: { "codegen_method": "no" },  # DEFER_UPDATE
+    1050052: { "codegen_method": "no" },  # RETURN_DEFERRED_VALUES
+    1050304: { "codegen_method": "no" },  # RESOURCE_DESCRIPTOR
+    1250003: { "codegen_method": "no" },  # RESOLUTION
+    1150008: { "codegen_method": "no" },  # START_TRIGGER
+    1150009: { "codegen_method": "no" },  # START_TRIG_SLOPE
+    1150011: { "codegen_method": "no" },  # SAMPLE_TRIGGER_DELAY
+    1150012: { "codegen_method": "no" },  # OLD_TRIGGER_MODEL
+    1150004: { "codegen_method": "no" },  # CHAN_NAMES
+    1150001: { "codegen_method": "no" },  # ID_QUERY_RESPONSE
+    1150005: { "codegen_method": "no" },  # AI_NUM_CHANNELS
+    1150006: { "codegen_method": "no" },  # FILTER_NOTCH
+    1150007: { "codegen_method": "no" },  # CONVER_PER_SAMPLE
+    1150013: { "codegen_method": "no" },  # AI_BUFFER_SIZE
+    1150031: { "codegen_method": "no" },  # SAMPLE_DELAY_MODE
 }
 
 
