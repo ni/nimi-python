2641de7980bc7aa9e26cb106b6dd0ae3f7120c53
Marcos Kirsch <github@kirsch.com.mx>
2017-11-01 14:07:06 -0500

    Don't hard-code DC-VOLTS, get it from enum

--- src/nidmm/examples/nidmm_measurement.py
+++ src/nidmm/examples/nidmm_measurement.py
@@ -3,9 +3,10 @@
 import argparse
 import nidmm
 
+supported_functions = list(nidmm.Function.__members__.keys())
 parser = argparse.ArgumentParser(description='Performs a single measurement using the NI-DMM API.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-n', '--name', default='PXI1Slot2', help='Resource name of a National Instruments Digital Multimeter.')
-parser.add_argument('-f', '--function', default='DC_VOLTS', choices=nidmm.Function.__members__.keys(), type=str.upper, help='Measurement function.')
+parser.add_argument('-f', '--function', default=supported_functions[0], choices=supported_functions, type=str.upper, help='Measurement function.')
 parser.add_argument('-r', '--range', default=10, type=float, help='Measurement range.')
 parser.add_argument('-d', '--digits', default=6.5, type=float, help='Digits of resolution for the measurement.')
 args = parser.parse_args()
