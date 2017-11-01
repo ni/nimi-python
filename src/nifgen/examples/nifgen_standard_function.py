e6586030d8b40b2647655180be8763d3c33dc0c9
Marcos Kirsch <github@kirsch.com.mx>
2017-11-01 15:17:48 -0500

    Slower default

--- src/nifgen/examples/nifgen_standard_function.py
+++ src/nifgen/examples/nifgen_standard_function.py
@@ -7,9 +7,9 @@ supported_waveforms = list(nifgen.Waveform.__members__.keys())[:-1]  # no suppor
 parser = argparse.ArgumentParser(description='Generates the standard function.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-n', '--name', default='PXI1Slot2', help='Resource name of a National Instruments Function Generator')
 parser.add_argument('-w', '--waveform', default=supported_waveforms[0], choices=supported_waveforms, type=str.upper, help='Standard waveform')
-parser.add_argument('-f', '--frequency', default=1e6, type=int, help='Frequency (Hz)')
-parser.add_argument('-a', '--amplitude', default=1.0, type=float, help='Amplitude (V)')
-parser.add_argument('-o', '--offset', default=0.0, type=float, help='DC Offset (Vpk-pk)')
+parser.add_argument('-f', '--frequency', default=1000, type=float, help='Frequency (Hz)')
+parser.add_argument('-a', '--amplitude', default=1.0, type=float, help='Amplitude (Vpk-pk)')
+parser.add_argument('-o', '--offset', default=0.0, type=float, help='DC Offset (V)')
 parser.add_argument('-p', '--phase', default=0.0, type=float, help='Start Phase (deg)')
 args = parser.parse_args()
 
