237e0ff5f48f9445429cef8e5e8cd8561c2144c5
Mark Silva <mark.silva@ni.com>
2017-11-05 10:54:12 -0600

    Update generated files

--- generated/nifake/__init__.py
+++ generated/nifake/__init__.py
@@ -5,3 +5,5 @@ from nifake.enums import *          # noqa: F403,F401,H303
 from nifake.errors import Error     # noqa: F401
 from nifake.errors import NifakeWarning   # noqa: F401
 from nifake.session import Session  # noqa: F401
+
+import nifake.custom_struct  # noqa: F401
