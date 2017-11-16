d9c668e7036f153e7a127e6dc18a055dd84d77e2
Jaleel <mailforjaleel@gmail.com>
2017-11-16 16:18:19 -0600

    Adding systemtests for nidmm and nidcpower (#601)
    Add nidmm and nidcpower system tests for attribute types not yet covered

--- src/nidcpower/system_tests/test_system_nidcpower.py
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -332,3 +332,9 @@ def test_get_ext_cal_last_temp(session):
 def test_get_ext_cal_recommended_interval(session):
     months = session.get_ext_cal_recommended_interval()
     assert months == 12
+
+
+def test_set_get_vi_int_64_attribute(session):
+    session['0'].active_advanced_sequence_step = 1
+    read_advanced_sequence_step = session['0'].active_advanced_sequence_step
+    assert read_advanced_sequence_step == 1
