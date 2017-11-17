2a031bfadd4d23a0e1eabe9c8bdea120482f1078
Mark Silva <github@markesilva.com>
2017-11-17 16:05:36 -0600

    Update output formatting

--- src/niscope/examples/niscope_fetch.py
+++ src/niscope/examples/niscope_fetch.py
@@ -18,14 +18,17 @@ with niscope.Session(args.name, False, False, 'Simulate=1, DriverSetup=Model:516
     session.configure_horizontal_timing(50000000, args.length, 50.0, 1, True)
     with session.initiate():
         wfm, wfm_infos = session[args.channels].fetch(1, args.length)
-    print('Number of samples acquired: {0}'.format(len(wfm)))
-    row_format = '    {:<20}: {:0.6f<20}'
+    print('Number of samples acquired: {:,}\n'.format(len(wfm)))
+    row_format_g = '    {:<20}: {:,.6g}'
+    row_format_d = '    {:<20}: {:,}'
     for i in range(len(wfm_infos)):
         wfm_info = wfm_infos[i]
-        print(row_format.format('Absolute X0', wfm_info.absolute_initial_x))
-        print(row_format.format('Relative X0', wfm_info.relative_initial_x))
-        print(row_format.format('dt', wfm_info.x_increment))
-        print(row_format.format('actual samples', wfm_info.actual_samples))
-        print(row_format.format('offset', wfm_info.offset))
-        print(row_format.format('gain', wfm_info.gain))
+        print('Waveform {0} information:'.format(i))
+        print(row_format_g.format('Absolute X0', wfm_info.absolute_initial_x))
+        print(row_format_g.format('Relative X0', wfm_info.relative_initial_x))
+        print(row_format_g.format('dt', wfm_info.x_increment))
+        print(row_format_d.format('actual samples', wfm_info.actual_samples))
+        print(row_format_g.format('offset', wfm_info.offset))
+        print(row_format_g.format('gain', wfm_info.gain))
+
 
