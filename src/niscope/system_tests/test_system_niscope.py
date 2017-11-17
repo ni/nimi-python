ea3765439fe5a8b91ab3c2e4381ce4259de3f8d9
Mark Silva <github@markesilva.com>
2017-11-17 15:23:55 -0600

    Add two simple system tests

--- src/niscope/system_tests/test_system_niscope.py
+++ src/niscope/system_tests/test_system_niscope.py
@@ -9,6 +9,27 @@ def session():
 
 
 # Basic usability tests
-def test_load_session(session):
-    pass
+def test_read(session):
+    test_voltage = 1.0
+    test_record_length = 2000
+    test_channels = '0,1'
+    test_num_channels = 2
+    session.configure_vertical(test_voltage, 0.0, 0, 1.0, True)
+    session.configure_horizontal_timing(50000000, test_record_length, 50.0, 1, True)
+    wfm, wfm_infos = session[test_channels].read(1, test_record_length)
+    assert len(wfm) == test_num_channels * test_record_length
+    assert len(wfm_infos) == test_num_channels
+
+
+def test_fetch(session):
+    test_voltage = 1.0
+    test_record_length = 2000
+    test_channels = '0,1'
+    test_num_channels = 2
+    session.configure_vertical(test_voltage, 0.0, 0, 1.0, True)
+    session.configure_horizontal_timing(50000000, test_record_length, 50.0, 1, True)
+    with session.initiate():
+        wfm, wfm_infos = session[test_channels].fetch(1, test_record_length)
+    assert len(wfm) == test_num_channels * test_record_length
+    assert len(wfm_infos) == test_num_channels
 
