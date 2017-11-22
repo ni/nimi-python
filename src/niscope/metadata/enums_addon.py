648128a050dc096266e2f808b31b2679294e7413
Fladolcetta <frank.ladolcetta@ni.com>
2017-11-22 09:00:19 -0600

    Adding prefix to all functions in enums_addon.py

--- src/niscope/metadata/enums_addon.py
+++ src/niscope/metadata/enums_addon.py
@@ -14,119 +14,119 @@ replacement_enums = {
     'VideoSignalFormat': {
         'values': [
             {
-                'name': 'NTSC',
+                'name': 'NISCOPE_VAL_NTSC',
                 'value': 1,
 'documentation': {
 'description': 'NTSC signal format supports line numbers from 1 to 525',
 },
             },
             {
-                'name': 'PAL',
+                'name': 'NISCOPE_VAL_PAL',
                 'value': 2,
 'documentation': {
 'description': 'PAL signal format supports line numbers from 1 to 625',
 },
             },
             {
-                'name': 'SECAM',
+                'name': 'NISCOPE_VAL_SECAM',
                 'value': 3,
 'documentation': {
 'description': 'SECAM signal format supports line numbers from 1 to 625',
 },
             },
             {
-                'name': 'M_PAL',
+                'name': 'NISCOPE_VAL_M_PAL',
                 'value': 1001,
 'documentation': {
 'description': 'M-PAL signal format supports line numbers from 1 to 525',
 },
             },
             {
-                'name': '_480I_59_94_FIELDS_PER_SECOND',
+                'name': 'NISCOPE_VAL__480I_59_94_FIELDS_PER_SECOND',
                 'value': 1010,
 'documentation': {
 'description': '480 lines, interlaced, 59.94 fields per second',
 },
             },
             {
-                'name': '_480I_60_FIELDS_PER_SECOND',
+                'name': 'NISCOPE_VAL__480I_60_FIELDS_PER_SECOND',
                 'value': 1011,
 'documentation': {
 'description': '480 lines, interlaced, 60 fields per second',
 },
             },
             {
-                'name': '_480P_59_94_FRAMES_PER_SECOND',
+                'name': 'NISCOPE_VAL__480P_59_94_FRAMES_PER_SECOND',
                 'value': 1015,
 'documentation': {
 'description': '480 lines, progressive, 59.94 frames per second',
 },
             },
             {
-                'name': '_480P_60_FRAMES_PER_SECOND',
+                'name': 'NISCOPE_VAL__480P_60_FRAMES_PER_SECOND',
                 'value': 1016,
 'documentation': {
 'description': '480 lines, progressive,60 frames per second',
 },
             },
             {
-                'name': '_576I_50_FIELDS_PER_SECOND',
+                'name': 'NISCOPE_VAL__576I_50_FIELDS_PER_SECOND',
                 'value': 1020,
 'documentation': {
 'description': '576 lines, interlaced, 50 fields per second',
 },
             },
             {
-                'name': '_576P_50_FRAMES_PER_SECOND',
+                'name': 'NISCOPE_VAL__576P_50_FRAMES_PER_SECOND',
                 'value': 1025,
 'documentation': {
 'description': '576 lines, progressive, 50 frames per second',
 },
             },
             {
-                'name': '_720P_50_FRAMES_PER_SECOND',
+                'name': 'NISCOPE_VAL__720P_50_FRAMES_PER_SECOND',
                 'value': 1031,
 'documentation': {
 'description': '720 lines, progressive, 50 frames per second',
 },
             },
             {
-                'name': '_720P_59_94_FRAMES_PER_SECOND',
+                'name': 'NISCOPE_VAL__720P_59_94_FRAMES_PER_SECOND',
                 'value': 1032,
 'documentation': {
 'description': '720 lines, progressive, 59.94 frames per second',
 },
             },
             {
-                'name': '_720P_60_FRAMES_PER_SECOND',
+                'name': 'NISCOPE_VAL__720P_60_FRAMES_PER_SECOND',
                 'value': 1033,
 'documentation': {
 'description': '720 lines, progressive, 60 frames per second',
 },
             },
             {
-                'name': '_1080I_50_FIELDS_PER_SECOND',
+                'name': 'NISCOPE_VAL__1080I_50_FIELDS_PER_SECOND',
                 'value': 1040,
 'documentation': {
 'description': '1,080 lines, interlaced, 50 fields per second',
 },
             },
             {
-                'name': '_1080I_59_94_FIELDS_PER_SECOND',
+                'name': 'NISCOPE_VAL__1080I_59_94_FIELDS_PER_SECOND',
                 'value': 1041,
 'documentation': {
 'description': '1,080 lines, interlaced, 59.94 fields per second',
 },
             },
             {
-                'name': '_1080I_60_FIELDS_PER_SECOND',
+                'name': 'NISCOPE_VAL__1080I_60_FIELDS_PER_SECOND',
                 'value': 1042,
 'documentation': {
 'description': '1,080 lines, interlaced, 60 fields per second',
 },
             },
             {
-                'name': '_1080P_24_FRAMES_PER_SECOND',
+                'name': 'NISCOPE_VAL__1080P_24_FRAMES_PER_SECOND',
                 'value': 1045,
 'documentation': {
 'description': '1,080 lines, progressive, 24 frames per second',
@@ -137,35 +137,35 @@ replacement_enums = {
     'TriggerCoupling': {
         'values': [
             {
-                'name': 'AC',
+                'name': 'NISCOPE_VAL_AC',
                 'value': 0,
 'documentation': {
 'description': 'AC coupling',
 },
             },
             {
-                'name': 'DC',
+                'name': 'NISCOPE_VAL_DC',
                 'value': 1,
 'documentation': {
 'description': 'DC coupling',
 },
             },
             {
-                'name': 'HF_REJECT',
+                'name': 'NISCOPE_VAL_HF_REJECT',
                 'value': 2,
 'documentation': {
 'description': 'Highpass filter coupling',
 },
             },
             {
-                'name': 'LF_REJECT',
+                'name': 'NISCOPE_VAL_LF_REJECT',
                 'value': 3,
 'documentation': {
 'description': 'Lowpass filter coupling',
 },
             },
             {
-                'name': 'AC_PLUS_HF_REJECT',
+                'name': 'NISCOPE_VAL_AC_PLUS_HF_REJECT',
                 'value': 1001,
 'documentation': {
 'description': 'Highpass and lowpass filter coupling',
