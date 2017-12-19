88db4d88c0b06df34ffbd0bcafa6182a34b8f0cb
Mark Silva <github@markesilva.com>
2017-12-19 09:58:50 -0600

    Remove reference to actual_count

--- src/nidcpower/examples/nidcpower_measure_record.py
+++ src/nidcpower/examples/nidcpower_measure_record.py
@@ -28,7 +28,7 @@ with nidcpower.Session(resource_name=args.resource_name, channels=args.channels,
     with session.initiate():
         while samples_acquired < args.length:
             voltage_measurements, current_measurements, in_compliance = session.fetch_multiple(count=session.fetch_backlog)
-            assert len(voltage_measurements) == len(current_measurements) == len(in_compliance) == actual_count
+            assert len(voltage_measurements) == len(current_measurements) == len(in_compliance)
             for i in range(len(voltage_measurements)):
                 samples_acquired += 1
                 print(row_format.format(samples_acquired, voltage_measurements[i], current_measurements[i], in_compliance[i]))
