546055cb09aa665cb9539a262426106f97f44438
Mark Silva <github@markesilva.com>
2018-01-09 17:15:48 -0600

    Update generated files

--- generated/niscope/session.py
+++ generated/niscope/session.py
@@ -2302,6 +2302,21 @@ class _SessionBase(object):
         '''
         return self._fetch(num_samples, timeout)
 
+    def fetch_array(self, num_samples, timeout=5.0):
+        import array
+
+        vi_ctype = visatype.ViSession(self._vi)  # case 1
+        channel_list_ctype = ctypes.create_string_buffer(self._repeated_capability.encode(self._encoding))  # case 2
+        timeout_ctype = visatype.ViReal64(timeout)  # case 9
+        num_samples_ctype = visatype.ViInt32(num_samples)  # case 9
+        wfm_array = array.array("d", range(num_samples * self._actual_num_wfms()))
+        addr, count = wfm_array.buffer_info()
+        wfm_ctype = ctypes.cast(addr, ctypes.POINTER(visatype.ViReal64))
+        wfm_info_ctype = (waveform_info.struct_niScope_wfmInfo * self._actual_num_wfms())()  # case 0.4
+        error_code = self._library.niScope_Fetch(vi_ctype, channel_list_ctype, timeout_ctype, num_samples_ctype, wfm_ctype, wfm_info_ctype)
+        errors.handle_error(self, error_code, ignore_warnings=False, is_error_handling=False)
+        return wfm_array, [waveform_info.WaveformInfo(wfm_info_ctype[i]) for i in range(self._actual_num_wfms())]
+
     def fetch_measurement(self, scalar_meas_function, timeout=5.0):
         '''fetch_measurement
 
