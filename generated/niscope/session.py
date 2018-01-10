be7e97b14df1464f95d6d4ef4925b2c0c43aeb5e
Mark Silva <github@markesilva.com>
2018-01-10 12:35:07 -0600

    Update generated files

--- generated/niscope/session.py
+++ generated/niscope/session.py
@@ -2318,7 +2318,7 @@ class _SessionBase(object):
         channel_list_ctype = ctypes.create_string_buffer(self._repeated_capability.encode(self._encoding))  # case 2
         timeout_ctype = visatype.ViReal64(timeout)  # case 9
         num_samples_ctype = visatype.ViInt32(num_samples)  # case 9
-        wfm_array = array.array("d", range(num_samples * self._actual_num_wfms()))
+        wfm_array = array.array("d", [0]) * (num_samples * self._actual_num_wfms())
         wfm_info_ctype = (waveform_info.struct_niScope_wfmInfo * self._actual_num_wfms())()  # case 0.4
         error_code = self._library.niScope_Fetch(vi_ctype, channel_list_ctype, timeout_ctype, num_samples_ctype, ctypes.cast(wfm_array.buffer_info()[0], ctypes.POINTER(visatype.ViReal64)), wfm_info_ctype)
         errors.handle_error(self, error_code, ignore_warnings=False, is_error_handling=False)
