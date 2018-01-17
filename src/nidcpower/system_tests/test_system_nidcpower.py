fbb177dddf587ada675828c243b91ad9e317f4d4
Mark Silva <github@markesilva.com>
2018-01-17 17:46:37 -0600

    Fix DCPower tests

--- src/nidcpower/system_tests/test_system_nidcpower.py
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -1,3 +1,4 @@
+import datetime
 import nidcpower
 import pytest
 
@@ -224,7 +225,7 @@ def test_wait_for_event_default_timeout(single_channel_session):
 
 def test_wait_for_event_with_timeout(single_channel_session):
     with single_channel_session.initiate():
-        single_channel_session.wait_for_event(nidcpower.Event.SOURCE_COMPLETE, 0.5)
+        single_channel_session.wait_for_event(nidcpower.Event.SOURCE_COMPLETE, datetime.timedelta(seconds=0.5))
 
 
 def test_commit(single_channel_session):
