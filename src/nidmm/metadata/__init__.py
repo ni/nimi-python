8f4fcc0db7b371da78154d5ec7f1347041cec938
Mark Silva <github@markesilva.com>
2018-02-02 18:15:58 -0600

    Add driver specific documentation to supplement option string documentation

--- src/nidmm/metadata/__init__.py
+++ src/nidmm/metadata/__init__.py
@@ -13,6 +13,39 @@ import sys
 config['modules'] = sys.modules
 helper.add_all_metadata(functions, attributes, enums, config)
 
+if 'note' not in config['functions']['_init_function']['parameters'][3]:
+    config['functions']['_init_function']['parameters'][3]['note'] = []
+if not isinstance(config['functions']['_init_function']['parameters'][3]['note'], list):
+    config['functions']['_init_function']['parameters'][3]['note'] = [config['functions']['_init_function']['parameters'][3]['note']]
+
+config['functions']['_init_function']['parameters'][3]['note'].append(
+'''
+Supported Combinations:
+
++-------+---------------------+
+| Model | Board Type          |
++=======+=====================+
+| 4065  | PXI, PCI, PCIe, USB |
++-------+---------------------+
+| 4070  | PXI, PCI            |
++-------+---------------------+
+| 4071  | PXI                 |
++-------+---------------------+
+| 4072  | PXI                 |
++-------+---------------------+
+| 4080  | PXIe                |
++-------+---------------------+
+| 4081  | PXIe                |
++-------+---------------------+
+| 4082  | PXIe                |
++-------+---------------------+
+
+If the Instrument Descriptor parameter and the Driver Setup property are not specified, an
+NI PXI-4070 is simulated by default. If the Driver Setup string is not provided, and the
+Instrument Descriptor parameter specifies a valid device, the device of the Instrument
+Descriptor property is simulated. If the Driver Setup string is specified, the Instrument
+Descriptor property is ignored.
+''')
 __version__ = config['module_version']
 
 
