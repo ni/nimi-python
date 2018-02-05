852ed9b6be5d24cee2d2e264a25670da9155c938
Mark Silva <github@markesilva.com>
2018-02-05 10:52:16 -0600

    Test and show both option string and option dictionary

--- src/nidcpower/examples/nidcpower_measure_record.py
+++ src/nidcpower/examples/nidcpower_measure_record.py
@@ -5,22 +5,14 @@ import nidcpower
 import sys
 
 
-def example(argsv):
-    parser = argparse.ArgumentParser(description='Outputs the specified voltage, then takes the specified number of voltage and current readings.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-n', '--resource_name', default='PXI1Slot2', help='Resource name of a National Instruments SMU')
-    parser.add_argument('-c', '--channels', default='0', help='Channel(s) to use')
-    parser.add_argument('-l', '--length', default='20', type=int, help='Measure record length')
-    parser.add_argument('-v', '--voltage', default=5.0, type=float, help='Voltage level (V)')
-    parser.add_argument('-op', '--option_string', default='', type=str, help='Option string')
-    args = parser.parse_args(argsv)
-
-    with nidcpower.Session(resource_name=args.resource_name, channels=args.channels, options=args.option_string) as session:
+def example(resource_name, channels, options, voltage, length):
+    with nidcpower.Session(resource_name=resource_name, channels=channels, options=options) as session:
 
         # Configure the session.
-        session.measure_record_length = args.length
+        session.measure_record_length = length
         session.measure_record_length_is_finite = True
         session.measure_when = nidcpower.MeasureWhen.AUTOMATICALLY_AFTER_SOURCE_COMPLETE
-        session.voltage_level = args.voltage
+        session.voltage_level = voltage
 
         session.commit()
         print('Effective measurement rate: {0} S/s'.format(session.measure_record_delta_time / 1))
@@ -29,23 +21,39 @@ def example(argsv):
         print('  #    Voltage    Current    In Compliance')
         row_format = '{0:3d}:   {1:8.6f}   {2:8.6f}   {3}'
         with session.initiate():
-            while samples_acquired < args.length:
+            while samples_acquired < length:
                 voltage_measurements, current_measurements, in_compliance = session.fetch_multiple(count=session.fetch_backlog)
                 samples_acquired += len(voltage_measurements)
                 for i in zip(range(len(voltage_measurements)), voltage_measurements, current_measurements, in_compliance):
                     print(row_format.format(i[0], i[1], i[2], i[3]))
 
 
-def _main():
-    example(sys.argv)
+def _main(argsv):
+    parser = argparse.ArgumentParser(description='Outputs the specified voltage, then takes the specified number of voltage and current readings.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-n', '--resource_name', default='PXI1Slot2', help='Resource name of a National Instruments SMU')
+    parser.add_argument('-c', '--channels', default='0', help='Channel(s) to use')
+    parser.add_argument('-l', '--length', default='20', type=int, help='Measure record length')
+    parser.add_argument('-v', '--voltage', default=5.0, type=float, help='Voltage level (V)')
+    parser.add_argument('-op', '--option-string', default='', type=str, help='Option string')
+    args = parser.parse_args(argsv)
+    example(args.resource_name, args.channels, args.option_string, args.voltage, args.length)
+
+
+def main():
+    _main(sys.argv)
 
 
 def test_example():
-    cmd_line = ['-op', 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe', ]
-    example(cmd_line)
+    options = {'similate': True, 'driver_setup': {'Model': '4162', 'BoardType': 'PXIe', }, }
+    example('PXI1Slot2', '', options, 5.0, 20)
+
+
+def test_main():
+    cmd_line = ['--option-string', 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe', ]
+    _main(cmd_line)
 
 
 if __name__ == '__main__':
-    _main()
+    main()
 
 
--- src/nidcpower/examples/nidcpower_source_delay_measure.py
+++ src/nidcpower/examples/nidcpower_source_delay_measure.py
@@ -11,19 +11,10 @@ def print_fetched_measurements(voltage_measurements, current_measurements, in_co
     print('        In compliance: {0}'.format(in_compliance[0]))
 
 
-def example(argsv):
-    parser = argparse.ArgumentParser(description='Outputs voltage 1, waits for source delay, and then takes a measurement. Then orepeat with voltage 2.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-n', '--resource_name', default='PXI1Slot2', help='Resource name of a National Instruments SMU')
-    parser.add_argument('-c', '--channels', default='0', help='Channel(s) to use')
-    parser.add_argument('-v1', '--voltage1', default=1.0, type=float, help='Voltage level 1 (V)')
-    parser.add_argument('-v2', '--voltage2', default=2.0, type=float, help='Voltage level 2 (V)')
-    parser.add_argument('-d', '--delay', default=0.05, type=float, help='Source delay (s)')
-    parser.add_argument('-op', '--option_string', default='', type=str, help='Option string')
-    args = parser.parse_args(argsv)
+def example(resource_name, channels, options, voltage1, voltage2, delay):
+    timeout = delay + 1.0
 
-    timeout = args.delay + 1.0
-
-    with nidcpower.Session(resource_name=args.resource_name, channels=args.channels, options=args.option_string) as session:
+    with nidcpower.Session(resource_name=resource_name, channels=channels, options=options) as session:
 
         # Configure the session.
         session.source_mode = nidcpower.SourceMode.SINGLE_POINT
@@ -31,29 +22,46 @@ def example(argsv):
         session.current_limit = .06
         session.voltage_level_range = 5.0
         session.current_limit_range = .06
-        session.source_delay = args.delay
+        session.source_delay = delay
         session.measure_when = nidcpower.MeasureWhen.AUTOMATICALLY_AFTER_SOURCE_COMPLETE
-        session.voltage_level = args.voltage1
+        session.voltage_level = voltage1
 
         with session.initiate():
             print('Voltage 1:')
             print_fetched_measurements(*session.fetch_multiple(count=1, timeout=timeout))
-            session.voltage_level = args.voltage2  # on-the-fly set
+            session.voltage_level = voltage2  # on-the-fly set
             print('Voltage 2:')
             print_fetched_measurements(*session.fetch_multiple(count=1, timeout=timeout))
             session.output_enabled = False
 
 
-def _main():
-    example(sys.argv)
+def _main(argsv):
+    parser = argparse.ArgumentParser(description='Outputs voltage 1, waits for source delay, and then takes a measurement. Then orepeat with voltage 2.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-n', '--resource_name', default='PXI1Slot2', help='Resource name of a National Instruments SMU')
+    parser.add_argument('-c', '--channels', default='0', help='Channel(s) to use')
+    parser.add_argument('-v1', '--voltage1', default=1.0, type=float, help='Voltage level 1 (V)')
+    parser.add_argument('-v2', '--voltage2', default=2.0, type=float, help='Voltage level 2 (V)')
+    parser.add_argument('-d', '--delay', default=0.05, type=float, help='Source delay (s)')
+    parser.add_argument('-op', '--option-string', default='', type=str, help='Option string')
+    args = parser.parse_args(argsv)
+    example(args.resource_name, args.channels, args.option_string, args.voltage1, args.voltage2, args.delay)
+
+
+def main():
+    _main(sys.argv)
+
+
+def test_main():
+    cmd_line = ['--option-string', 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe', ]
+    _main(cmd_line)
 
 
 def test_example():
-    cmd_line = ['-op', 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe', ]
-    example(cmd_line)
+    options = {'similate': True, 'driver_setup': {'Model': '4162', 'BoardType': 'PXIe', }, }
+    example('PXI1Slot2', '', options, 1.0, 2.0, 0.5)
 
 
 if __name__ == '__main__':
-    _main()
+    main()
 
 
