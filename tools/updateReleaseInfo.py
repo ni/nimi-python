fbd6606e5dff5944f833a4d0fa6505710b32527e
Mark Silva <github@markesilva.com>
2018-02-20 13:19:07 -0600

    Fix replacement

--- tools/updateReleaseInfo.py
+++ tools/updateReleaseInfo.py
@@ -57,16 +57,16 @@ Update version when it is a dev version. I.e. X.Y.Z.devN to X.Y.Z.dev(N+1)
     if m:
         if args.release:
             logging.info('Dev version found, updating {0}.dev{1} to {0}'.format(m.group(1), int(m.group(2)), int(m.group(2)) + 1))
-            contents = module_dev_version_re.sub("'{0}'".format(m.group(1), int(m.group(2)) + 1), contents)
+            contents = module_dev_version_re.sub("'module_version': '{0}'".format(m.group(1), int(m.group(2)) + 1), contents)
         else:
             logging.info('Dev version found, updating {0}.dev{1} to {0}.dev{2}'.format(m.group(1), int(m.group(2)), int(m.group(2)) + 1))
-            contents = module_dev_version_re.sub("'{0}.dev{1}'".format(m.group(1), int(m.group(2)) + 1), contents)
+            contents = module_dev_version_re.sub("'module_version': '{0}.dev{1}'".format(m.group(1), int(m.group(2)) + 1), contents)
 
     module_version_re = re.compile("'module_version': '(\d+\.\d+\.)(\d+)'")
     m = module_version_re.search(contents)
     if m and not args.release:
         logging.info('Release version found, updating {0}{1} to {0}{2}.dev0'.format(m.group(1), int(m.group(2)), int(m.group(2)) + 1))
-        contents = module_version_re.sub("'{0}{1}.dev0'".format(m.group(1), int(m.group(2)) + 1), contents)
+        contents = module_version_re.sub("'module_version': '{0}{1}.dev0'".format(m.group(1), int(m.group(2)) + 1), contents)
 
     with open(args.src_file, 'w') as content_file:
         content_file.write(contents)
