570fcc653c806a926f6942ae78be25db1b728d96
Mark Silva <github@markesilva.com>
2018-02-20 14:03:19 -0600

    Update generated files

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = 'National Instruments'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '0.7.0.dev0'
+release = '0.8.0.dev0'
 # The short X.Y version.
 version = release[:3]
 
