9826f610ce06259ca4d2fa2d23dd7d33d1981b0a
Mark Silva <github@markesilva.com>
2018-05-03 16:23:54 -0500

    Add one system test with lock context manager

--- src/nidcpower/system_tests/test_system_nidcpower.py
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -203,6 +203,12 @@ def test_create_advanced_sequence(single_channel_session):
     single_channel_session._create_advanced_sequence(sequence_name='my_sequence', attribute_ids=ids, set_as_active_sequence=True)
 
 
+def test_lock_context_manager(single_channel_session):
+    ids = [1150008, 1250001, 1150009]  # work around #507
+    with single_channel_session.lock():
+        single_channel_session._create_advanced_sequence(sequence_name='my_sequence', attribute_ids=ids, set_as_active_sequence=True)
+
+
 # TODO(marcoskirsch): Doesn't work, issue #515
 '''
 def test_set_sequence_default_source_delays(single_channel_session):
