2cf96ccf5a50be27b60ee2f6fe7edce14f1de8e9
Mark Silva <github@markesilva.com>
2018-05-16 17:40:21 -0500

    Use correct init function

--- src/nifgen/metadata/functions_addon.py
+++ src/nifgen/metadata/functions_addon.py
@@ -73,7 +73,7 @@ functions_locking = {
     'UnlockSession':                   { 'method_templates': [ { 'session_filename': 'unlock_session_wrapper', 'documentation_filename': 'unlock_session_wrapper', 'method_python_name_suffix': '', }, ],
                                          'render_in_session_base': True,
                                          'python_name': 'unlock', },
-    'InitWithOptions':                 { 'use_session_lock': False,  },  # Session not valid during complete function call so cannot use session locking
+    'InitializeWithChannels':          { 'use_session_lock': False,  },  # Session not valid during complete function call so cannot use session locking
     'close':                           { 'use_session_lock': False,  },  # Session not valid during complete function call so cannot use session locking
 }
 
--- src/niswitch/metadata/functions_addon.py
+++ src/niswitch/metadata/functions_addon.py
@@ -35,7 +35,7 @@ functions_locking = {
     'UnlockSession':                   { 'method_templates': [ { 'session_filename': 'unlock_session_wrapper', 'documentation_filename': 'unlock_session_wrapper', 'method_python_name_suffix': '', }, ],
                                          'render_in_session_base': True,
                                          'python_name': 'unlock', },
-    'InitWithOptions':                 { 'use_session_lock': False,  },  # Session not valid during complete function call so cannot use session locking
+    'InitWithTopology':                { 'use_session_lock': False,  },  # Session not valid during complete function call so cannot use session locking
     'close':                           { 'use_session_lock': False,  },  # Session not valid during complete function call so cannot use session locking
 }
 
