63fa5aaf2381c344d3123d27e57f97370b4f0cde
Mark Silva <github@markesilva.com>
2018-05-18 14:42:37 -0500

    Create fetch forever example

--- src/niscope/examples/niscope_fetch_forever.py
+++ src/niscope/examples/niscope_fetch_forever.py
@@ -1,38 +1,42 @@
 #!/usr/bin/python
 
+import datetime
 import argparse
 import niscope
+import numpy as np
 import pprint
 import sys
+
 
 pp = pprint.PrettyPrinter(indent=4, width=80)
 
 
-def example(resource_name, channels, options, length, voltage):
-    sample_rate = 1000000
-    chunk_size = 500000
-    sample_time = 100  # in seconds
-    total_samples = sample_time * sample_rate
-    print('1. Opening session')
-    with niscope.Session("5122") as session:
-        print('2. Configuring')
-        session.configure_horizontal_timing(min_sample_rate=sample_rate, min_num_pts=1, ref_position=0.0, num_records=1, enforce_realtime=True)
+# This example is based on the C FetchForever example
+def example(resource_name, channels, options, total_acquisition_time_in_seconds, voltage, sample_rate_in_hz, samples_per_fetch):
+    total_samples = int(total_acquisition_time_in_seconds * sample_rate_in_hz)
+    channel_list = channels.split(',')  # We need channels as a list
+
+    print('1. Creating numpy arrays')
+    waveforms = [np.ndarray(total_samples, dtype=np.float64) for c in channel_list]
+
+    print('2. Opening session')
+    with niscope.Session(resource_name) as session:
+        print('3. Configuring')
+        session.configure_horizontal_timing(min_sample_rate=sample_rate_in_hz, min_num_pts=1, ref_position=0.0, num_records=1, enforce_realtime=True)
+        session.channels[channel_list].configure_vertical(voltage, coupling=niscope.VerticalCoupling.DC, enabled=True)
+        # We configure for a software trigger and then never send it
         session.configure_trigger_software()
-        session.channels['0,1'].configure_vertical(5.0, coupling=niscope.VerticalCoupling.DC, enabled=True)
-        print('3. Creating arrays')
-        wfm0 = np.ndarray(total_samples, dtype=np.float64)
-        wfm1 = np.ndarray(total_samples, dtype=np.float64)
         current_pos = 0
         print('4. initating')
         with session.initiate():
             while current_pos < total_samples:
                 # We fetch each channel at a time so we don't have to de-interleave afterwards
-                # If it takes more than one second to fetch, we are in trouble
-                print('5.0 fetching [{}:{}]'.format(current_pos, chunk_size))
-                session.channels[0].fetch_into(wfm0[current_pos:chunk_size], relative_to=niscope.FetchRelativeTo.READ_POINTER, offset=0, record_number=0, num_records=1, timeout=datetime.timedelta(seconds=1.0))
-                print('5.1 fetching [{}:{}]'.format(current_pos, chunk_size))
-                session.channels[1].fetch_into(wfm1[current_pos:chunk_size], relative_to=niscope.FetchRelativeTo.READ_POINTER, offset=0, record_number=0, num_records=1, timeout=datetime.timedelta(seconds=1.0))
-                current_pos += chunk_size
+                # We do not keep the wfm_info returned from fetch_into
+                for c, wfm in zip(channel_list, waveforms):
+                    print('5.{} fetching [{}:{}]'.format(c, current_pos, samples_per_fetch))
+                    session.channels[c].fetch_into(wfm[current_pos:samples_per_fetch], relative_to=niscope.FetchRelativeTo.READ_POINTER, 
+                                                 offset=0, record_number=0, num_records=1, timeout=datetime.timedelta(seconds=5.0))
+                current_pos += samples_per_fetch
 
         print('6 fetch complete')
     print('7 session closed')
@@ -42,11 +46,13 @@ def _main(argsv):
     parser = argparse.ArgumentParser(description='Acquires one record from the given channels.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-n', '--resource-name', default='PXI1Slot2', help='Resource name of a National Instruments Digitizer')
     parser.add_argument('-c', '--channels', default='0', help='Channel(s) to use')
-    parser.add_argument('-l', '--length', default=1000, type=int, help='Measure record length')
+    parser.add_argument('-t', '--time', default=100, type=int, help='Time to sample (s)')
     parser.add_argument('-v', '--voltage', default=1.0, type=float, help='Voltage range (V)')
     parser.add_argument('-op', '--option-string', default='', type=str, help='Option string')
+    parser.add_argument('-r', '--sample-rate', default=10000000.0, type=float, help='Sample Rate (Hz)')
+    parser.add_argument('-s', '--samples-per-fetch', default=1000000, type=int, help='Samples per fetch')
     args = parser.parse_args(argsv)
-    example(args.resource_name, args.channels, args.option_string, args.length, args.voltage)
+    example(args.resource_name, args.channels, args.option_string, args.time, args.voltage, args.sample_rate, args.chunk_size)
 
 
 def main():
@@ -55,7 +61,7 @@ def main():
 
 def test_example():
     options = {'simulate': True, 'driver_setup': {'Model': '5164', 'BoardType': 'PXIe', }, }
-    example('PXI1Slot2', '0', options, 1000, 1.0)
+    example('PXI1Slot2', '0', options, 1000, 1.0, 10000000.0, 1000000)
 
 
 def test_main():
