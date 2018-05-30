76fa7e365476fc217e387d8ed25c9745cf171c9e
Mark Silva <github@markesilva.com>
2018-05-30 09:51:19 -0500

    Update generated files

--- generated/nidmm/session.py
+++ generated/nidmm/session.py
@@ -204,7 +204,7 @@ class _SessionBase(object):
     Some cases exist where the end-user must specify instrument driver options  at initialization time.  An example of this is specifying a particular  instrument model from among a family of instruments that the driver supports.   This is useful when using simulation.  The end-user can specify  driver-specific options through the DriverSetup keyword in the optionsString  parameter to the niDMM Init With Options.vi.
     If the user does not specify a Driver Setup string, this property returns  an empty string.
     '''
-    freq_voltage_autorange = _attributes.AttributeViReal64(1150044)
+    freq_voltage_auto_range = _attributes.AttributeViReal64(1150044)
     '''Type: float
 
     For the NI 4070/4071/4072 only, specifies the value of the frequency voltage range.  If Auto Ranging, shows the actual value of the active frequency voltage range.  If not Auto Ranging, the value of this property is the same as that of  freq_voltage_range.
