69753d83c869be342a5e66f80a5dcee5e815bd49
Fladolcetta <frank.ladolcetta@ni.com>
2018-08-15 13:34:33 -0500

    Updating generated

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = 'National Instruments'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '0.9.0.dev0'
+release = '1.0.1.dev0'
 # The short X.Y version.
 version = release[:3]
 
