59ae74c8b350bd9d3f0d97a1b5a440d92549afb6
Mark Silva <github@markesilva.com>
2018-10-11 14:36:45 -0500

    Clean up and fix formatting

--- src/niscope/custom_types/waveform_info.py
+++ src/niscope/custom_types/waveform_info.py
@@ -72,26 +72,26 @@ class WaveformInfo(object):
             self.actual_samples, self.offset, self.gain)
 
     def __str__(self):
-        row_format_g = '{:<20}: {:,.6g}'
-        row_format_d = '{:<20}: {:,}'
-        row_format_s = '{:<20}: {:}'
+        row_format_g = '{:<20}: {:,.6g}\n'
+        row_format_d = '{:<20}: {:,}\n'
+        row_format_s = '{:<20}: {:}\n'
         string_representation = ''
         try:
-            string_representation += row_format_s.format('channel', self.channel) + '\n'
-            string_representation += row_format_d.format('record', self.record) + '\n'
+            string_representation += row_format_s.format('channel', self.channel)
+            string_representation += row_format_d.format('record', self.record)
         except AttributeError:
             pass
-        string_representation += row_format_g.format('Absolute X0', self.absolute_initial_x) + '\n'
-        string_representation += row_format_g.format('Relative X0', self.relative_initial_x) + '\n'
-        string_representation += row_format_g.format('dt', self.x_increment) + '\n'
+        string_representation += row_format_g.format('Absolute X0', self.absolute_initial_x)
+        string_representation += row_format_g.format('Relative X0', self.relative_initial_x)
+        string_representation += row_format_g.format('dt', self.x_increment)
         try:
-            string_representation += row_format_d.format('actual samples', self.actual_samples) + '\n'
+            string_representation += row_format_d.format('actual samples', self.actual_samples)
         except AttributeError:
             pass
-        string_representation += row_format_g.format('offset', self.offset) + '\n'
-        string_representation += row_format_g.format('gain', self.gain) + '\n'
+        string_representation += row_format_g.format('offset', self.offset)
+        string_representation += row_format_g.format('gain', self.gain)
         try:
-            string_representation += row_format_g.format('wfm length', len(self.samples)) + '\n'
+            string_representation += row_format_g.format('wfm length', len(self.samples))
         except AttributeError:
             pass
         return string_representation
