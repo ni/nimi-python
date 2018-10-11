3a67f9898ea62f61189e3a69dd5d4a12d041d05f
Mark Silva <github@markesilva.com>
2018-10-11 14:38:50 -0500

    Fix __repr__ for when there is no actual_samples

--- src/niscope/custom_types/waveform_info.py
+++ src/niscope/custom_types/waveform_info.py
@@ -69,7 +69,7 @@ class WaveformInfo(object):
     def __repr__(self):
         return '{0}(absolute_initial_x={1}, relative_initial_x={2}, x_increment={3}, actual_samples={4}, offset={5}, gain={6})'.format(
             self.__class__.__name__, self.absolute_initial_x, self.relative_initial_x, self.x_increment,
-            self.actual_samples, self.offset, self.gain)
+            self.actual_samples if hasattr(self, 'actual_samples') else 0, self.offset, self.gain)
 
     def __str__(self):
         row_format_g = '{:<20}: {:,.6g}\n'
