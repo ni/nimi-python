43826f955ef132a2cdb09c3124e98c10637eac7c
Mark Silva <github@markesilva.com>
2018-10-15 15:57:52 -0500

    Update generated files

--- generated/niscope/waveform_info.py
+++ generated/niscope/waveform_info.py
@@ -45,7 +45,7 @@ class struct_niScope_wfmInfo(ctypes.Structure):  # noqa N801
 
 class WaveformInfo(object):
     def __init__(self, data=None, absolute_initial_x=0.0, relative_initial_x=0.0,
-                 x_increment=0.0, actual_samples=0, offset=0.0, gain=0.0,
+                 x_increment=0.0, actual_samples=None, offset=0.0, gain=0.0,
                  reserved1=0.0, reserved2=0.0):
         if data is not None:
             self.absolute_initial_x = data.absolute_initial_x
@@ -66,34 +66,47 @@ class WaveformInfo(object):
             self.reserved1 = reserved1
             self.reserved2 = reserved2
 
+        # Additional fields added during fetch or fetch_into
+        self.channel = None
+        self.record = None
+        self.samples = None
+
     def __repr__(self):
-        return '{0}(absolute_initial_x={1}, relative_initial_x={2}, x_increment={3}, actual_samples={4}, offset={5}, gain={6})'.format(
-            self.__class__.__name__, self.absolute_initial_x, self.relative_initial_x, self.x_increment,
-            self.actual_samples if hasattr(self, 'actual_samples') else 0, self.offset, self.gain)
+        parameter_list = [
+            'absolute_initial_x={}'.format(self.absolute_initial_x),
+            'relative_initial_x={}'.format(self.relative_initial_x),
+            'x_increment={}'.format(self.x_increment),
+            'offset={}'.format(self.offset),
+            'gain={}'.format(self.gain),
+        ]
+        if self.actual_samples is not None:  # We explicitly look for not None to differentiate from 0
+            parameter_list.append('actual_samples'.format(self.actual_samples))
+
+        return '{0}({1})'.format(self.__class__.__name__, ', '.join(parameter_list))
 
     def __str__(self):
+        # different format lines
         row_format_g = '{:<20}: {:,.6g}\n'
         row_format_d = '{:<20}: {:,}\n'
         row_format_s = '{:<20}: {:}\n'
+
         string_representation = ''
-        try:
+        if self.channel is not None:  # We explicitly look for not None to differentiate from empty string
             string_representation += row_format_s.format('channel', self.channel)
+        if self.record is not None:  # We explicitly look for not None to differentiate from 0
             string_representation += row_format_d.format('record', self.record)
-        except AttributeError:
-            pass
+
         string_representation += row_format_g.format('Absolute X0', self.absolute_initial_x)
         string_representation += row_format_g.format('Relative X0', self.relative_initial_x)
         string_representation += row_format_g.format('dt', self.x_increment)
-        try:
+        if self.actual_samples is not None:  # We explicitly look for not None to differentiate from 0
             string_representation += row_format_d.format('actual samples', self.actual_samples)
-        except AttributeError:
-            pass
+
         string_representation += row_format_g.format('offset', self.offset)
         string_representation += row_format_g.format('gain', self.gain)
-        try:
+        if self.samples is not None:  # We explicitly look for not None to differentiate from empty array
             string_representation += row_format_g.format('wfm length', len(self.samples))
-        except AttributeError:
-            pass
+
         return string_representation
 
 
