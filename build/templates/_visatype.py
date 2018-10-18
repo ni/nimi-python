a133a5126ca47de8bcf92059071818b5fe4c80d2
Mark Silva <github@markesilva.com>
2018-10-18 15:09:29 -0500

    Define using other visatypes same as IviVisaTypes.h

--- build/templates/_visatype.py
+++ build/templates/_visatype.py
@@ -7,9 +7,6 @@ These are aliased directly to ctypes types so can be used directly to call into 
 '''
 
 
-ViStatus = ctypes.c_long
-ViRsrc = ctypes.c_char_p
-ViSession = ctypes.c_ulong
 ViChar = ctypes.c_char
 ViInt8 = ctypes.c_int8
 ViInt16 = ctypes.c_int16
@@ -18,9 +15,14 @@ ViInt32 = ctypes.c_int32
 ViUInt32 = ctypes.c_uint32
 ViInt64 = ctypes.c_int64
 ViString = ctypes.c_char_p
-ViAttr = ctypes.c_long
-ViConstString = ViString
-ViBoolean = ctypes.c_ushort
 ViReal32 = ctypes.c_float
 ViReal64 = ctypes.c_double
+
+# Types that are based on other visatypes
+ViBoolean = ViUInt16
+ViStatus = ViInt32
+ViSession = ViUInt32
+ViAttr = ViUInt32
+ViConstString = ViString
+ViRsrc = ViString
 
