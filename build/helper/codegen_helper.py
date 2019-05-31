078ed826a094a2aa17058408631f674bd541f42a
Mark Silva <github@markesilva.com>
2019-05-31 09:33:34 -0500

    Don't renumber existing cases

--- build/helper/codegen_helper.py
+++ build/helper/codegen_helper.py
@@ -264,10 +264,10 @@ def _get_ctype_variable_definition_snippet_for_scalar(parameter, parameters, ivi
         S160. Input is size of input buffer:                                       visatype.ViInt32(0 if list is None else len(list))
         S170. Input is size of output buffer with mechanism ivi-dance, QUERY_SIZE: visatype.ViInt32()
         S180. Input is size of output buffer with mechanism ivi-dance, GET_DATA:   visatype.ViInt32(error_code)
-        S190. Input is size of output buffer with mechanism ivi-dance-with-a-twist, QUERY_SIZE: visatype.ViInt32()
-        S200. Input is size of output buffer with mechanism ivi-dance-with-a-twist, GET_DATA:   visatype.ViInt32(error_code)
-        S210. Input is size of output buffer with mechanism passed-in:             visatype.ViInt32(buffer_size)
-        S220. Output scalar or enum:                                               visatype.ViInt32()
+        S190. Input is size of output buffer with mechanism passed-in:             visatype.ViInt32(buffer_size)
+        S200. Output scalar or enum:                                               visatype.ViInt32()
+        S210. Input is size of output buffer with mechanism ivi-dance-with-a-twist, QUERY_SIZE: visatype.ViInt32()
+        S220. Input is size of output buffer with mechanism ivi-dance-with-a-twist, GET_DATA:   visatype.ViInt32(error_code)
 
     Return Value (list): each item in the list will be one line needed for the declaration of that parameter
     '''
@@ -314,10 +314,10 @@ def _get_ctype_variable_definition_snippet_for_scalar(parameter, parameters, ivi
                     assert p['direction'] == 'out'
                     assert p['size']['mechanism'] == 'ivi-dance-with-a-twist'
                 if ivi_dance_step == IviDanceStep.QUERY_SIZE:
-                    definition = '{0}.{1}(0)  # case S190'.format(module_name, parameter['ctypes_type'])
+                    definition = '{0}.{1}(0)  # case S210'.format(module_name, parameter['ctypes_type'])
                 elif ivi_dance_step == IviDanceStep.GET_DATA:
                     size_parameter_twist = find_size_parameter(corresponding_buffer_parameters[0], parameters, key='value_twist')
-                    definition = '{0}.{1}({2}.value)  # case S200'.format(module_name, parameter['ctypes_type'], size_parameter_twist['ctypes_variable_name'])
+                    definition = '{0}.{1}({2}.value)  # case S220'.format(module_name, parameter['ctypes_type'], size_parameter_twist['ctypes_variable_name'])
                 else:
                     assert False, "ivi_dance_step {0} not valid for parameter {1} with ['size']['mechanism'] == 'ivi-dance-with-a-twist'".format(ivi_dance_step, parameter['name'])
             else:
@@ -325,10 +325,10 @@ def _get_ctype_variable_definition_snippet_for_scalar(parameter, parameters, ivi
                 for p in corresponding_buffer_parameters:
                     assert p['direction'] == 'out'
                     assert p['size']['mechanism'] != 'fixed-size'
-                definition = '{0}.{1}({2})  # case S210'.format(module_name, parameter['ctypes_type'], parameter['python_name'])
+                definition = '{0}.{1}({2})  # case S190'.format(module_name, parameter['ctypes_type'], parameter['python_name'])
     else:
         assert parameter['direction'] == 'out'
-        definition = '{0}.{1}()  # case S220'.format(module_name, parameter['ctypes_type'])
+        definition = '{0}.{1}()  # case S200'.format(module_name, parameter['ctypes_type'])
 
     if definition is not None:
         definitions.append(parameter['ctypes_variable_name'] + ' = ' + definition)
