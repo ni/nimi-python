ed5513f0a10b4349a8a18dc4014524431e8378e1
Mark Silva <github@markesilva.com>
2019-07-23 17:13:05 -0500

    Test helper function

--- build/helper/documentation_snippets.py
+++ build/helper/documentation_snippets.py
@@ -147,5 +147,84 @@ def initiate_function_def_for_doc(functions, config):
     return function_def
 
 
+def test_initiate_function_def_for_doc_no_exist():
+    '''Testing for lack of syntax error - not actual documentation'''
+    functions = {}
+    config = {
+        'context_manager_name': {
+            'task': 'acquisition',
+        },
+    }
+    initiate_doc = initiate_function_def_for_doc(functions, config)
+    assert initiate_doc is None
+    return
+
+
+def test_initiate_function_def_for_doc_note_not_list():
+    '''Testing for lack of syntax error - not actual documentation'''
+    functions = {
+        'Initiate': {
+            'documentation': {
+                'description': 'test',
+                'note': 'test',
+            },
+            'python_name': '_initiate',
+        },
+    }
+    config = {
+        'context_manager_name': {
+            'task': 'acquisition',
+            'initiate_function': 'Initiate',
+            'abort_function': 'Abort',
+        },
+    }
+    initiate_doc = initiate_function_def_for_doc(functions, config)
+    assert type(initiate_doc) is dict
+    return
+
+
+def test_initiate_function_def_for_doc_note_list():
+    '''Testing for lack of syntax error - not actual documentation'''
+    functions = {
+        'Initiate': {
+            'documentation': {
+                'description': 'test',
+                'note': ['test'],
+            },
+            'python_name': '_initiate',
+        },
+    }
+    config = {
+        'context_manager_name': {
+            'task': 'acquisition',
+            'initiate_function': 'Initiate',
+            'abort_function': 'Abort',
+        },
+    }
+    initiate_doc = initiate_function_def_for_doc(functions, config)
+    assert type(initiate_doc) is dict
+    return
+
+
+def test_initiate_function_def_for_doc_no_note():
+    '''Testing for lack of syntax error - not actual documentation'''
+    functions = {
+        'Initiate': {
+            'documentation': {
+                'description': 'test',
+            },
+            'python_name': '_initiate',
+        },
+    }
+    config = {
+        'context_manager_name': {
+            'task': 'acquisition',
+            'initiate_function': 'Initiate',
+            'abort_function': 'Abort',
+        },
+    }
+    initiate_doc = initiate_function_def_for_doc(functions, config)
+    assert type(initiate_doc) is dict
+
 
 
