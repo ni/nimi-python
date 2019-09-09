eb7bacdff4de42c180b54423a7a176a1b1200d60
Mark Silva <github@markesilva.com>
2019-09-09 17:16:42 -0500

    More tests!

--- src/nitclk/unit_tests/test_nitclk.py
+++ src/nitclk/unit_tests/test_nitclk.py
@@ -97,4 +97,104 @@ class TestSession(object):
         self.patched_library.niTClk_WaitUntilDone.assert_called_once_with(_matchers.ViUInt32Matcher(len(multiple_sessions)), _matchers.ViSessionBufferMatcher(multiple_sessions), _matchers.ViReal64Matcher(min_time))
         return
 
+    # API error handling
+
+    def test_api_error(self):
+        error_string = 'Error'
+        self.patched_library.niTClk_Synchronize.side_effect = self.side_effects_helper.niTClk_Synchronize
+        self.side_effects_helper['Synchronize']['return'] = -1
+        self.patched_library.niTClk_GetExtendedErrorInfo.side_effect = self.side_effects_helper.niTClk_GetExtendedErrorInfo
+        self.side_effects_helper['GetExtendedErrorInfo']['errorString'] = error_string
+        try:
+            nitclk.synchronize(multiple_sessions, 0.42)
+        except nitclk.Error as e:
+            assert e.code == -1
+            assert e.description == error_string
+
+    def test_api_get_error_description_fails(self):
+        self.patched_library.niTClk_Synchronize.side_effect = self.side_effects_helper.niTClk_Synchronize
+        self.side_effects_helper['Synchronize']['return'] = -1
+        self.patched_library.niTClk_GetExtendedErrorInfo.side_effect = self.side_effects_helper.niTClk_GetExtendedErrorInfo
+        self.side_effects_helper['GetExtendedErrorInfo']['return'] = -2
+        try:
+            nitclk.synchronize(multiple_sessions, 0.42)
+        except nitclk.Error as e:
+            assert e.code == -1  # we want the original error code from getting the attribute.
+            assert e.description == "Failed to retrieve error description."
+
+    # SessionReference error handling
+
+    def test_session_reference_error(self):
+        session = nitclk.SessionReference(SESSION_NUM_FOR_TEST)
+        error_string = 'Error'
+        self.patched_library.niTClk_GetAttributeViReal64.side_effect = self.side_effects_helper.niTClk_GetAttributeViReal64
+        self.side_effects_helper['GetAttributeViReal64']['return'] = -1
+        self.patched_library.niTClk_GetExtendedErrorInfo.side_effect = self.side_effects_helper.niTClk_GetExtendedErrorInfo
+        self.side_effects_helper['GetExtendedErrorInfo']['errorString'] = error_string
+        try:
+            test = session.sample_clock_delay
+            print(test)  # Get rid of flake8 F841
+            assert False
+        except nitclk.Error as e:
+            assert e.code == -1
+            assert e.description == error_string
+
+    def test_session_reference_get_error_description_fails(self):
+        session = nitclk.SessionReference(SESSION_NUM_FOR_TEST)
+        self.patched_library.niTClk_GetAttributeViReal64.side_effect = self.side_effects_helper.niTClk_GetAttributeViReal64
+        self.side_effects_helper['GetAttributeViReal64']['return'] = -1
+        self.patched_library.niTClk_GetExtendedErrorInfo.side_effect = self.side_effects_helper.niTClk_GetExtendedErrorInfo
+        self.side_effects_helper['GetExtendedErrorInfo']['return'] = -2
+        try:
+            test = session.sample_clock_delay
+            print(test)  # Get rid of flake8 F841
+            assert False
+        except nitclk.Error as e:
+            assert e.code == -1  # we want the original error code from getting the attribute.
+            assert e.description == "Failed to retrieve error description."
+
+    # Session Reference
+
+    def test_set_vi_real64(self):
+        session = nitclk.SessionReference(SESSION_NUM_FOR_TEST)
+        self.patched_library.niTClk_SetAttributeViReal64.side_effect = self.side_effects_helper.niTClk_SetAttributeViReal64
+        attribute_id = 11
+        test_number = 4.2
+        session.sample_clock_delay = test_number
+        self.patched_library.niTClk_SetAttributeViReal64.assert_called_once_with(_matchers.ViSessionMatcher(SESSION_NUM_FOR_TEST), _matchers.ViStringMatcher(''), _matchers.ViAttrMatcher(attribute_id), _matchers.ViReal64Matcher(test_number))
+
+    def test_get_vi_real64(self):
+        session = nitclk.SessionReference(SESSION_NUM_FOR_TEST)
+        self.patched_library.niTClk_GetAttributeViReal64.side_effect = self.side_effects_helper.niTClk_GetAttributeViReal64
+        attribute_id = 11
+        test_number = 4.2
+        self.side_effects_helper['GetAttributeViReal64']['value'] = test_number
+        attr_float = session.sample_clock_delay
+        assert(attr_float == test_number)
+        self.patched_library.niTClk_GetAttributeViReal64.assert_called_once_with(_matchers.ViSessionMatcher(SESSION_NUM_FOR_TEST), _matchers.ViStringMatcher(''), _matchers.ViAttrMatcher(attribute_id), _matchers.ViReal64PointerMatcher())
+
+    def test_set_vi_string(self):
+        session = nitclk.SessionReference(SESSION_NUM_FOR_TEST)
+        self.patched_library.niTClk_SetAttributeViString.side_effect = self.side_effects_helper.niTClk_SetAttributeViString
+        attribute_id = 13
+        test_string = "The answer to the ultimate question is 42"
+        session.sync_pulse_sender_sync_pulse_source = test_string
+        self.patched_library.niTClk_SetAttributeViString.assert_called_once_with(_matchers.ViSessionMatcher(SESSION_NUM_FOR_TEST), _matchers.ViStringMatcher(''), _matchers.ViAttrMatcher(attribute_id), _matchers.ViStringMatcher(test_string))
+
+    def test_get_vi_string(self):
+        session = nitclk.SessionReference(SESSION_NUM_FOR_TEST)
+        self.patched_library.niTClk_GetAttributeViString.side_effect = self.side_effects_helper.niTClk_GetAttributeViString
+        attribute_id = 13
+        test_string = "The answer to the ultimate question is 42"
+        self.side_effects_helper['GetAttributeViString']['value'] = test_string
+        attr_string = session.sync_pulse_sender_sync_pulse_source
+        assert(attr_string == test_string)
+
+        from mock import call
+        calls = [call(_matchers.ViSessionMatcher(SESSION_NUM_FOR_TEST), _matchers.ViStringMatcher(''), _matchers.ViAttrMatcher(attribute_id), _matchers.ViInt32Matcher(0), None), call(_matchers.ViSessionMatcher(SESSION_NUM_FOR_TEST), _matchers.ViStringMatcher(''), _matchers.ViAttrMatcher(attribute_id), _matchers.ViInt32Matcher(len(test_string)), _matchers.ViCharBufferMatcher(len(test_string)))]
+        self.patched_library.niTClk_GetAttributeViString.assert_has_calls(calls)
+        assert self.patched_library.niTClk_GetAttributeViString.call_count == 2
+
+
+
 
