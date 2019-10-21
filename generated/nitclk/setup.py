6df70b84f1152e46058fd17b8beb6231544313f8
Mark Silva <github@markesilva.com>
2019-10-21 17:16:43 -0500

    Update nitclk generated files

--- generated/nitclk/setup.py
+++ generated/nitclk/setup.py
@@ -29,7 +29,7 @@ def read_contents(file_to_read):
 setup(
     name=pypi_name,
     zip_safe=True,
-    version='0.1.0.dev0',
+    version='0.1.0',
     description='NI-TClk Python API',
     long_description=read_contents('README.rst'),
     long_description_content_type='text/x-rst',
