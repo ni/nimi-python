88aabeabacf496f99e792f63cd21cbb354a1620c
Mark Silva <github@markesilva.com>
2019-11-06 16:08:43 -0600

    Add fancy wrapper for get_pin_results_pin_information

--- src/nidigital/metadata/functions_addon.py
+++ src/nidigital/metadata/functions_addon.py
@@ -6,6 +6,47 @@ functions_override_metadata = {
         'codegen_method': 'private',
     },
 }
+
+functions_additional_get_pin_results_pin_information = {
+    'FancyGetPinResultsPinInformation': {
+        'python_name': 'get_pin_results_pin_information',
+        'codegen_method': 'python-only',
+        'method_templates': [
+            {
+                'documentation_filename': 'default_method',
+                'method_python_name_suffix': '',
+                'session_filename': 'fancy_get_pin_results_pin_information',
+            }
+        ],
+        'documentation': {
+            'description': '\nReturns a list of named tuples (PinInfo) that <FILL IN THE BLANK HERE>\n\nFields in PinInfo:\n\n- **pin_indexes** (int)\n- **site_numbers** (int)\n- **channel_indexes** (int)\n\n',
+        },
+        'parameters': [
+            {
+                'direction': 'in',
+                'name': 'vi',
+                'type': 'ViSession'
+            },
+            {
+                'direction': 'in',
+                'name': 'channelList',
+                'type': 'ViConstString'
+            },
+            {
+                'direction': 'out',
+                'documentation': {
+                    'description': '\nList of named tuples with fields:\n\n- **pin_indexes** (int)\n- **site_numbers** (int)\n- **channel_indexes** (int)\n'
+                },
+                'name': 'pin_info',
+                'python_type': 'PinInfo',
+                'size': {
+                    'mechanism': 'python-code',
+                    'value': None
+                },
+                'type': 'ViReal64[]'
+            }
+        ],
+    },
 }
 
 
