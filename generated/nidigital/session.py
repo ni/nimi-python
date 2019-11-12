6f12d64a32b132521b56d9bbc12d38a55e3f87b5
Mark Silva <github@markesilva.com>
2019-11-12 11:13:16 -0600

    Update generated files

--- generated/nidigital/session.py
+++ generated/nidigital/session.py
@@ -2679,11 +2679,15 @@ class Session(_SessionBase):
     @ivi_synchronized
     def _fetch_capture_waveform(self, site_list, waveform_name, samples_to_read, timeout):
         # This is slightly modified codegen from the function
+        # We cannot use codegen without major modifications to the code generator
+        # This function uses two 'ivi-dance' parameters and then multiplies them together - see
+        # the (modified) line below
+        # Also, we want to return the two sized that normally wouldn't be returned
         vi_ctype = _visatype.ViSession(self._vi)  # case S110
         site_list_ctype = ctypes.create_string_buffer(site_list.encode(self._encoding))  # case C020
         waveform_name_ctype = ctypes.create_string_buffer(waveform_name.encode(self._encoding))  # case C020
         samples_to_read_ctype = _visatype.ViInt32(samples_to_read)  # case S150
-        timeout_ctype = _visatype.ViReal64(timeout)  # case S150
+        timeout_ctype = _converters.convert_timedelta_to_seconds(timeout, _visatype.ViReal64)  # case S140
         data_buffer_size_ctype = _visatype.ViInt32(0)  # case S190
         data_ctype = None  # case B610
         actual_num_waveforms_ctype = _visatype.ViInt32()  # case S220
@@ -2696,7 +2700,7 @@ class Session(_SessionBase):
         data_ctype = get_ctypes_pointer_for_buffer(value=data_array, library_type=_visatype.ViUInt32)  # case B620
         error_code = self._library.niDigital_FetchCaptureWaveformU32(vi_ctype, site_list_ctype, waveform_name_ctype, samples_to_read_ctype, timeout_ctype, data_buffer_size_ctype, data_ctype, None if actual_num_waveforms_ctype is None else (ctypes.pointer(actual_num_waveforms_ctype)), None if actual_samples_per_waveform_ctype is None else (ctypes.pointer(actual_samples_per_waveform_ctype)))
         errors.handle_error(self, error_code, ignore_warnings=False, is_error_handling=False)
-        return data_array, actual_num_waveforms_ctype.value, actual_samples_per_waveform_ctype.value
+        return data_array, actual_num_waveforms_ctype.value, actual_samples_per_waveform_ctype.value  # (modified)
 
     def fetch_capture_waveform(self, site_list, waveform_name, samples_to_read, timeout):
         '''fetch_capture_waveform
@@ -2728,8 +2732,7 @@ class Session(_SessionBase):
         import collections
         import sys
 
-        timeout_secs = _converters.convert_timedelta_to_seconds(timeout, _visatype.ViReal64)
-        data, actual_num_waveforms, actual_samples_per_waveform = self._fetch_capture_waveform(site_list, waveform_name, samples_to_read, timeout_secs)
+        data, actual_num_waveforms, actual_samples_per_waveform = self._fetch_capture_waveform(site_list, waveform_name, samples_to_read, timeout)
 
         # Get the site list
         site_list = self.get_site_results_site_numbers(site_list, enums.SiteType.CAPTURE_WAVEFORM)
