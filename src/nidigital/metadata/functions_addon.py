1ec0cf9f48763d9023e4d21fcd4c6793371945f6
Mark Silva <github@markesilva.com>
2019-11-12 09:53:29 -0600

    Add FancyFetchCaptureWaveform

--- src/nidigital/metadata/functions_addon.py
+++ src/nidigital/metadata/functions_addon.py
@@ -4,4 +4,61 @@
 functions_override_metadata = {
 }
 
-
+functions_additional_fetch_capture_waveform = {
+    'FancyFetchCaptureWaveform': {
+        'python_name': 'fetch_capture_waveform',
+        'codegen_method': 'python-only',
+        'method_templates': [
+            {
+                'documentation_filename': 'default_method',
+                'method_python_name_suffix': '',
+                'session_filename': 'fancy_fetch_capture_waveform',
+            }
+        ],
+       'documentation': {
+            'description': '\nReturns a list of named tuples (Waveform) that <FILL IN THE BLANK HERE>\n\nFields in Waveform:\n\n- **site** (int)\n- **data** (array.array of int)\n\n',
+        },
+        'parameters': [
+            {
+                'direction': 'in',
+                'name': 'vi',
+                'type': 'ViSession'
+            },
+            {
+                'direction': 'in',
+                'name': 'siteList',
+                'type': 'ViConstString'
+            },
+            {
+                'direction': 'in',
+                'name': 'waveformName',
+                'type': 'ViConstString'
+            },
+            {
+                'direction': 'in',
+                'name': 'samplesToRead',
+                'type': 'ViInt32'
+            },
+            {
+                'direction': 'in',
+                'name': 'timeout',
+                'type': 'ViReal64',
+                'python_api_converter_name': 'convert_timedelta_to_seconds',
+                'python_type': 'float or datetime.timedelta',
+            },
+            {
+                'direction': 'out',
+                'documentation': {
+                    'description': '\nList of named tuples with fields:\n\n- **site** (int)\n- **data** (array.array of int)\n'
+                },
+                'name': 'waveform',
+                'python_type': 'Waveform',
+                'size': {
+                    'mechanism': 'python-code',
+                    'value': None
+                },
+                'type': 'ViUInt32[]'
+            },
+        ],
+    },
+}
