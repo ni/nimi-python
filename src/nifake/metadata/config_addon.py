ed7bb1b6495082c95c45e5ed02dffa04e1c28870
Mark Silva <github@markesilva.com>
2019-11-19 11:03:35 -0600

    Update version after release

--- src/nifake/metadata/config_addon.py
+++ src/nifake/metadata/config_addon.py
@@ -1,4 +1,4 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
-    'module_version': '1.1.4',
+    'module_version': '1.1.5.dev0',
 }
