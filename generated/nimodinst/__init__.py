2b1fa04acddfc0a496ee5b33e50cbbff223a1be4
Shreyas Bethur <48041236+sbethur@users.noreply.github.com>
2019-11-22 16:54:35 -0600

    Post release changes for 1.1.5 (#1150)
    * Update version after release
    
    * Update generated files after release
    
    * Update CHANGELOG after release

--- generated/nimodinst/__init__.py
+++ generated/nimodinst/__init__.py
@@ -2,7 +2,7 @@
 # This file was generated
 
 
-__version__ = '1.1.5'
+__version__ = '1.1.6.dev0'
 
 from nimodinst.errors import DriverWarning  # noqa: F401
 from nimodinst.errors import Error  # noqa: F401
@@ -63,7 +63,7 @@ def get_diagnostic_information():
     info['driver']['name'] = "NI-ModInst"
     info['driver']['version'] = driver_version
     info['module']['name'] = 'nimodinst'
-    info['module']['version'] = "1.1.5"
+    info['module']['version'] = "1.1.6.dev0"
     info['python']['version'] = sys.version
     info['python']['bits'] = '64' if is_python_64bit() else '32'
     info['python']['is_venv'] = is_venv()
