cf70061d9be006cf5cda0623a36cad0ab3e5027b
Mark Silva <github@markesilva.com>
2020-01-10 14:01:08 -0600

    Backslashes

--- tools/system_tests.bat
+++ tools/system_tests.bat
@@ -1,18 +1,18 @@
 rem First we need a local nitclk wheel so let's build that
-pushd generated/nitclk
+pushd generated\nitclk
 c:\python38\python.exe setup.py bdist_wheel --universal 
 popd
 
 rem Now we run the tests
-pushd generated/niscope
+pushd generated\niscope
 c:\python38\python.exe -m tox
 popd
 
-pushd generated/nidmm
+pushd generated\nidmm
 c:\python38\python.exe -m tox
 popd
 
-pushd generated/nimodinst
+pushd generated\nimodinst
 c:\python38\python.exe -m tox
 popd
 
