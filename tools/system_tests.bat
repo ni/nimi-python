f8048f79ee3709d439f73134dd8084773d2d7a55
Mark Silva <github@markesilva.com>
2020-01-10 15:21:23 -0600

    See what environment variables are available

--- tools/system_tests.bat
+++ tools/system_tests.bat
@@ -1,3 +1,14 @@
+echo %JENKINS_URL%
+echo %ghprbSourceBranch%
+echo %GIT_BRANCH%
+echo %ghprbActualCommit%
+echo %GIT_COMMIT%
+echo %CHANGE_ID%
+echo %BRANCH_NAME%
+echo %BUILD_NUMBER%
+echo %ghprbPullId%
+echo %BUILD_URL%
+
 pushd generated\niscope
 c:\python38\python.exe -m tox -c tox-driver.ini
 popd
