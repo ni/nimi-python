1cf08aac964a6fef6405ecb3abe7b15d45e222d3
Mark Silva <github@markesilva.com>
2020-01-10 14:10:56 -0600

    Building nitclk wheel in tox now, if needed

--- tools/system_tests.bat
+++ tools/system_tests.bat
@@ -1,9 +1,3 @@
-rem First we need a local nitclk wheel so let's build that
-pushd generated\nitclk
-c:\python38\python.exe setup.py bdist_wheel --universal 
-popd
-
-rem Now we run the tests
 pushd generated\niscope
 c:\python38\python.exe -m tox
 popd
