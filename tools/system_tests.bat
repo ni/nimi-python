91a314becbc036f68f2f2b0a86256afe0f8581ac
Mark Silva <github@markesilva.com>
2020-01-10 13:58:25 -0600

    Add system_test batch file

---
+++ tools/system_tests.bat
@@ -0,0 +1,19 @@
+rem First we need a local nitclk wheel so let's build that
+pushd generated/nitclk
+c:\python38\python.exe setup.py bdist_wheel --universal 
+popd
+
+rem Now we run the tests
+pushd generated/niscope
+c:\python38\python.exe -m tox
+popd
+
+pushd generated/nidmm
+c:\python38\python.exe -m tox
+popd
+
+pushd generated/nimodinst
+c:\python38\python.exe -m tox
+popd
+
+
