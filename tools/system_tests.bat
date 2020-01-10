c6e9b86379636d5c1adf5c8a0631590560d8dbc8
Mark Silva <github@markesilva.com>
2020-01-10 14:13:46 -0600

    Use driver tox file

--- tools/system_tests.bat
+++ tools/system_tests.bat
@@ -1,13 +1,13 @@
 pushd generated\niscope
-c:\python38\python.exe -m tox
+c:\python38\python.exe -m tox -c tox-driver.ini
 popd
 
 pushd generated\nidmm
-c:\python38\python.exe -m tox
+c:\python38\python.exe -m tox -c tox-driver.ini
 popd
 
 pushd generated\nimodinst
-c:\python38\python.exe -m tox
+c:\python38\python.exe -m tox -c tox-driver.ini
 popd
 
 
