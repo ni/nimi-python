b56f7ba75061716b44b42c665c1e57a9ecf5469c
Mark Silva <github@markesilva.com>
2020-01-17 10:52:25 -0600

    Use the correct ini file name

--- tools/system_tests.bat
+++ tools/system_tests.bat
@@ -2,47 +2,47 @@
 
 echo Testing nidcpower
 pushd generated\nidcpower
-c:\python38\python.exe -m tox -c tox-driver.ini
+c:\python38\python.exe -m tox -c tox-system_tests.ini
 popd
 
 echo Testing nidigital
 pushd generated\nidigital
-c:\python38\python.exe -m tox -c tox-driver.ini
+c:\python38\python.exe -m tox -c tox-system_tests.ini
 popd
 
 echo Testing niscope
 pushd generated\niscope
-c:\python38\python.exe -m tox -c tox-driver.ini
+c:\python38\python.exe -m tox -c tox-system_tests.ini
 popd
 
 echo Testing nidmm
 pushd generated\nidmm
-c:\python38\python.exe -m tox -c tox-driver.ini
+c:\python38\python.exe -m tox -c tox-system_tests.ini
 popd
 
 echo Testing nifgen
 pushd generated\nifgen
-c:\python38\python.exe -m tox -c tox-driver.ini
+c:\python38\python.exe -m tox -c tox-system_tests.ini
 popd
 
 echo Testing niscope
 pushd generated\niscope
-c:\python38\python.exe -m tox -c tox-driver.ini
+c:\python38\python.exe -m tox -c tox-system_tests.ini
 popd
 
 echo Testing niswitch
 pushd generated\niswitch
-c:\python38\python.exe -m tox -c tox-driver.ini
+c:\python38\python.exe -m tox -c tox-system_tests.ini
 popd
 
 echo Testing nise
 pushd generated\nise
-c:\python38\python.exe -m tox -c tox-driver.ini
+c:\python38\python.exe -m tox -c tox-system_tests.ini
 popd
 
 echo Testing nimodinst
 pushd generated\nimodinst
-c:\python38\python.exe -m tox -c tox-driver.ini
+c:\python38\python.exe -m tox -c tox-system_tests.ini
 popd
 
 
