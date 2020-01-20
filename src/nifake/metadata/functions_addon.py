06152430f5ffbb66988fba5031a82c5d2b984526
Mark Silva <github@markesilva.com>
2020-01-20 16:49:34 -0600

    Changes needed to handle multiple types and return bytes

--- src/nifake/metadata/functions_addon.py
+++ src/nifake/metadata/functions_addon.py
@@ -2,6 +2,19 @@
 # Changes to the metadata should be made here, because functions.py is generated thus any changes get overwritten.
 
 functions_override_metadata = {
+    'ImportAttributeConfigurationBuffer': {
+        'parameters': {
+            2: {'python_api_converter_name': 'convert_import_buffer', }
+        },
+    },
+    'ExportAttributeConfigurationBuffer': {
+        'parameters': {
+            2: {
+                'python_api_converter_name': 'convert_buffer_to_bytes',
+                'use_array': True,
+            },
+        },
+    },
 }
 
 
