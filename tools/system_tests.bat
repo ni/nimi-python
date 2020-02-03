108c7838a305b81ff7c79cc3eb2b4af809aa43d9
Mark Silva <github@markesilva.com>
2020-02-03 16:25:37 -0600

    Fix parameter order

--- tools/system_tests.bat
+++ tools/system_tests.bat
@@ -1,7 +1,7 @@
 echo Testing %1
 pushd generated\%1
 set TOX_PARALLEL_NO_SPINNER=1
-c:\python38\python.exe -m tox -c tox-system_tests.ini --parallel --parallel-live auto
+c:\python38\python.exe -m tox -c tox-system_tests.ini --parallel auto --parallel-live 
 popd
 
 
