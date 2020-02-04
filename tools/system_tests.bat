84ecb528baffaa766e3ff963da57ede41f6316e1
Mark Silva <github@markesilva.com>
2020-02-04 10:11:04 -0600

    Make sure the junit directory exists to avoid collisions creating it in parallel

--- tools/system_tests.bat
+++ tools/system_tests.bat
@@ -1,4 +1,6 @@
 echo Testing %1
+rem Make sure the junit folder exists, otherwise we can have a race collision creating it in parallel
+mkdir generated\junit
 pushd generated\%1
 set TOX_PARALLEL_NO_SPINNER=1
 c:\python38\python.exe -m tox -c tox-system_tests.ini --parallel auto --parallel-live 
