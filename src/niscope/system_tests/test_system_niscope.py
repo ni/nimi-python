7d134dc82e606cd4f5d23395d141ae7ccca90b9d
Mark Silva <github@markesilva.com>
2020-02-07 16:14:30 -0600

    Add multi session tests

--- src/niscope/system_tests/test_system_niscope.py
+++ src/niscope/system_tests/test_system_niscope.py
@@ -37,6 +37,13 @@ def session_5142():
             yield simulated_session
 
 
+@pytest.fixture(scope='function')
+def session_multiple_sessions():
+    with daqmx_sim_5124_lock, daqmx_sim_5142_lock:
+        with niscope.Session('5142') as simulated_session_5124, niscope.Session('5142') as simulated_session_5142:
+            yield [simulated_session_5124, simulated_session_5142]
+
+
 # Attribute tests
 def test_vi_boolean_attribute(session):
     session.allow_more_records_than_memory = False
@@ -413,9 +420,43 @@ def test_nitclk_error_handling():
     try:
         test_session_reference.exported_tclk_output_terminal = 'test'
         assert False
-    except niscope.errors.DriverError as e:
+    except nitclk.errors.DriverError as e:
         assert e.code == -250032
 
+
+def test_nitclk_configure_for_homogeneous_triggers(session_multiple_sessions):
+    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
+
+
+def test_nitclk_sync_pulse_sender_synchronize(session_multiple_sessions):
+    nitclk.setup_for_sync_pulse_sender_synchronize(session_multiple_sessions, datetime.timedelta(seconds=.001))
+    nitclk.synchronize_to_sync_pulse_sender(session_multiple_sessions, datetime.timedelta(seconds=.001))
+    nitclk.finish_sync_pulse_sender_synchronize(session_multiple_sessions, datetime.timedelta(seconds=.001))
+
+
+def test_nitclk_synchronize(session_multiple_sessions):
+    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
+    nitclk.synchronize(session_multiple_sessions)
+
+
+def test_nitclk_initiate(session_multiple_sessions):
+    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
+    nitclk.synchronize(session_multiple_sessions)
+    nitclk.initiate(session_multiple_sessions)
+
+
+def test_nitclk_is_done(session_multiple_sessions):
+    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
+    nitclk.synchronize(session_multiple_sessions)
+    nitclk.initiate(session_multiple_sessions)
+    nitclk.is_done(session_multiple_sessions)
+
+
+def test_nitclk_wait_until_done(session_multiple_sessions):
+    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
+    nitclk.synchronize(session_multiple_sessions)
+    nitclk.initiate(session_multiple_sessions)
+    nitclk.wait_until_done(session_multiple_sessions)
 
 
 
