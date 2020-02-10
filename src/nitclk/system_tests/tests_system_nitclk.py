24568e719c3d116238ab1ea9f49b5ac30318f212
Mark Silva <github@markesilva.com>
2020-02-10 16:52:14 -0600

    Fix and don't skip test

--- src/nitclk/system_tests/tests_system_nitclk.py
+++ src/nitclk/system_tests/tests_system_nitclk.py
@@ -72,11 +72,8 @@ def test_nitclk_configure_for_homogeneous_triggers(session_multiple_sessions):
     nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
 
 
-# TODO(texasaggie97) - Remove once we determine how to configure the session(s) for these functions
-@pytest.mark.skip(reason='Need to figure out how to configure in order to test these functions')
 def test_nitclk_sync_pulse_sender_synchronize(session_multiple_sessions):
-    for session in session_multiple_sessions:
-        session.tclk.sync_pulse_clock_source = 'PXI_CLK100'
+    nitclk.setup_for_sync_pulse_synchornize(session_multiple_sessions)
     nitclk.setup_for_sync_pulse_sender_synchronize(session_multiple_sessions, .001)
     nitclk.synchronize_to_sync_pulse_sender(session_multiple_sessions, .001)
     nitclk.finish_sync_pulse_sender_synchronize(session_multiple_sessions, .001)
