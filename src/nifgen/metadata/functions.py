74ca550f9b193b1a89062fa0ba4f64713b6cda3d
Mark Silva <github@markesilva.com>
2020-02-11 14:48:58 -0600

    Update nifgen metadata for imp/exp bytes

--- src/nifgen/metadata/functions.py
+++ src/nifgen/metadata/functions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# This file is generated from NI-FGEN API metadata version 20.0.0d1
+# This file is generated from NI-FGEN API metadata version 20.0.0d3
 functions = {
     'AbortGeneration': {
         'documentation': {
@@ -1248,11 +1248,13 @@ functions = {
                     'description': '\nSpecifies the byte array buffer to be populated with the exported\nattribute configuration.\n'
                 },
                 'name': 'configuration',
+                'python_api_converter_name': 'convert_to_bytes',
                 'size': {
                     'mechanism': 'ivi-dance',
                     'value': 'sizeInBytes'
                 },
-                'type': 'ViInt8[]'
+                'type': 'ViInt8[]',
+                'use_array': True
             }
         ],
         'returns': 'ViStatus'
@@ -1917,6 +1919,7 @@ functions = {
                     'description': '\nSpecifies the byte array buffer that contains the attribute\nconfiguration to import.\n'
                 },
                 'name': 'configuration',
+                'python_api_converter_name': 'convert_import_buffer_to_array',
                 'size': {
                     'mechanism': 'len',
                     'value': 'sizeInBytes'
