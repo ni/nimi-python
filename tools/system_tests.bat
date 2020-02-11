639e89b527cfb0af212c4a7022d2223dafd8807d
Mark Silva <github@markesilva.com>
2020-02-11 14:29:42 -0600

    Try using the spinner

--- tools/system_tests.bat
+++ tools/system_tests.bat
@@ -3,9 +3,9 @@ rem Make sure the junit folder exists, otherwise we can have a race condition cr
 mkdir generated\junit
 pushd generated\%1
 rem We don't need no spinner - mainly because we aren't running tox interactively
-set TOX_PARALLEL_NO_SPINNER=1
+rem set TOX_PARALLEL_NO_SPINNER=1
 rem Run up to 3 environments in parallel, but still send the output to stdout
-c:\python38\python.exe -m tox -c tox-system_tests.ini --parallel 3 --parallel-live 
+c:\python38\python.exe -m tox -c tox-system_tests.ini --parallel 3
 popd
 
 
