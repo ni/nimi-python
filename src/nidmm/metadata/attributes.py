6299dcdba7aa970d390fd16752eae97fbe25b2b3
Mark Silva <github@markesilva.com>
2020-02-11 14:20:37 -0600

    Update nidmm metadata (#1258)
    * Update nidmm metadata
    
    * Update generated files

--- src/nidmm/metadata/attributes.py
+++ src/nidmm/metadata/attributes.py
@@ -1,42 +1,6 @@
 # -*- coding: utf-8 -*-
-# This file is generated from NI-DMM API metadata version 19.6.0d0
+# This file is generated from NI-DMM API metadata version 19.6.0d4
 attributes = {
-    1050002: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nSpecifies whether to validate attribute values and function parameters. If  enabled, the instrument driver validates the parameter values passed to  driver functions. Range checking parameters is very useful for debugging.  After the user program is validated, this attribute can be set to VI_FALSE (0) to  disable range checking and maximize performance.\nThe default value is VI_TRUE (1). Use the niDMM_InitWithOptions function to  override this value.\n'
-        },
-        'lv_property': 'Inherent IVI Attributes:User Options:Range Check',
-        'name': 'RANGE_CHECK',
-        'resettable': False,
-        'type': 'ViBoolean'
-    },
-    1050003: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nSpecifies whether the instrument driver queries the instrument status after  each operation. Querying the instrument status is very useful for debugging.  After the user program is validated, this attribute can be set to VI_FALSE (0) to  disable status checking and maximize performance.\nThe instrument driver can choose to ignore status checking for particular  attributes regardless of the setting of this attribute.\nThe default value is VI_TRUE (1). Use the niDMM_InitWithOptions function to  override this value.\n'
-        },
-        'lv_property': 'Inherent IVI Attributes:User Options:Query Instrument Status',
-        'name': 'QUERY_INSTRUMENT_STATUS',
-        'resettable': False,
-        'type': 'ViBoolean'
-    },
-    1050004: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': 'Specifies whether to cache the value of attributes. When caching is enabled,  the instrument driver keeps track of the current instrument settings and  avoids sending redundant commands to the instrument. Thus, it significantly  increases execution speed. The instrument driver can choose always to cache  or to never cache particular attributes regardless of the setting of this  attribute. The default value is VI_TRUE (1). Use the niDMM_InitWithOptions  function to override this value.'
-        },
-        'lv_property': 'Inherent IVI Attributes:User Options:Cache',
-        'name': 'CACHE',
-        'resettable': False,
-        'type': 'ViBoolean'
-    },
     1050005: {
         'access': 'read-write',
         'channel_based': False,
@@ -45,18 +9,6 @@ attributes = {
         },
         'lv_property': 'Inherent IVI Attributes:User Options:Simulate',
         'name': 'SIMULATE',
-        'resettable': False,
-        'type': 'ViBoolean'
-    },
-    1050006: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nSpecifies whether the IVI engine keeps a list of the value coercions it makes  for ViInt32 and ViReal64 attributes. Call niDMM_GetNextCoercionRecord to extract  and delete the oldest coercion record from the list.\nThe default value is VI_FALSE (0). Use the niDMM_InitWithOptions function to  override this value.\n'
-        },
-        'lv_property': 'Inherent IVI Attributes:User Options:Record Value Coercions',
-        'name': 'RECORD_COERCIONS',
         'resettable': False,
         'type': 'ViBoolean'
     },
@@ -71,54 +23,6 @@ attributes = {
         'resettable': False,
         'type': 'ViString'
     },
-    1050021: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nSpecifies whether to perform interchangeability checking and log  interchangeability warnings when you call niDMM functions.\nThe default value is VI_FALSE.\nInterchangeability warnings indicate that using your application with a  different instrument might cause different behavior.  Call niDMM_GetNextInterchangeWarning  to extract interchange warnings.  Call niDMM_ClearInterchangeWarnings  to clear the list of interchangeability warnings  without reading them.\nInterchangeability checking examines the attributes in a capability group  only if you specify a value for at least one attribute within that group.   Interchangeability warnings can occur when an attribute affects the behavior  of the instrument and you have not set that attribute, or the attribute has  been invalidated since you set it.\n'
-        },
-        'lv_property': 'Inherent IVI Attributes:User Options:Interchange Check',
-        'name': 'INTERCHANGE_CHECK',
-        'resettable': False,
-        'type': 'ViBoolean'
-    },
-    1050101: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nA code that describes the first error that occurred since the last call\nto niDMM Get Error for the session. The value follows the VXIplug&play\nconventions. A negative value describes an error condition. A positive\nvalue describes a warning condition. A zero indicates that no error or\nwarning occurred. The error and warning values can be status codes\ndefined by IVI, VISA, class drivers, or specific drivers.\n'
-        },
-        'lv_property': 'Obsolete:Inherent IVI Attributes:Error Info:Primary Error',
-        'name': 'PRIMARY_ERROR',
-        'resettable': False,
-        'type': 'ViInt32'
-    },
-    1050102: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nAn optional code that provides additional information concerning the\nprimary error condition. The error and warning values can be status\ncodes defined by IVI, VISA, class drivers, or specific drivers. Zero\nindicates no additional information.\n'
-        },
-        'lv_property': 'Obsolete:Inherent IVI Attributes:Error Info:Secondary Error',
-        'name': 'SECONDARY_ERROR',
-        'resettable': False,
-        'type': 'ViInt32'
-    },
-    1050103: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nAn optional string that contains additional information concerning the\nprimary error condition.\n'
-        },
-        'lv_property': 'Obsolete:Inherent IVI Attributes:Error Info:Error Elaboration',
-        'name': 'ERROR_ELABORATION',
-        'resettable': False,
-        'type': 'ViString'
-    },
     1050203: {
         'access': 'read only',
         'channel_based': False,
@@ -129,18 +33,6 @@ attributes = {
         'name': 'CHANNEL_COUNT',
         'resettable': False,
         'type': 'ViInt32'
-    },
-    1050302: {
-        'access': 'read only',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nThe prefix for the specific instrument driver.  The name of each  user-callable function in this driver starts with this prefix.\nThe prefix can be up to a maximum of eight characters.\n'
-        },
-        'lv_property': 'Inherent IVI Attributes:Instrument Capabilities:Specific Driver Prefix',
-        'name': 'SPECIFIC_DRIVER_PREFIX',
-        'resettable': False,
-        'type': 'ViString'
     },
     1050304: {
         'access': 'read only',
@@ -174,42 +66,6 @@ attributes = {
         'name': 'SUPPORTED_INSTRUMENT_MODELS',
         'resettable': False,
         'type': 'ViString'
-    },
-    1050401: {
-        'access': 'read only',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nA string containing the capabilities and extension groups supported by the  specific driver.\n'
-        },
-        'lv_property': 'Inherent IVI Attributes:Specific Driver Capabilities:Group Capabilities',
-        'name': 'GROUP_CAPABILITIES',
-        'resettable': False,
-        'type': 'ViString'
-    },
-    1050501: {
-        'access': 'read only',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': 'The major version number of the IVI engine.'
-        },
-        'lv_property': 'Obsolete:Inherent IVI Attributes:Version Info:Engine Major Version',
-        'name': 'ENGINE_MAJOR_VERSION',
-        'resettable': False,
-        'type': 'ViInt32'
-    },
-    1050502: {
-        'access': 'read only',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': 'The minor version number of the IVI engine.'
-        },
-        'lv_property': 'Obsolete:Inherent IVI Attributes:Version Info:Engine Minor Version',
-        'name': 'ENGINE_MINOR_VERSION',
-        'resettable': False,
-        'type': 'ViInt32'
     },
     1050503: {
         'access': 'read only',
@@ -288,30 +144,6 @@ attributes = {
         'resettable': False,
         'type': 'ViString'
     },
-    1050515: {
-        'access': 'read only',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nThe major version number of the class specification for the specific driver.\n'
-        },
-        'lv_property': 'Inherent IVI Attributes:Specific Driver Identification:Specific Driver Class Spec Major Version',
-        'name': 'SPECIFIC_DRIVER_CLASS_SPEC_MAJOR_VERSION',
-        'resettable': False,
-        'type': 'ViInt32'
-    },
-    1050516: {
-        'access': 'read only',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nThe minor version number of the class specification for the specific driver.\n'
-        },
-        'lv_property': 'Inherent IVI Attributes:Specific Driver Identification:Specific Driver Class Spec Minor Version',
-        'name': 'SPECIFIC_DRIVER_CLASS_SPEC_MINOR_VERSION',
-        'resettable': False,
-        'type': 'ViInt32'
-    },
     1050551: {
         'access': 'read only',
         'channel_based': False,
@@ -322,68 +154,6 @@ attributes = {
         'name': 'SPECIFIC_DRIVER_REVISION',
         'resettable': False,
         'type': 'ViString'
-    },
-    1050553: {
-        'access': 'read only',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nA string that contains additional version information about the IVI\nengine.\n'
-        },
-        'lv_property': 'Obsolete:Inherent IVI Attributes:Version Info:Engine Revision',
-        'name': 'ENGINE_REVISION',
-        'resettable': False,
-        'type': 'ViString'
-    },
-    1150001: {
-        'access': 'read only',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nA string containing the type of instrument used in the current session.\n'
-        },
-        'lv_property': 'Obsolete:Misc:IDQuery response',
-        'name': 'ID_QUERY_RESPONSE',
-        'resettable': False,
-        'type': 'ViString'
-    },
-    1150002: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nSpecifies the polarity of the generated measurement complete signal.\n'
-        },
-        'enum': 'MeasurementDestinationSlope',
-        'lv_property': 'Trigger:Measurement Destination Slope',
-        'name': 'MEAS_DEST_SLOPE',
-        'resettable': False,
-        'type': 'ViInt32'
-    },
-    1150003: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nFor the NI 4050 only, specifies the shunt resistance value.\nThe NI 4050 requires an external shunt resistor for current measurements.  This attribute should be set to the value of shunt resistor.\n'
-        },
-        'lv_property': 'Configuration:Measurement Options:Shunt Value',
-        'name': 'SHUNT_VALUE',
-        'resettable': False,
-        'type': 'ViReal64'
-    },
-    1150010: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nSpecifies the edge of the signal from the specified sample trigger source on  which the DMM is triggered.\n'
-        },
-        'enum': 'SampleTrigSlope',
-        'lv_property': 'Multi Point Acquisition:Sample Trig Slope',
-        'name': 'SAMPLE_TRIGGER_SLOPE',
-        'resettable': False,
-        'type': 'ViInt32'
     },
     1150014: {
         'access': 'read-write',
@@ -501,18 +271,6 @@ attributes = {
         'resettable': False,
         'type': 'ViReal64'
     },
-    1150031: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nFor the NI 4060 only, specifies a delay interval after an sample external trigger.\n0\nNIDMM_ATTR_SAMPLE_INTERVAL is only used when the Sample Trigger attribute is set to  INTERVAL.\n1\nNIDMM_ATTR_SAMPLE_INTERVAL is used as a delay after ANY type of Sample  Trigger\n'
-        },
-        'lv_property': 'Multi Point Acquisition:Sample Delay Mode',
-        'name': 'SAMPLE_DELAY_MODE',
-        'resettable': False,
-        'type': 'ViInt32'
-    },
     1150032: {
         'access': 'read-write',
         'channel_based': False,
@@ -521,18 +279,6 @@ attributes = {
         },
         'lv_property': 'Configuration:Advanced:Number Of Averages',
         'name': 'NUMBER_OF_AVERAGES',
-        'resettable': False,
-        'type': 'ViInt32'
-    },
-    1150034: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nSpecifies the number of measurements transferred at a time from the  instrument to an internal buffer. When set to NIDMM_VAL_LATENCY_AUTO (-1),  NI-DMM chooses the transfer size.\n'
-        },
-        'lv_property': 'Multi Point Acquisition:Advanced:Latency',
-        'name': 'LATENCY',
         'resettable': False,
         'type': 'ViInt32'
     },
@@ -1036,18 +782,5 @@ attributes = {
         'name': 'POWERLINE_FREQ',
         'resettable': False,
         'type': 'ViReal64'
-    },
-    1250334: {
-        'access': 'read-write',
-        'channel_based': False,
-        'codegen_method': 'no',
-        'documentation': {
-            'description': '\nSpecifies the edge of the signal from the specified trigger source on which  the DMM is triggered.\n'
-        },
-        'enum': 'TriggerSlope',
-        'lv_property': 'Trigger:Trigger Slope',
-        'name': 'TRIGGER_SLOPE',
-        'resettable': False,
-        'type': 'ViInt32'
     }
 }
