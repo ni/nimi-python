281c9ea337e54ef260791ec79df4a7b18f04d431
Mark Silva <github@markesilva.com>
2020-02-12 16:01:49 -0600

    Use different simulated HW for nitclk system tests

--- src/nitclk/system_tests/tests_system_nitclk.py
+++ src/nitclk/system_tests/tests_system_nitclk.py
@@ -9,54 +9,61 @@ import pytest
 import tempfile
 
 
-# We need a lock file so multiple tests aren't hitting the simulated HW at the same time
-daqmx_sim_5124_lock_file = os.path.join(tempfile.gettempdir(), 'daqmx_5124.lock')
-daqmx_sim_5124_lock = fasteners.InterProcessLock(daqmx_sim_5124_lock_file)
-daqmx_sim_5142_lock_file = os.path.join(tempfile.gettempdir(), 'daqmx_5142.lock')
-daqmx_sim_5142_lock = fasteners.InterProcessLock(daqmx_sim_5142_lock_file)
+# nitclk system tests require a different HW that the PXIe-5164 that we simulate else where. It does not support NI-TClk
+# in simulation. As a workaround, the nimi-bot VMs are configured with two simulated instrument of the PXIe-5122
+# named "5122_1" and "5122_2". If you want to run these tests on your own system, you will need to create these two simulated
+# instruments.
+# In addition, we need a global lock in order to keep us from opening more than one session to the same simulated instrument
+# at the same time. This is because NI-SCOPE (like other MI driver runtimes) disallow two simultaneous sessions to the same
+# instrument, even when the instrument is simulated. This will impact the performance at which system tests run because we
+# parallelize at the tox level :(.
+daqmx_sim_5122_1_lock_file = os.path.join(tempfile.gettempdir(), 'daqmx_5122_1.lock')
+daqmx_sim_5122_1_lock = fasteners.InterProcessLock(daqmx_sim_5122_1_lock_file)
+daqmx_sim_5122_2_lock_file = os.path.join(tempfile.gettempdir(), 'daqmx_5122_2.lock')
+daqmx_sim_5122_2_lock = fasteners.InterProcessLock(daqmx_sim_5122_2_lock_file)
 
 
 @pytest.fixture(scope='function')
-def session_5124():
-    with daqmx_sim_5124_lock:
+def session_5122_1():
+    with daqmx_sim_5122_1_lock:
         with niscope.Session('5124') as simulated_session:
             yield simulated_session
 
 
 @pytest.fixture(scope='function')
 def session_multiple_sessions():
-    with daqmx_sim_5124_lock, daqmx_sim_5142_lock:
-        with niscope.Session('5124') as simulated_session_5124, niscope.Session('5142') as simulated_session_5142:
-            yield [simulated_session_5124, simulated_session_5142]
+    with daqmx_sim_5122_1_lock, daqmx_sim_5122_2_lock:
+        with niscope.Session('5122_1') as simulated_session_5122_1, niscope.Session('5122_2') as simulated_session_5122_2:
+            yield [simulated_session_5122_1, simulated_session_5122_2]
 
 
-def test_nitclk_integration(session_5124):
-    assert type(session_5124.tclk) == nitclk.SessionReference
+def test_nitclk_integration(session_5122_1):
+    assert type(session_5122_1.tclk) == nitclk.SessionReference
 
 
-def test_nitclk_vi_string(session_5124):
+def test_nitclk_vi_string(session_5122_1):
     # default is empty string
-    assert session_5124.tclk.exported_tclk_output_terminal == ''
-    session_5124.tclk.exported_tclk_output_terminal = 'PXI_Trig0'
-    assert session_5124.tclk.exported_tclk_output_terminal == 'PXI_Trig0'
+    assert session_5122_1.tclk.exported_tclk_output_terminal == ''
+    session_5122_1.tclk.exported_tclk_output_terminal = 'PXI_Trig0'
+    assert session_5122_1.tclk.exported_tclk_output_terminal == 'PXI_Trig0'
 
 
-def test_nitclk_session_reference(session_5124):
+def test_nitclk_session_reference(session_5122_1):
     test_session = niscope.Session('FakeDevice', False, True, 'Simulate=1, DriverSetup=Model:5164; BoardType:PXIe')
-    session_5124.tclk.ref_trigger_master_session = test_session
+    session_5122_1.tclk.ref_trigger_master_session = test_session
     # We need to look at the actual session number inside the class
     # we know the type returned from session.tclk.pause_trigger_master_session will be nitclk.SessionReference
     # This test assumes knowledge of the class internals
-    assert session_5124.tclk.ref_trigger_master_session._session_number == test_session.tclk._get_tclk_session_reference()
-    assert session_5124.tclk.ref_trigger_master_session._session_number == test_session._vi
+    assert session_5122_1.tclk.ref_trigger_master_session._session_number == test_session.tclk._get_tclk_session_reference()
+    assert session_5122_1.tclk.ref_trigger_master_session._session_number == test_session._vi
 
 
-def test_nitclk_vi_real64(session_5124):
+def test_nitclk_vi_real64(session_5122_1):
     # default is 0
-    assert session_5124.tclk.sample_clock_delay.total_seconds() == 0
+    assert session_5122_1.tclk.sample_clock_delay.total_seconds() == 0
     test_number = 4.2
-    session_5124.tclk.sample_clock_delay = test_number
-    assert session_5124.tclk.sample_clock_delay.total_seconds() == test_number
+    session_5122_1.tclk.sample_clock_delay = test_number
+    assert session_5122_1.tclk.sample_clock_delay.total_seconds() == test_number
 
 
 def test_nitclk_error_handling():
