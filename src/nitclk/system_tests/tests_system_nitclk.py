4d8d75ec3e83221338afea42049976663afe018c
Mark Silva <github@markesilva.com>
2020-02-13 17:31:58 -0600

    Use 5164 for system tests

--- src/nitclk/system_tests/tests_system_nitclk.py
+++ src/nitclk/system_tests/tests_system_nitclk.py
@@ -8,32 +8,17 @@ import os
 import pytest
 import tempfile
 
-# nitclk does not work with session based simulated devices.
-# As a workaround, the nimi-bot VMs are configured with two persistently simulated PXIe-5122 instruments named
-# "5122_1" and "5122_2". If you want to run these tests on your own system, you will need to create these two
-# simulated instruments.
-# In addition, we need a global lock in order to keep us from opening more than one session to the same simulated instrument
-# at the same time. This is because NI-SCOPE (like other MI driver runtimes) disallow two simultaneous sessions to the same
-# instrument, even when the instrument is simulated. This will impact the performance at which system tests run because we
-# parallelize at the tox level :(.
-daqmx_sim_5122_1_lock_file = os.path.join(tempfile.gettempdir(), 'daqmx_5122_1.lock')
-daqmx_sim_5122_1_lock = fasteners.InterProcessLock(daqmx_sim_5122_1_lock_file)
-daqmx_sim_5122_2_lock_file = os.path.join(tempfile.gettempdir(), 'daqmx_5122_2.lock')
-daqmx_sim_5122_2_lock = fasteners.InterProcessLock(daqmx_sim_5122_2_lock_file)
-
 
 @pytest.fixture(scope='function')
 def session_5122_1():
-    with daqmx_sim_5122_1_lock:
-        with niscope.Session('5124') as simulated_session:
-            yield simulated_session
+    with niscope.Session('', False, False, 'Simulate=1, DriverSetup=Model:5164;BoardType:PXIe') as simulated_session:
+        yield simulated_session
 
 
 @pytest.fixture(scope='function')
 def session_multiple_sessions():
-    with daqmx_sim_5122_1_lock, daqmx_sim_5122_2_lock:
-        with niscope.Session('5122_1') as simulated_session_5122_1, niscope.Session('5122_2') as simulated_session_5122_2:
-            yield [simulated_session_5122_1, simulated_session_5122_2]
+    with niscope.Session('', False, False, 'Simulate=1, DriverSetup=Model:5164;BoardType:PXIe') as simulated_session_1, niscope.Session('', False, False, 'Simulate=1, DriverSetup=Model:5164;BoardType:PXIe') as simulated_session_2:
+        yield [simulated_session_1, simulated_session_2]
 
 
 def test_nitclk_integration(session_5122_1):
