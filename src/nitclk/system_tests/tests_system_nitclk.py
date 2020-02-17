fd0ed3e447d316edf1c523f28de2332c138039a6
Mark Silva <github@markesilva.com>
2020-02-17 10:56:10 -0600

    Remove device number from fixture name

--- src/nitclk/system_tests/tests_system_nitclk.py
+++ src/nitclk/system_tests/tests_system_nitclk.py
@@ -10,44 +10,44 @@ import tempfile
 
 
 @pytest.fixture(scope='function')
-def session_5164():
+def single_niscope_session():
     with niscope.Session('', False, False, 'Simulate=1, DriverSetup=Model:5164;BoardType:PXIe') as simulated_session:
         yield simulated_session
 
 
 @pytest.fixture(scope='function')
-def session_multiple_sessions():
+def multiple_niscope_sessions():
     with niscope.Session('', False, False, 'Simulate=1, DriverSetup=Model:5164;BoardType:PXIe') as simulated_session_1, niscope.Session('', False, False, 'Simulate=1, DriverSetup=Model:5164;BoardType:PXIe') as simulated_session_2:
         yield [simulated_session_1, simulated_session_2]
 
 
-def test_nitclk_integration(session_5164):
-    assert type(session_5164.tclk) == nitclk.SessionReference
+def test_nitclk_integration(single_niscope_session):
+    assert type(single_niscope_session.tclk) == nitclk.SessionReference
 
 
-def test_nitclk_vi_string(session_5164):
+def test_nitclk_vi_string(single_niscope_session):
     # default is empty string
-    assert session_5164.tclk.exported_tclk_output_terminal == ''
-    session_5164.tclk.exported_tclk_output_terminal = 'PXI_Trig0'
-    assert session_5164.tclk.exported_tclk_output_terminal == 'PXI_Trig0'
+    assert single_niscope_session.tclk.exported_tclk_output_terminal == ''
+    single_niscope_session.tclk.exported_tclk_output_terminal = 'PXI_Trig0'
+    assert single_niscope_session.tclk.exported_tclk_output_terminal == 'PXI_Trig0'
 
 
-def test_nitclk_session_reference(session_5164):
+def test_nitclk_session_reference(single_niscope_session):
     test_session = niscope.Session('FakeDevice', False, True, 'Simulate=1, DriverSetup=Model:5164; BoardType:PXIe')
-    session_5164.tclk.ref_trigger_master_session = test_session
+    single_niscope_session.tclk.ref_trigger_master_session = test_session
     # We need to look at the actual session number inside the class
     # we know the type returned from session.tclk.pause_trigger_master_session will be nitclk.SessionReference
     # This test assumes knowledge of the class internals
-    assert session_5164.tclk.ref_trigger_master_session._session_number == test_session.tclk._get_tclk_session_reference()
-    assert session_5164.tclk.ref_trigger_master_session._session_number == test_session._vi
+    assert single_niscope_session.tclk.ref_trigger_master_session._session_number == test_session.tclk._get_tclk_session_reference()
+    assert single_niscope_session.tclk.ref_trigger_master_session._session_number == test_session._vi
 
 
-def test_nitclk_vi_real64(session_5164):
+def test_nitclk_vi_real64(single_niscope_session):
     # default is 0
-    assert session_5164.tclk.sample_clock_delay.total_seconds() == 0
+    assert single_niscope_session.tclk.sample_clock_delay.total_seconds() == 0
     test_number = 4.2
-    session_5164.tclk.sample_clock_delay = test_number
-    assert session_5164.tclk.sample_clock_delay.total_seconds() == test_number
+    single_niscope_session.tclk.sample_clock_delay = test_number
+    assert single_niscope_session.tclk.sample_clock_delay.total_seconds() == test_number
 
 
 def test_nitclk_error_handling():
@@ -59,40 +59,40 @@ def test_nitclk_error_handling():
         assert e.code == -250032
 
 
-def test_nitclk_configure_for_homogeneous_triggers(session_multiple_sessions):
-    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
+def test_nitclk_configure_for_homogeneous_triggers(multiple_niscope_sessions):
+    nitclk.configure_for_homogeneous_triggers(multiple_niscope_sessions)
 
 
-def test_nitclk_sync_pulse_sender_synchronize(session_multiple_sessions):
-    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
-    nitclk.setup_for_sync_pulse_sender_synchronize(session_multiple_sessions, .001)
-    nitclk.synchronize_to_sync_pulse_sender(session_multiple_sessions, .001)
-    nitclk.finish_sync_pulse_sender_synchronize(session_multiple_sessions, .001)
+def test_nitclk_sync_pulse_sender_synchronize(multiple_niscope_sessions):
+    nitclk.configure_for_homogeneous_triggers(multiple_niscope_sessions)
+    nitclk.setup_for_sync_pulse_sender_synchronize(multiple_niscope_sessions, .001)
+    nitclk.synchronize_to_sync_pulse_sender(multiple_niscope_sessions, .001)
+    nitclk.finish_sync_pulse_sender_synchronize(multiple_niscope_sessions, .001)
 
 
-def test_nitclk_synchronize(session_multiple_sessions):
-    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
-    nitclk.synchronize(session_multiple_sessions, .001)
+def test_nitclk_synchronize(multiple_niscope_sessions):
+    nitclk.configure_for_homogeneous_triggers(multiple_niscope_sessions)
+    nitclk.synchronize(multiple_niscope_sessions, .001)
 
 
-def test_nitclk_initiate(session_multiple_sessions):
-    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
-    nitclk.synchronize(session_multiple_sessions, .001)
-    nitclk.initiate(session_multiple_sessions)
+def test_nitclk_initiate(multiple_niscope_sessions):
+    nitclk.configure_for_homogeneous_triggers(multiple_niscope_sessions)
+    nitclk.synchronize(multiple_niscope_sessions, .001)
+    nitclk.initiate(multiple_niscope_sessions)
 
 
-def test_nitclk_is_done(session_multiple_sessions):
-    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
-    nitclk.synchronize(session_multiple_sessions, .001)
-    nitclk.initiate(session_multiple_sessions)
-    nitclk.is_done(session_multiple_sessions)
+def test_nitclk_is_done(multiple_niscope_sessions):
+    nitclk.configure_for_homogeneous_triggers(multiple_niscope_sessions)
+    nitclk.synchronize(multiple_niscope_sessions, .001)
+    nitclk.initiate(multiple_niscope_sessions)
+    nitclk.is_done(multiple_niscope_sessions)
 
 
-def test_nitclk_wait_until_done(session_multiple_sessions):
-    nitclk.configure_for_homogeneous_triggers(session_multiple_sessions)
-    nitclk.synchronize(session_multiple_sessions, .001)
-    nitclk.initiate(session_multiple_sessions)
-    nitclk.wait_until_done(session_multiple_sessions, .001)
+def test_nitclk_wait_until_done(multiple_niscope_sessions):
+    nitclk.configure_for_homogeneous_triggers(multiple_niscope_sessions)
+    nitclk.synchronize(multiple_niscope_sessions, .001)
+    nitclk.initiate(multiple_niscope_sessions)
+    nitclk.wait_until_done(multiple_niscope_sessions, .001)
 
 
 
