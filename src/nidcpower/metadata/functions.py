6fa5b0096c7da3315f6db92e50c84aed3ee8f7c6
Mark Silva <github@markesilva.com>
2020-03-02 17:17:04 -0600

    Move fancy create advanced sequence to addon file, plus copy documentation from regular create advanced sequence

--- src/nidcpower/metadata/functions.py
+++ src/nidcpower/metadata/functions.py
@@ -883,52 +883,6 @@ functions = {
         ],
         'returns': 'ViStatus'
     },
-    'FancyCreateAdvancedSequence': {
-        'codegen_method': 'python-only',
-        'returns': 'ViStatus',
-        'python_name': 'create_advanced_sequence',
-        'method_templates': [
-            { 'session_filename': 'fancy_advanced_sequence', 'documentation_filename': 'default_method', 'method_python_name_suffix': '', },
-        ],
-        'parameters': [
-            {
-                'direction': 'in',
-                'name': 'vi',
-                'type': 'ViSession',
-                'documentation': {
-                    'description': 'Identifies a particular instrument session. **vi** is obtained from the niDCPower_InitExtCal or niDCPower_InitializeWithChannels function.',
-                },
-            },
-            {
-                'direction': 'in',
-                'name': 'SequenceName',
-                'type': 'ViString',
-                'documentation': {
-                    'description': 'Name of sequence.',
-                },
-            },
-            {
-                'direction': 'in',
-                'name': 'PropertyNames',
-                'type': 'ViString[]',
-                'documentation': {
-                    'description': 'Sequence.',
-                },
-            },
-            {
-                'direction': 'in',
-                'name': 'setAsActiveSequence',
-                'type': 'ViBoolean',
-                'default_value': True,
-                'documentation': { 'description': 'Specifies that this current sequence is active.', },
-            },
-        ],
-        'documentation':
-        {
-            'description': 'Test.',
-            'note': 'Test.',
-        },
-    },
     'FancyFetchMultiple': {
         'codegen_method': 'python-only',
         'documentation': {
