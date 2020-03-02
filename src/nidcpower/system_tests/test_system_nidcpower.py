7c0d2c7ea138a9fa186ed8028a38a718db6c9e89
Mark Silva <github@markesilva.com>
2020-03-02 15:20:55 -0600

    Wrong variable

--- src/nidcpower/system_tests/test_system_nidcpower.py
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -266,13 +266,13 @@ def test_create_and_delete_advanced_sequence(single_channel_session):
     sequence_name = 'my_sequence'
     single_channel_session.source_mode = nidcpower.SourceMode.SEQUENCE
     single_channel_session.create_advanced_sequence(sequence_name=sequence_name, property_names=properties_used, set_as_active_sequence=True)
-    assert session.active_advanced_sequence == sequence_name
+    assert single_channel_session.active_advanced_sequence == sequence_name
     single_channel_session.create_advanced_sequence_step(set_as_active_step=True)
     single_channel_session.output_function = nidcpower.OutputFunction.DC_VOLTAGE
     single_channel_session.voltage_level = 1
     single_channel_session.delete_advanced_sequence(sequence_name=sequence_name)
     try:
-        session.active_advanced_sequence = sequence_name
+        single_channel_session.active_advanced_sequence = sequence_name
         assert False
     except nidcpower.errors.DriverError:
         pass
