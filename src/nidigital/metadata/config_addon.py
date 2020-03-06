828a7dfe1dcbb4e5351ca9cd03f0ed1e4f08092b
sbethur <48041236+sbethur@users.noreply.github.com>
2020-03-06 16:21:49 -0600

    Updated module and runtime versions

--- src/nidigital/metadata/config_addon.py
+++ src/nidigital/metadata/config_addon.py
@@ -1,6 +1,6 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
-    'module_version': '0.2.2.dev0',
+    'module_version': '0.5.0',
     'latest_runtime_version_tested_against': '19.0.1',
     'custom_types': [
         {
