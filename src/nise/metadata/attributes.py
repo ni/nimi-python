ff304830b479e2714ffa6cdec07ce3eac53aaec6
Shreyas Bethur <48041236+sbethur@users.noreply.github.com>
2020-03-06 13:25:35 -0600

    nise metadata update (#1311)

--- src/nise/metadata/attributes.py
+++ src/nise/metadata/attributes.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# This file is generated from NI Switch Executive API metadata version 21.0.0d0
+# This file is generated from NI Switch Executive API metadata version 21.0.0d1
 attributes = {
 }
