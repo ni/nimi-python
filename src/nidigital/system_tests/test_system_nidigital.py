be54ae8d8f53d5758b4d384a2c9c4bfca823693a
sbethur <48041236+sbethur@users.noreply.github.com>
2020-03-18 16:28:55 -0500

    Add whitespace within dict literal, as required by flake8

--- src/nidigital/system_tests/test_system_nidigital.py
+++ src/nidigital/system_tests/test_system_nidigital.py
@@ -488,7 +488,7 @@ def test_get_site_pass_fail(multi_instrument_session):
         timeout=5)
 
     pass_fail = multi_instrument_session.get_site_pass_fail(site_list='')
-    assert pass_fail == {0: True, 1: True, 2:True, 3:True}
+    assert pass_fail == {0: True, 1: True, 2: True, 3: True}
 
     pass_fail = multi_instrument_session.get_site_pass_fail(site_list='site3,site0')
     assert pass_fail == {3: True, 0: True}
