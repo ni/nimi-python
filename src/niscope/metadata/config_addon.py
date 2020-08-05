8c32f07c01a4db19e468893f4bdfdfb43adb4f29
shilohbeeler-NI <shiloh.beeler@ni.com>
2020-08-05 11:08:10 -0500

    Added comment to link to issue in config_addon.

--- src/niscope/metadata/config_addon.py
+++ src/niscope/metadata/config_addon.py
@@ -3,6 +3,7 @@ config_additional_config = {
     'module_version': '1.3.2.dev0',
     'latest_runtime_version_tested_against': '20.0.0',
     'custom_types': [
+        # Redundant, since waveform_info is also in the base config.py file. See issue 1495 (https://github.com/ni/nimi-python/issues/1495) 
         {
             'ctypes_type': 'struct_niScope_wfmInfo',
             'file_name': 'waveform_info',
