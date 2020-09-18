524a1e26d5b649f66b23739ba1ad4ff98c79da9b
sbethur <48041236+sbethur@users.noreply.github.com>
2020-09-18 11:42:22 -0500

    Update version in config_addon.py and LATEST_RELEASE

--- src/niscope/metadata/config_addon.py
+++ src/niscope/metadata/config_addon.py
@@ -1,6 +1,6 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
-    'module_version': '1.3.2.dev0',
+    'module_version': '1.3.2',
     'latest_runtime_version_tested_against': '20.0.0',
     'custom_types': [
         # Redundant, since waveform_info is also in the base config.py file. See issue 1495 (https://github.com/ni/nimi-python/issues/1495) 
