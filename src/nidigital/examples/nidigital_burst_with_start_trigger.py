49920c4198116eaebbef3b0eeef348ed1d669f29
Jerry Xie <jerry.xie@ni.com>
2021-01-27 10:53:10 -0600

    Addressing Shreyas's comments round 2

--- src/nidigital/examples/nidigital_burst_with_start_trigger.py
+++ src/nidigital/examples/nidigital_burst_with_start_trigger.py
@@ -35,12 +35,13 @@ def example(resource_name, options, trigger_source=None, trigger_edge=None):
             session.digital_edge_start_trigger_source = trigger_source
             session.digital_edge_start_trigger_edge = nidigital.DigitalEdge.RISING if trigger_edge == 'Rising' else nidigital.DigitalEdge.FALLING
 
-        # Waiting for the trigger to start bursting and then blocks until the pattern is done bursting
+        # If start trigger is configured, waiting for the trigger to start bursting and then blocks until the pattern is done bursting
+        # Else just start bursting and block until the pattern is done bursting
         session.burst_pattern(start_label='new_pattern')
         if trigger_source is None:
             print('Start bursting pattern')
         else:
-            print('Waiting for start trigger and then start bursting pattern')
+            print('Wait for start trigger and then start bursting pattern')
 
         # Disconnect all channels using programmable onboard switching
         session.selected_function = nidigital.SelectedFunction.DISCONNECT
@@ -50,15 +51,15 @@ def example(resource_name, options, trigger_source=None, trigger_edge=None):
 def _main(argsv):
     parser = argparse.ArgumentParser(description='Demonstrates how to create and configure an instrument session and to burst a pattern on the digital pattern instrument using a start trigger.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-n', '--resource-name', default='PXI1Slot2,PXI1Slot3', help='Resource name of a NI digital pattern instrument. Ensure the resource name matches the instrument name in the pinmap file.')
-    parser.add_argument('-s', '--simulate', default=True, type=bool, help='Whether to run on hardware or run on software simulation')
+    parser.add_argument('-s', '--simulate', default='True', choices=['True', 'False'], help='Whether to run on simulated hardware or real hardware')
     subparser = parser.add_subparsers(dest='command', help='Sub-command help')
     start_trigger = subparser.add_parser('start-trigger', help='Configure start trigger')
-    start_trigger.add_argument('-src', '--trigger-source', default='/PXI1Slot2/PXI_Trig0', help='Source terminal for the start trigger')
-    start_trigger.add_argument('-edge', '--trigger-edge', default='Rising', choices=['Rising', 'Falling'], help='Trigger on rising edge or falling edge of start trigger')
+    start_trigger.add_argument('-ts', '--trigger-source', default='/PXI1Slot2/PXI_Trig0', help='Source terminal for the start trigger')
+    start_trigger.add_argument('-te', '--trigger-edge', default='Rising', choices=['Rising', 'Falling'], help='Trigger on rising edge or falling edge of start trigger')
     args = parser.parse_args(argsv)
 
     example(args.resource_name,
-            'Simulate=1, DriverSetup=Model:6571' if args.simulate else '',
+            'Simulate=1, DriverSetup=Model:6571' if args.simulate == 'True' else '',
             args.trigger_source if args.command == 'start-trigger' else None,
             args.trigger_edge if args.command == 'start-trigger' else None)
 
@@ -78,7 +79,7 @@ def test_example():
     example(resource_name, options)
 
     trigger_source = '/PXI1Slot2/PXI_Trig0'
-    trigger_edge = True
+    trigger_edge = 'Rising'
     example(resource_name, options, trigger_source, trigger_edge)
 
 
