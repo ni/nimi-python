c6e371ea8077bcef4dd93de86d25b384b0e2ea6c
gfisher-NI <68872082+gfisher-NI@users.noreply.github.com>
2021-02-02 10:36:50 -0600

    Update nifgen_trigger_example.py
    Fixed the "expected 2 blank lines" issues in the CI test.

--- src/nifgen/examples/nifgen_trigger_example.py
+++ src/nifgen/examples/nifgen_trigger_example.py
@@ -3,6 +3,7 @@ import nifgen
 import sys
 import time
 
+
 def example(resource_name1, resource_name2, options, waveform, frequency, amplitude, offset, phase, gen_time):
     with nifgen.Session(resource_name=resource_name1, options=options) as session1, nifgen.Session(resource_name=resource_name2, options=options) as session2:
         session_list = [session1, session2]
@@ -18,6 +19,7 @@ def example(resource_name1, resource_name2, options, waveform, frequency, amplit
                 session1.send_software_edge_trigger(nifgen.Trigger.START)
                 time.sleep(gen_time)
 
+                
 def _main(argsv):
     supported_waveforms = list(nifgen.Waveform.__members__.keys())[:-1]  # no support for user-defined waveforms in example
     parser = argparse.ArgumentParser(description='Generates the standard function.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -33,6 +35,7 @@ def _main(argsv):
     args = parser.parse_args(argsv)
     example(args.resource_name1, args.resource_name2, args.option_string, args.waveform, args.frequency, args.amplitude, args.offset, args.phase, args.time)
 
+    
 def main():
     _main(sys.argv[1:])
 
