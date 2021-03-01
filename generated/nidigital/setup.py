49f968d6256f950920197af16f81e7884327f0ee
sbethur <48041236+sbethur@users.noreply.github.com>
2021-03-01 12:29:31 -0600

    Update generated files with new version

--- generated/nidigital/setup.py
+++ generated/nidigital/setup.py
@@ -29,7 +29,7 @@ def read_contents(file_to_read):
 setup(
     name=pypi_name,
     zip_safe=True,
-    version='0.9.3.dev0',
+    version='1.0.0',
     description='NI-Digital Pattern Driver Python API',
     long_description=read_contents('README.rst'),
     long_description_content_type='text/x-rst',
@@ -52,7 +52,7 @@ setup(
     tests_require=['pytest'],
     test_suite='tests',
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Manufacturing",
         "Intended Audience :: Science/Research",
