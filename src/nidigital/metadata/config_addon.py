89b9d841b3138c7726f45b102308485ab779ca0a
sbethur <48041236+sbethur@users.noreply.github.com>
2021-03-02 11:51:11 -0600

    Update version in config_addon.py

--- src/nidigital/metadata/config_addon.py
+++ src/nidigital/metadata/config_addon.py
@@ -1,6 +1,6 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
-    'module_version': '1.0.0',
+    'module_version': '1.0.1.dev0',
     'latest_runtime_version_tested_against': '20.6.0',
     'custom_types': [
         {
