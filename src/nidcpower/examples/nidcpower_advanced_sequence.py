4ff538d2f7392b7025b89fae7ccf9b81596a14c6
Luis Gomes <luis.gomes@ni.com>
2021-05-10 09:58:43 -0500

    Update dcpower examples to use independent channels

--- src/nidcpower/examples/nidcpower_advanced_sequence.py
+++ src/nidcpower/examples/nidcpower_advanced_sequence.py
@@ -6,10 +6,10 @@ import nidcpower
 import sys
 
 
-def example(resource_name, channels, options, voltage_max, current_max, points_per_output_function, delay_in_seconds):
+def example(resource_name, options, voltage_max, current_max, points_per_output_function, delay_in_seconds):
     timeout = hightime.timedelta(seconds=(delay_in_seconds + 1.0))
 
-    with nidcpower.Session(resource_name=resource_name, channels=channels, options=options) as session:
+    with nidcpower.Session(resource_name=resource_name, options=options) as session:
 
         # Configure the session.
         session.source_mode = nidcpower.SourceMode.SEQUENCE
@@ -33,28 +33,33 @@ def example(resource_name, channels, options, voltage_max, current_max, points_p
 
         with session.initiate():
             session.wait_for_event(nidcpower.Event.SEQUENCE_ENGINE_DONE)
-            measurements = session.fetch_multiple(points_per_output_function * 2, timeout=timeout)
+            channels = resource_name.split(',')
+            measurement_group = [None] * len(channels)
+            for i, channel in enumerate(channels):
+                channel_name = channel.strip()
+                measurement_group[i] = session.channels[channel_name].fetch_multiple(points_per_output_function * 2, timeout=timeout)
 
         session.delete_advanced_sequence(sequence_name='my_sequence')
-        line_format = '{:,<4} {:,.6g} {:,.6g} {:<6}\n'
-        print('{:<4} {:<10} {:,<10} {:<6}'.format('Num', 'Voltage', 'Current', 'In Compliance'))
-        i = 0
-        for measurement in measurements:
-            print(line_format.format(i, measurement.voltage, measurement.current, str(measurement.in_compliance)))
-            i += 1
+        line_format = '{:<15} {:<4} {:<10} {:<10} {:<6}'
+        print(line_format.format('Channel', 'Num', 'Voltage', 'Current', 'In Compliance'))
+        for i, measurements in enumerate(measurement_group):
+            num = 0
+            channel_name = channels[i].strip()
+            for measurement in measurements:
+                print(line_format.format(channel_name, num, measurement.voltage, measurement.current, str(measurement.in_compliance)))
+                num += 1
 
 
 def _main(argsv):
     parser = argparse.ArgumentParser(description='Output ramping voltage to voltage max, then ramping current to current max.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-n', '--resource-name', default='PXI1Slot2', help='Resource name of a National Instruments SMU')
-    parser.add_argument('-c', '--channels', default='0', help='Channel(s) to use')
+    parser.add_argument('-n', '--resource-name', default='PXI1Slot2/0, PXI1Slot3/1', help='Resource name of National Instruments SMUs')
     parser.add_argument('-s', '--number-steps', default=256, help='Number of steps per output function')
     parser.add_argument('-v', '--voltage-max', default=1.0, type=float, help='Maximum voltage (V)')
     parser.add_argument('-i', '--current-max', default=0.001, type=float, help='Maximum Current (I)')
     parser.add_argument('-d', '--delay', default=0.05, type=float, help='Source delay (s)')
     parser.add_argument('-op', '--option-string', default='', type=str, help='Option string')
     args = parser.parse_args(argsv)
-    example(args.resource_name, args.channels, args.option_string, args.voltage_max, args.current_max, args.number_steps, args.delay)
+    example(args.resource_name, args.option_string, args.voltage_max, args.current_max, args.number_steps, args.delay)
 
 
 def main():
@@ -68,7 +73,7 @@ def test_main():
 
 def test_example():
     options = {'simulate': True, 'driver_setup': {'Model': '4162', 'BoardType': 'PXIe', }, }
-    example('PXI1Slot2', '0', options, 1.0, 0.001, 256, 0.05)
+    example('PXI1Slot2/0, PXI1Slot3/1', options, 1.0, 0.001, 256, 0.05)
 
 
 if __name__ == '__main__':
