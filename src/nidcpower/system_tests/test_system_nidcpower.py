ee9f9efb7dfc8455abafa247c902b5727eab0fec
Sean Moore <sean.moore@ni.com>
2021-06-11 07:08:43 -0500

    default to test with independent channels only

--- src/nidcpower/system_tests/test_system_nidcpower.py
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -8,11 +8,10 @@ import tempfile
 def pytest_generate_tests(metafunc):
     '''Parametrizes the "session" fixture by examining the the markers set for a test.
 
-    By default, the session fixture is parametrized so each test runs twice. Once with a legacy
-    Synchronized Channels session and once with an Independent Channels session. To limit the
-    session used for a test to only a legacy session, decorate the test with
-    @pytest.mark.legacy_session_only. To limit the session used for a test to only an Independent
-    Channels session, decorate the test with @pytest.mark.independent_channels_session_only.
+    By default, the session fixture is parametrized so each test runs once with an Independent
+    Channels session. To also run a test with a legacy Synchronized Channels session, decorate the
+    test with the custom marker @pytest.mark.include_legacy_session. To run a test with only a
+    legacy session, decorate the test with @pytest.mark.legacy_session_only.
     '''
 
     if 'session' in metafunc.fixturenames:
@@ -20,14 +19,14 @@ def pytest_generate_tests(metafunc):
         # markers from being set on the same test
 
         legacy_session_only = metafunc.definition.get_closest_marker('legacy_session_only')
-        independent_channels_session_only = metafunc.definition.get_closest_marker('independent_channels_session_only')
+        include_legacy_session = metafunc.definition.get_closest_marker('include_legacy_session')
 
         if legacy_session_only:
             metafunc.parametrize('session', [False], indirect=True)
-        if independent_channels_session_only:
+        if include_legacy_session:
+            metafunc.parametrize('session', [True, False], indirect=True)
+        if not legacy_session_only and not include_legacy_session:
             metafunc.parametrize('session', [True], indirect=True)
-        if not legacy_session_only and not independent_channels_session_only:
-            metafunc.parametrize('session', [False, True], indirect=True)
 
 
 @pytest.fixture(scope='function')
@@ -44,9 +43,9 @@ def session(request):
         @pytest.mark.options
         @pytest.mark.independent_channels
 
-    By default, all dependent tests will run twice. First with a legacy Synchronized Channels
-    session. Second with an Independent Channels session. Dependent tests can override this behavior
-    by using markers. Refer to the documentation in pytest_generate_tests for more information.
+    By default, all dependent tests will run once with an Independent Channels session. Dependent
+    tests can override this behavior by using custom markers. Refer to the documentation in
+    pytest_generate_tests for more information.
     '''
 
     # set default values
@@ -68,15 +67,16 @@ def session(request):
         yield simulated_session
 
 
+@pytest.mark.include_legacy_session
 def test_self_test(session):
     session.self_test()
 
 
+@pytest.mark.include_legacy_session
 def test_self_cal(session):
     session.self_cal()
 
 
-@pytest.mark.independent_channels_session_only
 def test_get_channel_name_independent_channels(session):
     name = session.get_channel_name(1)
     assert name == '4162/0'
@@ -88,7 +88,6 @@ def test_get_channel_name_synchronized_channels(session):
     assert name == '0'
 
 
-@pytest.mark.independent_channels_session_only
 def test_get_channel_names_independent_channels(session):
     expected_string = ['4162/{0}'.format(x) for x in range(12)]
     channel_indices = ['0-1, 2, 3:4', 5, (6, 7), range(8, 10), slice(10, 12)]
@@ -102,11 +101,13 @@ def test_get_channel_names_synchronized_channels(session):
     assert session.get_channel_names(channel_indices) == expected_string
 
 
+@pytest.mark.include_legacy_session
 def test_get_attribute_string(session):
     model = session.instrument_model
     assert model == 'NI PXIe-4162'
 
 
+@pytest.mark.include_legacy_session
 def test_error_message():
     try:
         # We pass in an invalid model name to force going to error_message
@@ -117,6 +118,7 @@ def test_error_message():
         assert e.description.find('The option string parameter contains an entry with an unknown option value.') != -1
 
 
+@pytest.mark.include_legacy_session
 def test_get_error(session):
     try:
         session.instrument_model = ''
@@ -126,6 +128,7 @@ def test_get_error(session):
         assert e.description.find('Attribute is read-only.') != -1
 
 
+@pytest.mark.include_legacy_session
 def test_get_self_cal_last_date_and_time(session):
     last_cal = session.get_self_cal_last_date_and_time()
     assert last_cal.year == 1940
@@ -135,16 +138,19 @@ def test_get_self_cal_last_date_and_time(session):
     assert last_cal.minute == 0
 
 
+@pytest.mark.include_legacy_session
 def test_get_self_cal_last_temp(session):
     temperature = session.get_self_cal_last_temp()
     assert temperature == 25.0
 
 
+@pytest.mark.include_legacy_session
 def test_read_current_temperature(session):
     temperature = session.read_current_temperature()
     assert temperature == 25.0
 
 
+@pytest.mark.include_legacy_session
 def test_reset_device(session):
     channel = session.channels['0']
     default_output_function = channel.output_function
@@ -155,6 +161,7 @@ def test_reset_device(session):
     assert function_after_reset == default_output_function
 
 
+@pytest.mark.include_legacy_session
 def test_reset_with_default(session):
     channel = session.channels['0']
     assert channel.aperture_time_units == nidcpower.ApertureTimeUnits.SECONDS
@@ -163,6 +170,7 @@ def test_reset_with_default(session):
     assert channel.aperture_time_units == nidcpower.ApertureTimeUnits.SECONDS
 
 
+@pytest.mark.include_legacy_session
 def test_reset(session):
     channel = session.channels['0']
     assert channel.output_enabled is True
@@ -171,6 +179,7 @@ def test_reset(session):
     assert channel.output_enabled is True
 
 
+@pytest.mark.include_legacy_session
 def test_disable(session):
     channel = session.channels['0']
     assert channel.output_enabled is True
@@ -179,6 +188,7 @@ def test_disable(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_measure(session):
     session.source_mode = nidcpower.SourceMode.SINGLE_POINT
     session.output_function = nidcpower.OutputFunction.DC_VOLTAGE
@@ -191,6 +201,7 @@ def test_measure(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_query_output_state(session):
     with session.initiate():
         assert session.query_output_state(nidcpower.OutputStates.VOLTAGE) is True   # since default function is DCVolt when initiated output state for DC Volt\DC current should be True and False respectively
@@ -198,6 +209,7 @@ def test_query_output_state(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_config_aperture_time(session):
     expected_default_aperture_time = 0.01666
     default_aperture_time = session.aperture_time
@@ -213,6 +225,7 @@ def test_config_aperture_time(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_fetch_multiple(session):
     session.source_mode = nidcpower.SourceMode.SINGLE_POINT
     session.configure_aperture_time(0, nidcpower.ApertureTimeUnits.SECONDS)
@@ -229,6 +242,7 @@ def test_fetch_multiple(session):
         assert measurements[1].current == 0.00001
 
 
+@pytest.mark.include_legacy_session
 def test_measure_multiple(session):
     with session.initiate():
         # session is open to all 12 channels on the device
@@ -246,6 +260,7 @@ def test_measure_multiple(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_query_max_current_limit(session):
     max_current_limit = session.query_max_current_limit(6)
     expected_max_current_limit = 0.1  # for a simulated 4162 max current limit should be 0.1 for 6V Voltage level
@@ -254,6 +269,7 @@ def test_query_max_current_limit(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_query_max_voltage_level(session):
     max_voltage_level = session.query_max_voltage_level(0.03)
     expected_max_voltage_level = 24  # for a simulated 4162 max voltage level should be 24V for 30mA current limit
@@ -262,6 +278,7 @@ def test_query_max_voltage_level(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_query_min_current_limit(session):
     min_current_limit = session.query_min_current_limit(0.03)
     expected_min_current_limit = 0.0000001  # for a simulated 4162 min_current_limit should be 1uA for 6V voltage level
@@ -270,11 +287,13 @@ def test_query_min_current_limit(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_set_sequence_with_source_delays(session):
     session.set_sequence([0.1, 0.2, 0.3], [0.001, 0.002, 0.003])
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_set_sequence_with_too_many_source_delays(session):
     try:
         session.set_sequence([0.1, 0.2, 0.3], [0.001, 0.002, 0.003, 0.004])
@@ -284,6 +303,7 @@ def test_set_sequence_with_too_many_source_delays(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_set_sequence_with_too_few_source_delays(session):
     try:
         session.set_sequence([0.1, 0.2, 0.3, 0.4], [0.001, 0.002])
@@ -293,18 +313,21 @@ def test_set_sequence_with_too_few_source_delays(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_wait_for_event_default_timeout(session):
     with session.initiate():
         session.wait_for_event(nidcpower.Event.SOURCE_COMPLETE)
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_wait_for_event_with_timeout(session):
     with session.initiate():
         session.wait_for_event(nidcpower.Event.SOURCE_COMPLETE, hightime.timedelta(seconds=0.5))
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_commit(session):
     non_default_current_limit = 0.00021
     session.current_limit = non_default_current_limit
@@ -312,6 +335,7 @@ def test_commit(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_import_export_buffer(session):
     test_value_1 = 1
     test_value_2 = 2
@@ -325,6 +349,7 @@ def test_import_export_buffer(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_import_export_file(session):
     test_value_1 = 1
     test_value_2 = 2
@@ -343,6 +368,7 @@ def test_import_export_file(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_create_and_delete_advanced_sequence(session):
     properties_used = ['output_function', 'voltage_level']
     sequence_name = 'my_sequence'
@@ -361,6 +387,7 @@ def test_create_and_delete_advanced_sequence(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_create_and_delete_advanced_sequence_bad_name(session):
     properties_used = ['output_function_bad', 'voltage_level']
     sequence_name = 'my_sequence'
@@ -373,6 +400,7 @@ def test_create_and_delete_advanced_sequence_bad_name(session):
 
 
 @pytest.mark.channels('0')
+@pytest.mark.include_legacy_session
 def test_create_and_delete_advanced_sequence_bad_type(session):
     properties_used = ['unlock', 'voltage_level']
     sequence_name = 'my_sequence'
@@ -394,6 +422,7 @@ def test_send_software_edge_trigger_error(session):
         assert e.description.find('The requested function is not available when multiple channels are present in the same session.') != -1
 
 
+@pytest.mark.include_legacy_session
 def test_get_ext_cal_last_date_and_time(session):
     print(type(session))
     last_cal = session.get_ext_cal_last_date_and_time()
@@ -404,22 +433,26 @@ def test_get_ext_cal_last_date_and_time(session):
     assert last_cal.minute == 0
 
 
+@pytest.mark.include_legacy_session
 def test_get_ext_cal_last_temp(session):
     temperature = session.get_ext_cal_last_temp()
     assert temperature == 25.0
 
 
+@pytest.mark.include_legacy_session
 def test_get_ext_cal_recommended_interval(session):
     interval = session.get_ext_cal_recommended_interval()
     assert interval.days == 365
 
 
+@pytest.mark.include_legacy_session
 def test_set_get_vi_int_64_attribute(session):
     session.channels['0'].active_advanced_sequence_step = 1
     read_advanced_sequence_step = session.channels['0'].active_advanced_sequence_step
     assert read_advanced_sequence_step == 1
 
 
+@pytest.mark.include_legacy_session
 def test_channel_format_types():
     with nidcpower.Session('4162', [0, 1], False, 'Simulate=1, DriverSetup=Model:4162; BoardType:PXIe') as simulated_session:
         assert simulated_session.channel_count == 2
@@ -527,6 +560,7 @@ def test_init_raises_driver_errors_for_invalid_arguments(resource_name, channels
     assert e.value.code == expected_error_code
 
 
+@pytest.mark.include_legacy_session
 def test_repeated_capabilities_on_method_when_all_channels_are_specified(session):
     '''Sessions should not error when specifying all channels by number.'''
     assert session.channels['0'].output_enabled is True
@@ -551,5 +585,6 @@ def test_error_channel_name_not_allowed(session):
     assert e.value.description.find('The channel or repeated capability name is not allowed.') != -1
 
 
+@pytest.mark.include_legacy_session
 def test_repeated_capabilities_with_initiate(session):
     session.channels['0-11'].initiate()
