bfd0bd9cb1911794c9d0dfb5c17e1c6b7de0b353
Shreyas Bethur <48041236+sbethur@users.noreply.github.com>
2021-07-12 09:35:14 -0500

    Post release changes for 1.4.0 (#1642)
    * Update version in metadata
    
    * Update generated files
    
    * Update CHANGELOG.md

--- src/nidigital/metadata/config_addon.py
+++ src/nidigital/metadata/config_addon.py
@@ -1,6 +1,6 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
-    'module_version': '1.4.0',
+    'module_version': '1.4.1.dev0',
     'latest_runtime_version_tested_against': '20.6.1',
     'custom_types': [
         {
