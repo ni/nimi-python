6b6026355d876745bde4f8d48afaefdf559a1b96
Sukruth Grandhi <Sukruth.Grandhi@ni.com>
2021-09-13 23:21:19 +0530

    Addressing review comments

--- tools/system_tests.bat
+++ tools/system_tests.bat
@@ -2,9 +2,9 @@ echo Testing %1
 pushd generated\%1
 rem We don't need no spinner - mainly because we aren't running tox interactively
 set TOX_PARALLEL_NO_SPINNER=1
-rem Run up to 3 environments in parallel, but still send the output to stdout
+rem Run up to 4 environments in parallel, but still send the output to stdout
 rem We only run the tests if the tox file exists
-python.exe -m tox -c tox-system_tests.ini --parallel 1 --parallel-live
+python.exe -m tox -c tox-system_tests.ini --parallel 4 --parallel-live
 popd
 
 
