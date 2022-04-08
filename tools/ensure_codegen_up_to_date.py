7f49c8dc6b5dcecbfed37a654411225318664029
Ayesha Mohamed Kaleel Rahman <ayesha.mohamed.kaleel.rahman@ni.com>
2022-04-08 16:39:39 -0500

    Use error string as input to sys.exit and move codegen check inside function

--- tools/ensure_codegen_up_to_date.py
+++ tools/ensure_codegen_up_to_date.py
@@ -13,34 +13,32 @@ def configure_git_credentials():
     Configures git user name and email with dummy name and email
     """
     if os.system('git config user.email "dummy@ni.com"') != 0:
-        print("Error: Unable to configure \"user email\" using git", file=sys.stderr)
-        sys.exit(-1)
+        sys.exit("Error: Unable to configure \"user email\" using git")
 
     if os.system('git config user.name "dummy"') != 0:
-        print("Error: Unable to configure \"user name\" using git", file=sys.stderr)
-        sys.exit(-1)
+        sys.exit("Error: Unable to configure \"user name\" using git")
 
 def clean_codegen_files():
     """
     Before code generation clean the existing codegen files
     """
     if os.system("tox -e clean") != 0:
-        print("Error: Unable to clean the repo using \"tox -e clean\"", file=sys.stderr)
-        sys.exit(-1)
+        sys.exit("Error: Unable to clean the repo using \"tox -e clean\"")
 
 def create_codegen_files():
     """
     create codegen files
     """
     if os.system("tox -e codegen") != 0:
-        print("Error: Unable to generate code using \"tox -e codegen\"", file=sys.stderr)
-        sys.exit(-1)
+        sys.exit("Error: Unable to generate code using \"tox -e codegen\"")
 
 def check_file_status():
     '''
     Checks if there are any modified files
     '''
-    return int(subprocess.check_output("git status -s -uno | wc -l", shell=True).decode().strip("b'\\n'"))
+    if int(subprocess.check_output("git status -s -uno | wc -l", shell=True).decode().strip("b'\\n'")) != 0:
+        sys.exit("Run codegen and include the generated files in the PR")
+    print("All changes to code generation are committed to repository")
 
 if __name__ == "__main__":
     """
@@ -51,8 +49,4 @@ if __name__ == "__main__":
     clean_codegen_files()
     create_codegen_files()
 
-    if check_file_status() != 0:
-        print("Run codegen and include the generated files in the PR", file=sys.stderr)
-        sys.exit(-1)
-
-    print("All changes to code generation are committed to repository")
+    check_file_status()
