6536a73a7dc3502bf7abba216bd1e94069d2a990
dummy <dummy@ni.com>
2022-04-08 14:31:51 -0500

    Used git status to check for modified files and removed old methods

--- tools/ensure_codegen_up_to_date.py
+++ tools/ensure_codegen_up_to_date.py
@@ -24,14 +24,6 @@ def configure_git_user_name():
         print("Unable to configure \"user name\" using git", file=sys.stderr)
         sys.exit(-1)
 
-def get_commit_id():
-    """
-    Returns the commit id
-    Returns:
-        [str]: Returns the commit id
-    """
-    return subprocess.check_output("git log -1 --format=%h", shell=True).decode().strip("b'\\n'")
-
 def clean_codegen_files():
     """
     Before code generation clean the existing codegen files
@@ -48,28 +40,11 @@ def create_codegen_files():
         print("Unable to generate code using \"tox -e codegen\"", file=sys.stderr)
         sys.exit(-1)
 
-def add_codegen_files_to_git_staging_area():
-    """
-    Add the codegen files to git staging area.
-    No files will be added to git staging area if
-      1) No new files are generated
-      2) No existing files are modified/deleted.
-    """
-    if os.system('git add .') != 0:
-        print("Unable to add files to git", file=sys.stderr)
-        sys.exit(-1)
-
-def commit_git_staging_area_files():
-    """
-    Commits the staging area files with dummy message
-    If no files are present in the staging area then no new commit will be created.
-    """
-    os.system('git commit -m "dummy"') # return code is 1 if there are no files to commit
-
-def reset_dummy_commit():
-    if os.system("git reset --soft HEAD~1") != 0:
-        print("Unable to reset dummy commit")
-        sys.exit(-1)
+def check_file_status():
+    '''
+    Checks if there are any modified files
+    '''
+    return int(subprocess.check_output("git status -s -uno | wc -l", shell=True).decode().strip("b'\\n'"))
 
 if __name__ == "__main__":
     """
@@ -79,19 +54,11 @@ if __name__ == "__main__":
     configure_git_user_email()
     configure_git_user_name()
 
-    commit_id_before_code_generator = get_commit_id()
-
     clean_codegen_files()
     create_codegen_files()
-    add_codegen_files_to_git_staging_area()
-    commit_git_staging_area_files()
 
-    commit_id_after_code_generator = get_commit_id()
-
-
-    if commit_id_before_code_generator != commit_id_after_code_generator:
+    if check_file_status() != 0:
         print("Run codegen and include the generated files in the PR", file=sys.stderr)
-        reset_dummy_commit()
         sys.exit(-1)
 
     print("All changes to code generation are committed to repository")
