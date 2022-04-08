aa7a75b6f4b45fe59c1de0c96bcd3e69722aba59
Ayesha Mohamed Kaleel Rahman <ayesha.mohamed.kaleel.rahman@ni.com>
2022-04-08 16:21:51 -0500

    Update error printout

--- tools/ensure_codegen_up_to_date.py
+++ tools/ensure_codegen_up_to_date.py
@@ -8,20 +8,16 @@ import os
 import sys
 import subprocess
 
-def configure_git_user_email():
+def configure_git_credentials():
     """
-    Configures git user email with dummy email id
+    Configures git user name and email with dummy name and email
     """
     if os.system('git config user.email "dummy@ni.com"') != 0:
-        print("Unable to configure \"user email\" using git", file=sys.stderr)
+        print("Error: Unable to configure \"user email\" using git", file=sys.stderr)
         sys.exit(-1)
 
-def configure_git_user_name():
-    """
-    Configures git user email with dummy name
-    """
     if os.system('git config user.name "dummy"') != 0:
-        print("Unable to configure \"user name\" using git", file=sys.stderr)
+        print("Error: Unable to configure \"user name\" using git", file=sys.stderr)
         sys.exit(-1)
 
 def clean_codegen_files():
@@ -29,7 +25,7 @@ def clean_codegen_files():
     Before code generation clean the existing codegen files
     """
     if os.system("tox -e clean") != 0:
-        print("Unable to clean the repo using \"tox -e clean\"", file=sys.stderr)
+        print("Error: Unable to clean the repo using \"tox -e clean\"", file=sys.stderr)
         sys.exit(-1)
 
 def create_codegen_files():
@@ -37,7 +33,7 @@ def create_codegen_files():
     create codegen files
     """
     if os.system("tox -e codegen") != 0:
-        print("Unable to generate code using \"tox -e codegen\"", file=sys.stderr)
+        print("Error: Unable to generate code using \"tox -e codegen\"", file=sys.stderr)
         sys.exit(-1)
 
 def check_file_status():
@@ -50,9 +46,7 @@ if __name__ == "__main__":
     """
     Main
     """
-    # Configure git credentials
-    configure_git_user_email()
-    configure_git_user_name()
+    configure_git_credentials()
 
     clean_codegen_files()
     create_codegen_files()
