8f25c3aeefb46658d7aed015d688b01d0715a373
Ayesha Mohamed Kaleel Rahman <ayesha.mohamed.kaleel.rahman@ni.com>
2022-04-12 14:21:58 -0500

    Fix typo in code

--- tools/ensure_codegen_up_to_date.py
+++ tools/ensure_codegen_up_to_date.py
@@ -43,7 +43,7 @@ def convert_all_files_to_unix_line_endings():
 
     nimi_python_files = glob.glob(os.path.join(os.getcwd(), '**'), recursive=True)
     for file_name in nimi_python_files:
-        subprocess.run(['dos2unix', file_name], shell=False) != 0
+        subprocess.run(['dos2unix', file_name], shell=False)
 
 
 def check_no_dirty_files():
