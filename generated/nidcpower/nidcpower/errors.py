cd988472d1500337b9c5678f031b8bb621fd3aff
Seong Liang Ooi Lim <seong.liang.ooi.lim@ni.com>
2022-06-23 23:19:51 +0800

    Move DriverTooNewError to default, revert metadata
    - Add docs for DriverTooOldError

--- generated/nidcpower/nidcpower/errors.py
+++ generated/nidcpower/nidcpower/errors.py
@@ -64,6 +64,13 @@ class DriverTooOldError(Error):
         super(DriverTooOldError, self).__init__('A function was not found in the NI-DCPower runtime. Please visit http://www.ni.com/downloads/drivers/ to download a newer version and install it.')
 
 
+class DriverTooNewError(Error):
+    '''An error due to the driver runtime being too new for the Python module.'''
+
+    def __init__(self, prefix_message):
+        super(DriverTooNewError, self).__init__(f'{prefix_message}This can occur if the NI-DCPower runtime is newer than the nidcpower Python module. Upgrade the nidcpower Python module.')
+
+
 class InvalidRepeatedCapabilityError(Error):
     '''An error due to an invalid character in a repeated capability'''
 
@@ -78,13 +85,6 @@ class SelfTestError(Error):
         self.code = code
         self.message = msg
         super(SelfTestError, self).__init__('Self-test failed with code {0}: {1}'.format(code, msg))
-
-
-class DriverTooNewError(Error):
-    '''An error due to the driver runtime being too new compared to the Python module'''
-
-    def __init__(self, prefix_message):
-        super(DriverTooNewError, self).__init__(f'{prefix_message}This can occur if the nidcpower Python module is too old. Upgrade the nidcpower Python module.')
 
 
 def handle_error(session, code, ignore_warnings, is_error_handling):
