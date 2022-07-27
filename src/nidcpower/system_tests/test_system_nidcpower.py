349ef46b229c412f3306a1e5544009736c4cde70
Shreyas Bethur <48041236+sbethur@users.noreply.github.com>
2022-07-27 10:50:50 -0500

    Update test_system_nidcpower.py

--- src/nidcpower/system_tests/test_system_nidcpower.py
+++ src/nidcpower/system_tests/test_system_nidcpower.py
@@ -71,6 +71,8 @@ def test_self_test(session):
     session.self_test()
 
 
+# Workaround for driver runtime bug. See issue #1798 for details.
+@pytest.mark.resource_name('')
 def test_self_cal(session):
     session.self_cal()
 
