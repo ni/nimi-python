02ebc9ed0bcc43f74d685b26f30a634c19a98e8c
olsl21 <80089084+olsl21@users.noreply.github.com>
2022-07-29 03:51:00 +0800

    NI-DCPower: Update create advanced sequence docs, metadata (#1797)
    * NI-DCPower: Update create advanced sequence docs
    
    - Update NI-DCPower metadata and functions_addon.py to update the docs for `_create_advanced_sequence_with_channels()` and `create_advanced_sequence()` to include:
      - PXIe-4147 and PXIe-4190
      - New LCR-related attributes
    
    * NI-DCPower: Update metadata to 22.8.0d129

--- src/nidcpower/metadata/config.py
+++ src/nidcpower/metadata/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# This file is generated from NI-DCPower API metadata version 22.8.0d88
+# This file is generated from NI-DCPower API metadata version 22.8.0d129
 config = {
-    'api_version': '22.8.0d88',
+    'api_version': '22.8.0d129',
     'c_function_prefix': 'niDCPower_',
     'close_function': 'close',
     'context_manager_name': {
