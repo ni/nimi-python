6e31d492d27710a9b1f72c227f8e78bdcbaff63b
Shreyas Bethur <48041236+sbethur@users.noreply.github.com>
2022-07-29 11:05:22 -0500

    Update Driver Version Tested Against for 2022 Q3 drivers

--- src/nidigital/metadata/config_addon.py
+++ src/nidigital/metadata/config_addon.py
@@ -1,7 +1,7 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
     'module_version': '1.4.2.dev0',
-    'latest_runtime_version_tested_against': '21.0.0',
+    'latest_runtime_version_tested_against': '21.8.0',
     'custom_types': [
         {
             'ctypes_type': '',
