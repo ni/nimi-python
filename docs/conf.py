744443205c22bdf9ad96690f18b14f34ea400df3
Shreyas Bethur <48041236+sbethur@users.noreply.github.com>
2022-08-02 16:00:55 -0500

    Update generated files

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = 'NI'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '1.4.2.dev0'
+release = '1.4.2'
 # The short X.Y version.
 version = release[:3]
 
