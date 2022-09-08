f5e02dfafb22cf972da7c2ec256cf26fc0969661
David Curtiss <david.curtiss@ni.com>
2022-09-07 19:43:06 -0500

    again

--- generated/nidigital/nidigital/_library_interpreter.py
+++ generated/nidigital/nidigital/_library_interpreter.py
@@ -339,14 +339,14 @@ class LibraryInterpreter(object):
         errors.handle_error(self, session, error_code, ignore_warnings=False, is_error_handling=False)
         return
 
-    def _fetch_capture_waveform(self, session, waveform_name, samples_to_read, timeout):
+    def _fetch_capture_waveform(self, session, site_list, waveform_name, samples_to_read, timeout):
         # This is slightly modified codegen from the function
         # We cannot use codegen without major modifications to the code generator
         # This function uses two 'ivi-dance' parameters and then multiplies them together - see
         # the (modified) line below
         # Also, we want to return the two sized that normally wouldn't be returned
         vi_ctype = _visatype.ViSession(session._vi)  # case S110
-        site_list_ctype = ctypes.create_string_buffer(self._repeated_capability.encode(self._encoding))  # case C010
+        site_list_ctype = ctypes.create_string_buffer(site_list.encode(self._encoding))  # case C010
         waveform_name_ctype = ctypes.create_string_buffer(waveform_name.encode(self._encoding))  # case C020
         samples_to_read_ctype = _visatype.ViInt32(samples_to_read)  # case S150
         timeout_ctype = _converters.convert_timedelta_to_seconds_real64(timeout)  # case S140
--- src/nidigital/templates/_library_interpreter.py/fancy_fetch_capture_waveform.py.mako
+++ src/nidigital/templates/_library_interpreter.py/fancy_fetch_capture_waveform.py.mako
@@ -1,13 +1,13 @@
 
     ## Define the private method below the public method so that lock decorator gets added to the public method
-    def _fetch_capture_waveform(self, session, waveform_name, samples_to_read, timeout):
+    def _fetch_capture_waveform(self, session, site_list, waveform_name, samples_to_read, timeout):
         # This is slightly modified codegen from the function
         # We cannot use codegen without major modifications to the code generator
         # This function uses two 'ivi-dance' parameters and then multiplies them together - see
         # the (modified) line below
         # Also, we want to return the two sized that normally wouldn't be returned
         vi_ctype = _visatype.ViSession(session._vi)  # case S110
-        site_list_ctype = ctypes.create_string_buffer(self._repeated_capability.encode(self._encoding))  # case C010
+        site_list_ctype = ctypes.create_string_buffer(site_list.encode(self._encoding))  # case C010
         waveform_name_ctype = ctypes.create_string_buffer(waveform_name.encode(self._encoding))  # case C020
         samples_to_read_ctype = _visatype.ViInt32(samples_to_read)  # case S150
         timeout_ctype = _converters.convert_timedelta_to_seconds_real64(timeout)  # case S140
