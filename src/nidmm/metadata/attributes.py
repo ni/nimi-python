607e27f3bde68a681456d58462ee3af00290cbac
David Curtiss <david.curtiss@ni.com>
2022-11-02 15:16:46 -0500

    Update metadata from exports

--- src/nidmm/metadata/attributes.py
+++ src/nidmm/metadata/attributes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# This file is generated from NI-DMM API metadata version 23.0.0d9999
+# This file is generated from NI-DMM API metadata version 23.0.0d67
 attributes = {
     1050005: {
         'access': 'read-write',
