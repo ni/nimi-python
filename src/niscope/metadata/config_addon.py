b955ac771cc26a78b1a5dfa30b7a5f6396d652d6
Tobias Gordon <tobias.gordon@ni.com>
2022-12-16 21:27:22 +0000

    Ran `python3 tools/build_release.py --update`

--- src/niscope/metadata/config_addon.py
+++ src/niscope/metadata/config_addon.py
@@ -1,6 +1,6 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
-    'module_version': '1.4.3',
+    'module_version': '1.4.4.dev0',
     'latest_runtime_version_tested_against': '2022 Q3',
     'custom_types': [
         # Redundant, since waveform_info is also in the base config.py file. See issue 1495 (https://github.com/ni/nimi-python/issues/1495) 
