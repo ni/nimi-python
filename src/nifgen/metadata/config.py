a902d5eb6d537f712793f70008e6381586c13413
Jay Fitzgerald <34140133+ni-jfitzger@users.noreply.github.com>
2023-04-13 15:53:04 -0500

    Update nifgen metadata for nimi-python 1.4.4 release (#1929)
    * update nifgen metadata
    
    * update nifgen.proto

--- src/nifgen/metadata/config.py
+++ src/nifgen/metadata/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# This file is generated from NI-FGEN API metadata version 23.0.0d57
+# This file is generated from NI-FGEN API metadata version 23.5.0d28
 config = {
-    'api_version': '23.0.0d57',
+    'api_version': '23.5.0d28',
     'c_function_prefix': 'niFgen_',
     'close_function': 'close',
     'context_manager_name': {
