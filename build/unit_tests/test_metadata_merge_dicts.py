55a843e4531ed590f2809af306dc8e0391cb7da4
Jay Fitzgerald <34140133+ni-jfitzger@users.noreply.github.com>
2023-05-08 16:14:53 -0500

    Split build unit tests out from code (#1961)
    * separate out codegen_helper tests
    
    * separate out documentation_helper tests
    
    * separate out documentation_snippets tests
    
    * separate out helper tests
    
    * separate out metadata_add_all tests
    
    * separate out metadata_merge_dicts tests
    
    * Explain the difference between the 2 calls to pytest (since I decided to keep both)
    
    * Use --append to merge coverage results from the 2 runs

---
+++ build/unit_tests/test_metadata_merge_dicts.py
@@ -0,0 +1,68 @@
+from build.helper.metadata_merge_dicts import *
+
+
+def _do_the_test_merge_dicts(a, b, expected, use_re):
+    actual = a.copy()
+    merge_dicts(actual, b, use_re, 'test')
+    assert expected == actual, "\na = {0}\nb = {1}\nexpected = {2}\nactual = {3}".format(a, b, expected, actual)
+
+
+def test_merge_dict_second_is_empty():
+    a = {'a': 1, 'b': 2}
+    b = {}
+    expected = {'a': 1, 'b': 2}
+    _do_the_test_merge_dicts(a, b, expected, use_re=True)
+
+
+def test_merge_dict_key_exists():
+    a = {'a': 1, 'b': 2}
+    b = {'b': 3}
+    expected = {'a': 1, 'b': 3}
+    _do_the_test_merge_dicts(a, b, expected, use_re=True)
+
+
+def test_merge_dict_recurse():
+    a = {'a': 1, 'b': {'b1': 5, 'b2': 6}}
+    b = {'b': {'b3': 7}}
+    expected = {'a': 1, 'b': {'b1': 5, 'b2': 6, 'b3': 7}}
+    _do_the_test_merge_dicts(a, b, expected, use_re=True)
+
+
+def test_merge_dict_replace_in_list():
+    a = {'a': 1, 'b': {'b1': 5, 'b2': 6}, 'c': ['x', 'y', 'z']}
+    b = {'b': {'b3': 7, 'b1': 8}, 'c': {0: 'r', 1: 's', 2: 't'}}
+    expected = {'a': 1, 'b': {'b1': 8, 'b2': 6, 'b3': 7}, 'c': ['r', 's', 't']}
+    _do_the_test_merge_dicts(a, b, expected, use_re=True)
+
+
+def test_merge_dict_replace_in_dict_and_list():
+    a = {'a': 1, 'b': {'b1': 2, 'b2': 3}, 'c': ['x', 'y', 'z']}
+    b = {'b': {'b3': 7, 'b1': 8}, 'c': {0: 'r', 1: 's', 2: 't'}}
+    expected = {'a': 1, 'b': {'b1': 8, 'b2': 3, 'b3': 7}, 'c': ['r', 's', 't']}
+    _do_the_test_merge_dicts(a, b, expected, use_re=True)
+
+
+def test_merge_dict_with_regex():
+    a = {'aaa': {}, 'aaaa': {'x': 98}, 'aaaaa': {}, 'bbb': {'bbb1': 2, 'bbb2': 3}, 'ccc': ['x', 'y', 'z']}
+    b = {'a+': {'z': 99}}
+    expected = {'aaa': {'z': 99}, 'aaaa': {'x': 98, 'z': 99}, 'aaaaa': {'z': 99}, 'bbb': {'bbb1': 2, 'bbb2': 3}, 'ccc': ['x', 'y', 'z']}
+    _do_the_test_merge_dicts(a, b, expected, use_re=True)
+
+
+def test_merge_dict_with_regex_off():
+    a = {'aaa': {}, 'aaaa': {'x': 98}, 'aaaaa': {}, 'bbb': {'bbb1': 2, 'bbb2': 3}, 'ccc': ['x', 'y', 'z']}
+    b = {'aaa': {'z': 99}}
+    expected = {'aaa': {'z': 99}, 'aaaa': {'x': 98}, 'aaaaa': {}, 'bbb': {'bbb1': 2, 'bbb2': 3}, 'ccc': ['x', 'y', 'z']}
+    _do_the_test_merge_dicts(a, b, expected, use_re=False)
+
+
+def test_merge_dict_top_level_key_missing():
+    a = {'a': 1, 'b': 2}
+    b = {'b': 3, 'c': 4}
+    expected = {'a': 1, 'b': 3, 'c': 4}
+    try:
+        _do_the_test_merge_dicts(a, b, expected, use_re=True)
+        assert False
+    except KeyError:
+        pass
+
