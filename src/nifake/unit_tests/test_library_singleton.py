2eef79ab5716b73fd92e3f36da1b08c89ade687d
Jay Fitzgerald <34140133+ni-jfitzger@users.noreply.github.com>
2023-07-25 08:46:29 -0500

    rename test

--- src/nifake/unit_tests/test_library_singleton.py
+++ src/nifake/unit_tests/test_library_singleton.py
@@ -2,6 +2,6 @@ import nifake
 import pytest
 
 
-def test_get_uninstalled_driver_library_raises_driver_not_installed_error():
+def test_driver_runtime_not_installed_raises_driver_not_installed_error():
     with pytest.raises(nifake.errors.DriverNotInstalledError):
         nifake._library_singleton.get()
