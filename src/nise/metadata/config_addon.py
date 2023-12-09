655e310b39a24ff1b77e14daa056a41b589030ad
Jay Fitzgerald <34140133+ni-jfitzger@users.noreply.github.com>
2023-12-09 08:28:55 -0600

    Update version to 1.4.7 in metadata

--- src/nise/metadata/config_addon.py
+++ src/nise/metadata/config_addon.py
@@ -1,6 +1,6 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
-    'module_version': '1.4.7.dev0',
+    'module_version': '1.4.7',
     'latest_runtime_version_tested_against': '2023 Q1',
     'initial_release_year': '2018',
 }
