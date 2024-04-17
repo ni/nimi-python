5448e9cff0751e41864c15e3bd8c12cc8a900497
Jay Fitzgerald <34140133+ni-jfitzger@users.noreply.github.com>
2024-04-17 11:21:26 -0500

    Update runtime versions tested against - '23.8' aka '2023 Q4' for most drivers - '24.3' aka '2024 Q2' for NI-DCPower, NI-Digital   - These also update the versions for NI-ModInst and NI-TClk

--- src/nidigital/metadata/config_addon.py
+++ src/nidigital/metadata/config_addon.py
@@ -1,7 +1,7 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
     'module_version': '1.4.8.dev0',
-    'latest_runtime_version_tested_against': '2023 Q2',
+    'latest_runtime_version_tested_against': '2024 Q2',
     'initial_release_year': '2019',
     'custom_types': [
         {
