6e23439c993a7bebe6488d36635c5b9c0e086ac4
Jay Fitzgerald <34140133+ni-jfitzger@users.noreply.github.com>
2024-04-25 16:30:34 -0500

    Update niscope metadata - .proto file reordered

--- src/niscope/metadata/config.py
+++ src/niscope/metadata/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# This file is generated from NI-SCOPE API metadata version 23.5.0d118
+# This file is generated from NI-SCOPE API metadata version 24.3.0d61
 config = {
-    'api_version': '23.5.0d118',
+    'api_version': '24.3.0d61',
     'c_function_prefix': 'niScope_',
     'close_function': 'close',
     'context_manager_name': {
