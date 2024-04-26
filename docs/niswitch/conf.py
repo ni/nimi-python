6ed1167c3b0d05e7e21ba54d28f6896cc68bbee9
Jay Fitzgerald <34140133+ni-jfitzger@users.noreply.github.com>
2024-04-26 13:33:09 -0500

    Update generated files for 1.4.8 release

--- docs/niswitch/conf.py
+++ docs/niswitch/conf.py
@@ -63,7 +63,7 @@ author = 'NI'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '1.4.8.dev0'
+release = '1.4.8'
 # The short X.Y version.
 version = release[:3]
 
