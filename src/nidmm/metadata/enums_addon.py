64f00ea73b5d119c2457d9dd7b9e92399a26d49e
Jay Fitzgerald <34140133+ni-jfitzger@users.noreply.github.com>
2025-01-26 12:16:07 -0600

    Override some metadata to prevent API changes

--- src/nidmm/metadata/enums_addon.py
+++ src/nidmm/metadata/enums_addon.py
@@ -2,5 +2,42 @@
 # Any changes to the API should be made here. enums.py is code generated
 
 enums_override_metadata = {
+    # TODO (ni-jfitzger): delete this override once python_name is corrected for each value. See https://github.com/ni/nimi-python/issues/2072
+    'ThermistorType': {
+        'values': [
+            {
+                'documentation': {
+                    'description': 'Custom'
+                },
+                'name': 'NIDMM_VAL_TEMP_THERMISTOR_CUSTOM',
+                'python_name': 'CUSTOM',
+                'value': 0
+            },
+            {
+                'documentation': {
+                    'description': '44004'
+                },
+                'name': 'NIDMM_VAL_TEMP_THERMISTOR_44004',
+                'python_name': 'THERMISTOR_44004',
+                'value': 1
+            },
+            {
+                'documentation': {
+                    'description': '44006'
+                },
+                'name': 'NIDMM_VAL_TEMP_THERMISTOR_44006',
+                'python_name': 'THERMISTOR_44006',
+                'value': 2
+            },
+            {
+                'documentation': {
+                    'description': '44007'
+                },
+                'name': 'NIDMM_VAL_TEMP_THERMISTOR_44007',
+                'python_name': 'THERMISTOR_44007',
+                'value': 3
+            }
+        ]
+    },
 }
 
