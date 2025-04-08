5f543a040ae97096b0a5272b23aab697b3a2562c
Vagupta <vaishnavi.gupta@ni.com>
2025-04-08 18:27:32 +0000

    Added example

---
+++ src/nirfsg/examples/nirfsg_cw.py
@@ -0,0 +1,22 @@
+import nirfsg
+import numpy as np
+ 
+# RFSG session create
+options = "Simulate=0, DriverSetup=Model:5841"
+session  = nirfsg.Session('5841', id_query=False, reset_device=False, options=options)
+# session = nirfsg.Session("5841", id_query=False, reset_device=False, options=options)
+ 
+# configure frequency to 1Ghz, power level to -10db and iq rate to 1M
+session.configure_rf(
+    1e9, # frequency
+    -10  # powerLevel
+    )
+#session.iq_rate = 1e6
+session.generation_mode = nirfsg.GenerationMode.CW
+
+# Start generation
+with session.initiate():
+    # Generation context
+    input("Press Enter to stop generation")
+ 
+session.close()
