672937e95cd97aeaca154c8f8d28cfb2981cbc5a
dummy <dummy@ni.com>
2025-04-09 23:20:04 +0530

    Remvoing the changes in python scipts present in tools folder.

--- tools/build_release.py
+++ tools/build_release.py
@@ -9,6 +9,8 @@ from subprocess import check_call
 pp = pprint.PrettyPrinter(indent=4, width=100)
 
 default_python_cmd = ['python']
+drivers_to_upload = ['nidcpower', 'nidigital', 'nidmm', 'niswitch', 'nimodinst', 'nifgen', 'niscope', 'nise', 'nitclk']
+drivers_to_update = ['nifake'] + drivers_to_upload
 
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
@@ -21,8 +23,6 @@ class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescri
 
 
 def main():
-    drivers_to_update = ['nidcpower', 'nidigital', 'nidmm', 'nifake', 'niswitch', 'nimodinst', 'nifgen', 'niscope', 'nise', 'nitclk']
-
     # Setup the required arguments for this script
     usage = """Release script
 Prereqs
@@ -33,32 +33,17 @@ Steps: see "Release Process" section of CONTRIBUTING.md
     parser = argparse.ArgumentParser(description=usage, formatter_class=CustomFormatter)
 
     build_group = parser.add_argument_group("Build configuration")
+    build_group.add_argument("--release", action="store_true", default=False, help="This is a release build, so only remove '.devN'. build, then update with .dev0")
     build_group.add_argument("--upload", action="store_true", default=False, help="Upload build distributions to PyPI")
+    build_group.add_argument("--update", action="store_true", default=False, help="Update version in config.py files")
     build_group.add_argument("--build", action="store_true", default=False, help="Clean and build")
     build_group.add_argument("--python-cmd", action="store", default=None, help=f"Command to use for invoking python. Default: {default_python_cmd}")
-    build_group.add_argument("--drivers", action="store", default=None, help="Comma-separated list of drivers to update. Default: All Drivers")
-    build_group.add_argument("--increment-major-version", action="store_true", default=False, help="Increment the major version")
-    build_group.add_argument("--increment-minor-version", action="store_true", default=False, help="Increment the minor version")
-    build_group.add_argument("--increment-patch-version", action="store_true", default=False, help="Increment the patch version")
-    build_group.add_argument("--increment-build-number", action="store_true", default=False, help="Increment the build number")
-    build_group.add_argument("--update-for-release", action="store_true", default=False, help="This is a release build, so only remove '.devN'. build, then update with .dev0")
 
     verbosity_group = parser.add_argument_group("Verbosity, Logging & Debugging")
     verbosity_group.add_argument("-v", "--verbose", action="count", default=0, help="Verbose output")
     verbosity_group.add_argument("--preview", action="store_true", default=False, help="Show what would happen when running with given parameters")
     verbosity_group.add_argument("--log-file", action="store", default=None, help="Send logging to listed file instead of stdout")
     args = parser.parse_args()
-
-    # Validate that only one of the version-related flags is provided
-    version_flags = [
-        args.increment_major_version,
-        args.increment_minor_version,
-        args.increment_patch_version,
-        args.increment_build_number,
-        args.update_for_release,
-    ]
-    if sum(version_flags) > 1:
-        raise ValueError("Only one of --increment-major-version, --increment-minor-version, --increment-patch-version, --increment-build-number or --update-for-release can be provided.")
 
     if args.verbose > 1:
         configure_logging(logging.DEBUG, args.log_file)
@@ -79,30 +64,14 @@ Steps: see "Release Process" section of CONTRIBUTING.md
         passthrough_params.append('--preview')
     if args.log_file:
         passthrough_params.append('--log-file').append(args.log_file)
-    if args.update_for_release:
+    if args.release:
         passthrough_params.append('--release')
-    if args.increment_build_number:
-        passthrough_params.append('--update-type=build')
-    if args.increment_patch_version:
-        passthrough_params.append('--update-type=patch')
-    if args.increment_minor_version:
-        passthrough_params.append('--update-type=minor')
-    if args.increment_major_version:
-        passthrough_params.append('--update-type=major')
 
-    if args.drivers:
-        provided_drivers = args.drivers.split(",")
-        invalid_drivers = [driver for driver in provided_drivers if driver not in drivers_to_update]
-
-        if invalid_drivers:
-            raise ValueError(f"The following drivers are invalid: {', '.join(invalid_drivers)}. Valid drivers are: {','.join(drivers_to_update)}")
-        drivers_to_update = provided_drivers
-
-    if any([args.increment_major_version, args.increment_minor_version, args.increment_patch_version, args.increment_build_number, args.update_for_release]):
+    if args.update:
         logging.info('Updating versions')
 
         for d in drivers_to_update:
-            logging.info(pp.pformat(python_cmd + ['tools/updateReleaseInfo.py', '--src-folder', f'src/{d}',] + passthrough_params))
+            logging.info(pp.pformat(python_cmd + ['tools/updateReleaseInfo.py', '--src-folder', f'src/{d}', ] + passthrough_params))
             check_call(python_cmd + ['tools/updateReleaseInfo.py', '--src-folder', f'src/{d}', ] + passthrough_params)
 
     if args.build:
@@ -117,7 +86,6 @@ Steps: see "Release Process" section of CONTRIBUTING.md
     if args.upload:
         logging.info('Uploading to PyPI')
         complete_twine_cmd = twine_cmd + ['upload']
-        drivers_to_upload = [driver for driver in drivers_to_update if driver != 'nifake']
         for d in drivers_to_upload:
             complete_twine_cmd += [f'generated/{d}/dist/*']
 
--- tools/updateReleaseInfo.py
+++ tools/updateReleaseInfo.py
@@ -1,3 +1,5 @@
+# !python
+
 import argparse
 from configure_logging import configure_logging
 import logging
@@ -8,25 +10,8 @@ import re
 pp = pprint.PrettyPrinter(indent=4, width=100)
 
 
-# Increment version based on bump type ('major', 'minor', 'patch').
-def bump_version(version, bump_type):
-    major, minor, patch = map(int, version.split('.'))
-    logging.info(f"Parsed version: major={major}, minor={minor}, patch={patch}")
-
-    if bump_type == 'patch':
-        patch += 1
-    elif bump_type == 'minor':
-        minor += 1
-        patch = 0
-    elif bump_type == 'major':
-        major += 1
-        minor = 0
-        patch = 0
-
-    return f"{major}.{minor}.{patch}"
-
-
 def main():
+    # Setup the required arguments for this script
     usage = """
 Update version in files. Example: X.Y.Z.devN to X.Y.Z
 """
@@ -34,7 +19,6 @@ Update version in files. Example: X.Y.Z.devN to X.Y.Z
     file_group = parser.add_argument_group("Input and Output files")
     file_group.add_argument("--src-folder", action="store", required=True, help="Source folder")
     file_group.add_argument("--release", action="store_true", default=False, help="This is a release build, so only remove '.devN'. Error if not there")
-    file_group.add_argument("--update-type", action="store", default=None, choices=["major", "minor", "patch", "build"], help="Specify the type of update: major, minor, patch or build. ")
 
     verbosity_group = parser.add_argument_group("Verbosity, Logging & Debugging")
     verbosity_group.add_argument("-v", "--verbose", action="count", default=0, help="Verbose output")
@@ -50,51 +34,27 @@ Update version in files. Example: X.Y.Z.devN to X.Y.Z
         configure_logging(logging.WARNING, args.log_file)
 
     logging.info(pp.pformat(args))
+
     metadata_file = os.path.join(args.src_folder, "metadata", "config_addon.py")
     with open(metadata_file) as content_file:
         contents = content_file.read()
 
     module_dev_version_re = re.compile(r"'module_version': '(\d+\.\d+\.\d+)\.dev(\d+)'")
     m = module_dev_version_re.search(contents)
-    logging.debug(f"Dev version regex match: {m}")
-
     if m:
-        base_version = m.group(1)
-        dev_number = int(m.group(2))
         if args.release:
-            if args.update_type != 'build':
-                bumped_version = bump_version(base_version, args.update_type)
-                logging.info(f"Dev version found, updating {base_version}.dev{dev_number} to {bumped_version}")
-                contents = module_dev_version_re.sub(f"'module_version': '{bumped_version}'", contents)
-                new_version = bumped_version
-            else:
-                logging.info(f"Dev version found, updating {base_version}.dev{dev_number} to {base_version}")
-                contents = module_dev_version_re.sub(f"'module_version': '{base_version}'", contents)
-                new_version = base_version
+            logging.info('Dev version found, updating {0}.dev{1} to {0}'.format(m.group(1), int(m.group(2))))
+            contents = module_dev_version_re.sub(f"'module_version': '{m.group(1)}'", contents)
+            new_version = m.group(1)
         else:
-            if args.update_type == 'build':
-                logging.info(f"Dev version found, updating {base_version}.dev{dev_number} to {base_version}.dev{dev_number + 1}")
-                contents = module_dev_version_re.sub(f"'module_version': '{base_version}.dev{dev_number + 1}'", contents)
-                new_version = f"{base_version}.dev{dev_number + 1}"
-            else:
-                bumped_version = bump_version(base_version, args.update_type)
-                logging.info(f"Dev version found, updating {base_version}.dev{dev_number} to {bumped_version}.dev0")
-                contents = module_dev_version_re.sub(f"'module_version': '{bumped_version}.dev0'", contents)
-                new_version = f"{bumped_version}.dev0"
-    else:
-        module_version_re = re.compile(r"'module_version': '(\d+\.\d+\.)(\d+)'")
-        m = module_version_re.search(contents)
-        logging.debug(f"Release version regex match: {m}")
-        if m:
-            if args.release:
-                logging.error("Error: Attempting to release an already released version for " + os.path.basename(args.src_folder) + ".")
-                return
-            if args.update_type == 'build':
-                args.update_type = 'patch'
-            new_version = bump_version(f"{m.group(1)}{m.group(2)}", args.update_type)
-            logging.info(f"Release version found, updating {m.group(1)}{m.group(2)} to {new_version}.dev0")
-            contents = module_version_re.sub(f"'module_version': '{new_version}.dev0'", contents)
-            new_version = f"{new_version}.dev0"
+            logging.info('Dev version found, updating {0}.dev{1} to {0}.dev{2}'.format(m.group(1), int(m.group(2)), int(m.group(2)) + 1))
+            contents = module_dev_version_re.sub(f"'module_version': '{m.group(1)}.dev{int(m.group(2)) + 1}'", contents)
+
+    module_version_re = re.compile(r"'module_version': '(\d+\.\d+\.)(\d+)'")
+    m = module_version_re.search(contents)
+    if m and not args.release:
+        logging.info('Release version found, updating {0}{1} to {0}{2}.dev0'.format(m.group(1), int(m.group(2)), int(m.group(2)) + 1))
+        contents = module_version_re.sub(f"'module_version': '{m.group(1)}{int(m.group(2)) + 1}.dev0'", contents)
 
     if not args.preview:
         with open(metadata_file, 'w') as content_file:
@@ -110,3 +70,4 @@ Update version in files. Example: X.Y.Z.devN to X.Y.Z
 
 if __name__ == '__main__':
     main()
+
