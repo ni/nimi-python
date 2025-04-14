71f92e33c17051d640c54e24da91142e5656bc5a
Vagupta <vaishnavi.gupta@ni.com>
2025-04-14 08:27:58 +0000

    Updated RFSG feed info

--- src/nirfsg/metadata/config_addon.py
+++ src/nirfsg/metadata/config_addon.py
@@ -1,8 +1,8 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
-    'module_version': '1.4.8.dev0',
-    'latest_runtime_version_tested_against': '2023 Q1.1',
-    'initial_release_year': '2017',
+    'module_version': '0.2.0',
+    'latest_runtime_version_tested_against': '2025 Q1',
+    'initial_release_year': '2025',
     'custom_types': [
     ]
 }
