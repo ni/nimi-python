0d4fc7ff1fed49beab5a0bfb9357f33a4b515e88
Vagupta <vaishnavi.gupta@ni.com>
2025-04-25 13:32:31 +0530

    Updated latest nirfsg installer version

--- src/nirfsg/metadata/config_addon.py
+++ src/nirfsg/metadata/config_addon.py
@@ -1,7 +1,7 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
     'module_version': '0.2.0',
-    'latest_runtime_version_tested_against': '2025 Q1',
+    'latest_runtime_version_tested_against': '2025 Q2',
     'initial_release_year': '2025',
     'custom_types': [
     ]
