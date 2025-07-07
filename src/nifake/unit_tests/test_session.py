4bd214798f71340aa8602d63d233eebcb8ef18f7
rahulr-NI <rahul.r@emerson.com>
2025-07-08 02:34:33 +0530

    Support for complex types (#2098)
    * Implementation of Complex Numbers
    
    * Inclusion of test case for WriteWaveformComplexF64
    
    * Implementation of f32 and i16 and respective test cases
    
    * Updated changelog and included examples
    
    * Updated the metadata
    
    * Remove log file
    
    * Updated functions.py file and few minor cahnges
    
    * Update Changelog.md file
    
    * Updated minor change.
    
    * Updated few minor fix
    
    * Updated the waveform type as ctype
    
    * Updated the seesion lock parameter for new arb functions
    
    * Updated Code Review comments
    
    * Change log code review comments included
    
    * Removed RFSG specific changes and include complextype only if needed
    
    * Removed RFSG specific changes
    
    * Updated matchers and complextype file to be generated bsaed on condition if complex is used
    
    * Updating spacing issue
    
    * Updating the complex variables and code review comments incorporated
    
    * Test case name change
    
    * Updating minor fixes
    
    * Updating the test cases
    
    * Update i16 functions
    
    * Update duplicate entries
    
    * remove zone.identifier file changes.
    
    * Updaed comments for are_complex_parameters_used
    
    * Updated the numpy api names and description.
    
    * Updating default value of complext type as None rather than 'none'
    
    * Moving the common lines in complex matchers into a function
    
    * Updaing the library.py.mako file to import complex type
    
    * Code Reivew Changes
    
    * Updaed code review comments
    
    * Updaing only necessary fields in COMPLEX_NUMBER_PARAMETERS

--- src/nifake/unit_tests/test_session.py
+++ src/nifake/unit_tests/test_session.py
@@ -848,6 +848,33 @@ class TestSession:
             session.function_with_int_flag_parameter(flags)
             mock_func.assert_called_once_with(flags)
 
+    def test_session_write_waveform_numpy_complex64_invalid_dtype(self):
+        invalid_waveform_data = numpy.full(10, 1.0 + 1.0j, dtype=numpy.complex128)
+        expected_error_message = "waveform_data_array must be numpy.ndarray of dtype=complex64, is complex128"
+        import pytest
+        with nifake.Session('dev1') as session:
+            with pytest.raises(TypeError) as exc_info:
+                session.write_waveform_numpy_complex64(invalid_waveform_data)
+            assert str(exc_info.value) == expected_error_message
+
+    def test_session_write_waveform_numpy_complex128_invalid_dtype(self):
+        invalid_waveform_data = numpy.full(10, 1.0 + 1.0j, dtype=numpy.complex64)
+        expected_error_message = "waveform_data_array must be numpy.ndarray of dtype=complex128, is complex64"
+        import pytest
+        with nifake.Session('dev1') as session:
+            with pytest.raises(TypeError) as exc_info:
+                session.write_waveform_numpy_complex128(invalid_waveform_data)
+            assert str(exc_info.value) == expected_error_message
+
+    def test_session_write_waveform_numpy_complex_interleaved_i16_invalid_dtype(self):
+        invalid_waveform_data = numpy.full(10, 1.0 + 1.0j, dtype=numpy.complex64)
+        expected_error_message = "waveform_data_array must be numpy.ndarray of dtype=int16, is complex64"
+        import pytest
+        with nifake.Session('dev1') as session:
+            with pytest.raises(TypeError) as exc_info:
+                session.write_waveform_numpy_complex_interleaved_i16(invalid_waveform_data)
+            assert str(exc_info.value) == expected_error_message
+
 
 class TestGrpcSession:
 
