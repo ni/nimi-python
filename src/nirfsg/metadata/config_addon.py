d4e50b8c07572a3af5138a843c4ef741b368623a
vnktshr21 <venkatesh.rathnavelu@emerson.com>
2025-08-04 21:12:56 +0530

    Updated versions for release

--- src/nirfsg/metadata/config_addon.py
+++ src/nirfsg/metadata/config_addon.py
@@ -1,6 +1,6 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
-    'module_version': '1.0.0.dev0',
+    'module_version': '1.0.0',
     'latest_runtime_version_tested_against': '2025 Q2',
     'initial_release_year': '2025',
     'custom_types': [
