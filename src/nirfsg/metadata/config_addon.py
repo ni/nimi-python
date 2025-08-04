44313762bb3317d565039db27e81c724e4b2b7ca
vnktshr21 <venkatesh.rathnavelu@emerson.com>
2025-08-04 21:15:42 +0530

    Set development status to Beta

--- src/nirfsg/metadata/config_addon.py
+++ src/nirfsg/metadata/config_addon.py
@@ -1,6 +1,7 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
 config_additional_config = {
     'module_version': '1.0.0',
+    'development_status': '4 - Beta',
     'latest_runtime_version_tested_against': '2025 Q3',
     'initial_release_year': '2025',
     'custom_types': [
