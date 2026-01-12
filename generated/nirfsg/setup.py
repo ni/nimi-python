fc8d81ccb1f5135afdd0d1f7e89c036948f35cbb
vnktshr21 <12389486+vnktshr21@users.noreply.github.com>
2026-01-12 20:21:15 +0530

    generated files with version bump

--- generated/nirfsg/setup.py
+++ generated/nirfsg/setup.py
@@ -16,7 +16,7 @@ def read_contents(file_to_read):
 setup(
     name=pypi_name,
     zip_safe=True,
-    version='1.0.1',
+    version='1.0.2.dev0',
     description='NI-RFSG Python API',
     long_description=read_contents('README.rst'),
     long_description_content_type='text/x-rst',
