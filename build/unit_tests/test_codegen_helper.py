02b9c5fb4bd2c60f7b744c26fe46bc13538147fa
Rahul R <rahul.r@emerson.com>
2026-03-04 11:15:39 +0000

    Upding the S161 test case to remove duplicates of full parameter list.

--- build/unit_tests/test_codegen_helper.py
+++ build/unit_tests/test_codegen_helper.py
@@ -1150,12 +1150,11 @@ def test_get_ctype_variable_declaration_snippet_case_s160():
 
 
 def test_get_ctype_variable_declaration_snippet_case_s161():
-    parameters_with_multidim_buffer = list(parameters_for_testing)
-    size_parameter = parameters_with_multidim_buffer[11]
-    buffer_parameter = dict(parameters_with_multidim_buffer[10])
+    size_parameter = parameters_for_testing[11]
+    buffer_parameter = dict(parameters_for_testing[10])
     buffer_parameter['array_dimensions'] = 3
-    parameters_with_multidim_buffer[10] = buffer_parameter
-    snippet = get_ctype_variable_declaration_snippet(size_parameter, parameters_with_multidim_buffer, IviDanceStep.NOT_APPLICABLE, config_for_testing, use_numpy_array=False)
+    parameters_for_testing[10] = buffer_parameter
+    snippet = get_ctype_variable_declaration_snippet(size_parameter, parameters_for_testing, IviDanceStep.NOT_APPLICABLE, config_for_testing, use_numpy_array=False)
     assert snippet == ["input_array_size_ctype = _visatype.ViInt32(0 if input_array is None else input_array.size)  # case S161"]
 
 
