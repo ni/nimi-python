a43789a22db3991c40ff026c751513fade518a8f
rahur-NI <rahul.r@emerson.com>
2026-03-05 16:45:48 +0530

    codegen: add S161 for multidimensional input buffer to use array.size (#2166)

--- build/unit_tests/test_codegen_helper.py
+++ build/unit_tests/test_codegen_helper.py
@@ -1149,6 +1149,16 @@ def test_get_ctype_variable_declaration_snippet_case_s160():
     assert actual == ["input_array_size_ctype = _visatype.ViInt32(0 if input_array is None else len(input_array))  # case S160"]
 
 
+def test_get_ctype_variable_declaration_snippet_case_s161():
+    parameters_with_multidim_buffer = list(parameters_for_testing)
+    # Create a copy to add 'array_dimensions' without modifying original parameters_for_testing[10]
+    buffer_parameter = dict(parameters_with_multidim_buffer[10])
+    buffer_parameter['array_dimensions'] = 3
+    parameters_with_multidim_buffer[10] = buffer_parameter
+    snippet = get_ctype_variable_declaration_snippet(parameters_with_multidim_buffer[11], parameters_with_multidim_buffer, IviDanceStep.NOT_APPLICABLE, config_for_testing, use_numpy_array=False)
+    assert snippet == ["input_array_size_ctype = _visatype.ViInt32(0 if input_array is None else input_array.size)  # case S161"]
+
+
 def test_get_ctype_variable_declaration_snippet_case_s170():
     snippet = get_ctype_variable_declaration_snippet(parameters_for_testing[12], parameters_for_testing, IviDanceStep.QUERY_SIZE, config_for_testing, use_numpy_array=False)
     assert snippet == ["string_size_ctype = _visatype.ViInt32()  # case S170"]
