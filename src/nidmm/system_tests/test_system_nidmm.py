a87a556a78f4c5719036a8aeaacd4592096da1e7
Ryan Wixon <Ryan.Wixon@Emerson.com>
2026-09-17 11:34:37 -0500

    Fix flake

--- src/nidmm/system_tests/test_system_nidmm.py
+++ src/nidmm/system_tests/test_system_nidmm.py
@@ -16,6 +16,7 @@ import nidmm
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent / 'shared'))
 import system_test_utilities  # noqa: E402
 
+
 # Defines a subset of system tests to validate basic DMM functionality. This is run as a part of the full SystemTests class, and
 # independently for test classes which do not require running the entire suite (TLS-enabled gRPC tests today).
 class BasicValidationTests:
