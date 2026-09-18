6fb6f69c5086a9caab1940019f5aef97fe20fb32
Ryan Wixon <Ryan.Wixon@Emerson.com>
2026-09-18 13:51:35 -0500

    Yet another flake error

--- src/nidigital/system_tests/test_system_nidigital.py
+++ src/nidigital/system_tests/test_system_nidigital.py
@@ -35,7 +35,7 @@ class BasicValidationTests:
             levels_file_paths=self.get_test_file_path(test_name, 'pin_levels.digilevels'),
             timing_file_paths=self.get_test_file_path(test_name, 'timing.digitiming'))
         session.apply_levels_and_timing(levels_sheet='pin_levels', timing_sheet='timing')
-    
+
     def get_test_file_path(self, test_name, file_name):
         return os.path.join(test_files_base_dir, test_name, file_name)
 
